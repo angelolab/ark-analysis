95d7a764f03530164bf5fa58615794b96c141bf0
ngreenwald <noahfgreenwald@gmail.com>
2019-04-10 14:21:15 -0700

    added extraction code for new mask npy format

--- preprocessing.py
+++ preprocessing.py
@@ -4,6 +4,8 @@ import skimage.measure
 import numpy as np
 import os
 
+# code to take contoured data and generate mask necessary for CNN training
+
 os.chdir('/Users/noahgreenwald/Documents/Grad_School/Lab/Segmentation/Contours/First_Run/Point23/')
 
 nuc_label = plt.imread('/Users/noahgreenwald/Documents/Grad_School/Lab/Segmentation/Contours/First_Run/Point23/Nuclear_Interior_Mask.tif')
@@ -14,5 +16,3 @@ x.save('Nuclear_Interior_Mask_Label.tif')
 
 
 z = plt.imread('annotated/feature.tif')
-
-z
