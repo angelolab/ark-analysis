dac07f39aadc89a0167b901c2bffdd64ded453e3
ngreenwald <noahfgreenwald@gmail.com>
2019-07-24 14:18:21 -0700

    pre merge commit

--- extract_single_cell_data.py
+++ extract_single_cell_data.py
@@ -4,35 +4,48 @@ import skimage.morphology as morph
 import skimage
 import helper_functions
 import importlib
+import copy
+import numpy as np
 importlib.reload(helper_functions)
+
 
 # load segmentation masks
 seg_dir = '/Users/noahgreenwald/Documents/Grad_School/Lab/Segmentation_Project/Contours/First_Run/'
 seg_folder = ''
-
-seg_data = helper_functions.load_tifs_from_points_dir(seg_dir, seg_folder, ['Point23'], ['Nuclear_Mask_Label.tif'])
-mask = seg_data.loc['Point23', 'Nuclear_Mask_Label.tif', :, :].values.astype('int')
-
-mask_nuc = morph.erosion(mask, morph.square(3))
-mask_whole = morph.dilation(mask_nuc, morph.square(5))
+cell_seg_data = helper_functions.load_tifs_from_points_dir(seg_dir, seg_folder, ['Point23'], ['Nuclear_Mask_Label.tif'])
+nuc_seg_data = helper_functions.load_tifs_from_points_dir(seg_dir, seg_folder, ['Point23'], ['Nuclear_Mask_Label.tif'])
 
 # load TIFs
 tif_dir = '/Users/noahgreenwald/Documents/Grad_School/Lab/Segmentation_Project/Contours/First_Run/'
 tif_folder = 'TIFsNoNoise'
-
 image_data = helper_functions.load_tifs_from_points_dir(tif_dir, tif_folder, ['Point23'], ['dsDNA.tif', 'LaminAC.tif'])
 
+for i in range(len(image_data.channel)):
+    point = image_data.point.values[i]
 
-# merge small cells?
-cell_props = skimage.measure.regionprops(mask)
-adjc_matrix = helper_functions.calc_adjacency_matrix(mask)
+    # read in segmentation masks for whole-cell and nucleus
+    cell_labels = cell_seg_data.loc[point, 'Nuclear_Mask_Label.tif', :, :].values.astype('int')
 
-cell_arrays = np.zeros((3, marker_num, cell_num))
-cell_arrays[0, :, :] = helper_functions.segment_data_function(mask_whole, image_data)
-cell_arrays[1, :, :] = helper_functions.segment_data_function(mask_nuc)
-cell_arrays[2, :, :] = helper_functions.segment_data_function(mask_cyto)
+    # merge small cells?
+    cell_props = skimage.measure.regionprops(cell_labels)
 
-single_cell_xr = xr.DataArray(cell_arrays, coords=[['All_Signal', 'Nuclear_Signal', 'Cytoplasmic_Signal'],
-                                                   image_data.channel.values, cell_ids],
-                          dims=["point", "channel", "x_axis", "y_axis"])
+    nuc_mask = nuc_seg_data.loc[point, 'Nuclear_Mask_Label.tif', :, :].values.astype('int')
+
+    # fake data for now
+    nuc_mask = morph.erosion(nuc_mask, morph.square(3))
+    cell_labels = morph.dilation(nuc_mask, morph.square(5))
+    nuc_mask = nuc_mask > 0
+
+    # duplicate whole cell data for cytoplasmic data
+    cyto_labels = copy.copy(cell_labels)
+    cyto_labels[nuc_mask] = 0
+
+    nuc_labels = copy.copy(cell_labels)
+    nuc_labels[~nuc_mask] = 0
+
+    masks = np.zeros((3, 1024, 1024))
+    masks[0, :, :] = cell_labels
+    masks[1, :, :] = nuc_labels
+    masks[2, :, :] = cyto_labels
+    cell_arrays = helper_functions.segment_data_function(image_data, masks)
 
