8ec2a0cb0b2f68a1f6e6f1e419a92add0bad7f69
ngreenwald <noahfgreenwald@gmail.com>
2019-09-26 09:24:47 -0700

    reorg

--- preprocessing/run_network_preprocessing.py
+++ preprocessing/run_network_preprocessing.py
@@ -6,15 +6,16 @@ import helper_functions
 
 
 # load TIFs from GUI-based directory structure
-base_dir = '/Users/noahgreenwald/Documents/MIBI_Data/selena/20190524_GBM_test/no_noise'
+base_dir = '/Users/noahgreenwald/Documents/MIBI_Data/selena/20190925_PAH project/HH3NoNoise_RawMarkersIntensities/TMA1_run1_no_noise/'
 
-data_xr = helper_functions.load_tifs_from_points_dir(base_dir, tif_folder='TIFsNoBg', tifs=['H3.tif', 'CD45.tif'])
-data_xr.to_netcdf(base_dir + 'Nuclear_Channel_Input.nc')
+data_xr = helper_functions.load_tifs_from_points_dir(base_dir, tif_folder='TIFs', tifs=['H3.tif'])
+data_xr.name = 'TMA1_run1_no_noise'
+data_xr.to_netcdf(base_dir + 'Nuclear_Channel_Input.nc', format="NETCDF3_64BIT")
 
 # run deepcell, save output back to folder, then load here
 data_xr_watershed = xr.open_dataarray(base_dir + 'watershed_output.nc')
-data_xr_pixel = xr.open_dataarray(base_dir + 'pixel_output.nc')
-folders = data_xr.coords['point'].values.tolist()
+data_xr_pixel = xr.open_dataarray(base_dir + 'TMA1_run1_no_noise.nc')
+folders = data_xr.coords['points'].values.tolist()
 
 folders = [x for x in folders]
 # extract individual TIFs and save segmentation results back into matlab-compatible folder structure
