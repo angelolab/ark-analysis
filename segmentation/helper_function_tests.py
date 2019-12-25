d175cc799b56d0fba702a65067af36a8a42b8b14
ngreenwald <noahfgreenwald@gmail.com>
2019-12-25 10:02:58 -1000

    testing save

---
+++ segmentation/helper_function_tests.py
@@ -0,0 +1,18 @@
+from segmentation import helper_functions
+import xarray as xr
+import numpy as np
+
+
+def test_combine_xarrays():
+    xr1 = xr.DataArray(np.random.randint(10, size=(3, 30, 30, 3)),
+                       coords=[["Point1", "Point2", "Point3"], range(30), range(30), ["chan1", "chan2", "chan3"]],
+                       dims=["points", "rows", "cols", "channels"])
+
+    xr2 = xr.DataArray(np.random.randint(10, size=(2, 30, 30, 3)),
+                       coords=[["Point4", "Point5"], range(30), range(30), ["chan1", "chan2", "chan3"]],
+                       dims=["points", "rows", "cols", "channels"])
+
+    xr_combined = helper_functions.combine_xarrays((xr1, xr2), axis=0)
+    assert xr_combined.shape == (5, 30, 30, 3)
+
+
