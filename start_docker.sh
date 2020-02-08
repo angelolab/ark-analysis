197971fddb0c5c934de6cda3c0c07bf371dc6452
ngreenwald <noahfgreenwald@gmail.com>
2020-02-08 09:00:44 -0800

    removed scripts mounting

--- start_docker.sh
+++ start_docker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-NV_GPU='0' docker run -it \
+docker run -it \
   -p 8888:8888 \
-  -v $PWD/segmentation:/usr/local/lib/python3.6/dist-packages/segmentation/ \
   -v $PWD/scripts:/scripts \
   -v $PWD/data:/data \
   $USER/segmentation:latest
