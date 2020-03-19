f1b829fc063b36ba5132d10e638dfb0b2af8fce8
ngreenwald <noahfgreenwald@gmail.com>
2020-03-18 19:27:32 -0700

    notebook update

---
+++ segmentation/utils/notebooks_test.py
@@ -0,0 +1,15 @@
+import subprocess
+import tempfile
+
+
+def _exec_notebook(path):
+    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
+        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+                "--ExecutePreprocessor.timeout=1000",
+                "--output", fout.name, path]
+        subprocess.check_call(args)
+
+
+def test():
+    _exec_notebook('scripts/Deepcell_Preprocessing.ipynb')
+
