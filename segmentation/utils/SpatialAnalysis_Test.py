9b6e74c094251be8f5da06b75c56767edf2e6677
Jaiveer Singh <jaiveers.24@gmail.com>
2020-05-05 20:37:31 -0700

    New Spatial Analysis Test

--- segmentation/utils/SpatialAnalysis_Test.py
+++ segmentation/utils/SpatialAnalysis_Test.py
@@ -4,12 +4,11 @@ from segmentation.utils import Spatial_Analysis
 def test_calc_dist_matrix():
 
     test_mat = np.zeros((512, 512), dtype="int")
-
-    test_mat[40, 20] = 1
-    test_mat[44, 17] = 2
+    test_mat[0, 20] = 1
+    test_mat[4, 17] = 2
 
     dist_matrix = Spatial_Analysis.calc_dist_matrix(test_mat)
-    return dist_matrix
+    real_mat = np.array([[0, 5], [5, 0]])
+    assert np.array_equal(dist_matrix, real_mat)
 
-print(test_calc_dist_matrix())
-
+test_calc_dist_matrix()
