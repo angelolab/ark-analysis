e31323c0ddc4467b4b81953a7ad16550f7a2de82
Jaiveer Singh <jaiveers.24@gmail.com>
2020-05-17 17:57:09 -0700

    corrections and pvalue error test

--- segmentation/utils/spatialanalysis_utils_test.py
+++ segmentation/utils/spatialanalysis_utils_test.py
@@ -15,16 +15,80 @@ def test_calc_dist_matrix():
     assert np.array_equal(dist_matrix, real_mat)
 
 
+"""
 def test_distmat():
     testtiff = io.imread(
         "/Users/jaiveersingh/Documents/MATLAB/SpatialAnalysis/newLmod.tiff")
-    distMat = np.asarray(pd.read_csv(
+    distmat = np.asarray(pd.read_csv(
         "/Users/jaiveersingh/Documents/MATLAB/distancesMat5.csv", header=None))
-    testMat = spatialanalysis_utils.calc_dist_matrix(testtiff)
-    assert np.allclose(distMat, testMat)
+    testmat = spatialanalysis_utils.calc_dist_matrix(testtiff)
+    assert np.allclose(distmat, testmat)
+"""
+
+
+def make_distance_matrix(typeofenfrichment):
+    if(typeofenfrichment == "none"):
+        randmat = np.random.randint(0, 200, size=(60, 60))
+        np.fill_diagonal(randmat, 0)
+        return randmat
+    elif(typeofenfrichment == "positive"):
+        distmatp = np.zeros((80, 80))
+        distmatp[10:20, :10] = 50
+        distmatp[:10, 10:20] = 50
+        distmatp[20:40, :20] = 200
+        distmatp[:20, 20:40] = 200
+        distmatp[40:80, :40] = 300
+        distmatp[:40, 40:80] = 300
+        return distmatp
+    elif(typeofenfrichment == "negative"):
+        distmatn = np.zeros((60, 60))
+        distmatn[20:40, :20] = 300
+        distmatn[:20, 20:40] = 300
+        distmatn[40:50, :40] = 50
+        distmatn[:40, 40:50] = 50
+        distmatn[50:60, :50] = 200
+        distmatn[:50, 50:60] = 200
+        return distmatn
+
+
+def make_expression_matrix(typeofencrichment):
+    if(typeofencrichment == "none"):
+        cellarray = pd.DataFrame(np.zeros((60, 53)))
+        cellarray[0] = 6
+        cellarray[1] = np.arange(len(cellarray[1])) + 1
+        cellarray.iloc[0:20, 7] = 1
+        cellarray.iloc[20:40, 8] = 1
+        return cellarray
+    elif(typeofencrichment == "positive"):
+        cellarrayp = pd.DataFrame(np.zeros((80, 53)))
+        cellarrayp[0] = 6
+        cellarrayp[1] = np.arange(len(cellarrayp[1])) + 1
+        cellarrayp.iloc[0:8, 7] = 1
+        cellarrayp.iloc[28:30, 7] = 1
+        cellarrayp.iloc[38:40, 7] = 1
+        cellarrayp.iloc[10:18, 8] = 1
+        cellarrayp.iloc[27, 8] = 1
+        cellarrayp.iloc[30, 8] = 1
+        cellarrayp.iloc[36:38, 8] = 1
+        return cellarrayp
+    elif(typeofencrichment == "negative"):
+        cellarrayn = pd.DataFrame(np.zeros((60, 53)))
+        cellarrayn[0] = 6
+        cellarrayn[1] = np.arange(len(cellarrayn[1])) + 1
+        cellarrayn.iloc[0:20, 7] = 1
+        cellarrayn.iloc[20:40, 8] = 1
+        return cellarrayn
+
+
+def make_threshold_mat():
+    thresh = pd.DataFrame(np.zeros((37, 2)))
+    thresh.iloc[:, 1] = .5
+    return thresh
 
 
 def test_spatial_analysis():
+
+    """
     # test the closenum function
     test_cellarray = pd.read_csv(
         "/Users/jaiveersingh/Desktop/tests/celllabels.csv")
@@ -32,20 +96,12 @@ def test_spatial_analysis():
         "/Users/jaiveersingh/Desktop/tests/thresholds.csv")
     test_distmat = np.asarray(pd.read_csv(
         "/Users/jaiveersingh/Desktop/tests/distmat.csv", header=None))
-    test_closeNum = spatialorgmarkerexpression_utils.spatial_analysis(
-        test_distmat, test_thresholds, test_cellarray)
-    assert test_closeNum[0, 0] == 16
-    assert test_closeNum[0, 1] == 16
-    assert test_closeNum[1, 0] == 16
-    assert test_closeNum[1, 1] == 16
-    assert test_closeNum[2, 2] == 25
-    assert test_closeNum[2, 3] == 25
-    assert test_closeNum[3, 2] == 25
-    assert test_closeNum[3, 3] == 25
-    assert test_closeNum[4, 4] == 1
-    assert test_closeNum[5, 4] == 1
-    assert test_closeNum[4, 5] == 1
-    assert test_closeNum[5, 5] == 1
+    test_closeNum, closeNumRand, z, muhat, sigmahat, p, h, adj_p, \
+        markerTitles = spatialorgmarkerexpression_utils.spatial_analysis(
+            test_distmat, test_thresholds, test_cellarray)
+    assert (test_closeNum[:2, :2] == 16).all()
+    assert (test_closeNum[2:4, 2:4] == 25).all()
+    assert (test_closeNum[4:6, 4:6] == 1).all()
 
     # Now test with Erin's output
     cell_array = pd.read_csv(
@@ -55,36 +111,40 @@ def test_spatial_analysis():
         "/Users/jaiveersingh/Downloads/SpatialEnrichment/markerThresholds.csv")
     dist_matrix = np.asarray(pd.read_csv(
         "/Users/jaiveersingh/Documents/MATLAB/distancesMat5.csv", header=None))
-    closeNum = spatialorgmarkerexpression_utils.spatial_analysis(
-        dist_matrix, marker_thresholds, cell_array)
-    real_closeNum = np.asarray(pd.read_csv(
+    closenum, closenumRand, z, muhat, sigmahat, p, h, adj_p, markertitles = \
+        spatialorgmarkerexpression_utils.spatial_analysis(
+            dist_matrix, marker_thresholds, cell_array)
+    real_closenum = np.asarray(pd.read_csv(
         "/Users/jaiveersingh/Documents/MATLAB/SpatialAnalysis/closeNum.csv"))
-    assert np.array_equal(closeNum, real_closeNum)
+    assert np.array_equal(closenum, real_closenum)
+    """
 
     # test z and p values
-    marker_thresholds = pd.read_csv(
-        "/Users/jaiveersingh/Downloads/SpatialEnrichment/markerThresholds.csv")
+    marker_thresholds = make_threshold_mat()
     # positive enrichment
-    cellArrayP = spatialanalysis_utils.make_expression_matrix("positive")
-    distMatP = spatialanalysis_utils.make_distance_matrix("positive")
-    closeNum, closeNumRand, z, muhat, sigmahat, p, h, adj_p, markerTitles = \
+    cellarrayp = make_expression_matrix("positive")
+    distmatp = make_distance_matrix("positive")
+    closenum, closenumrand, zp, muhat, sigmahat, pp, h, adj_p, markertitles = \
         spatialorgmarkerexpression_utils.spatial_analysis(
-            distMatP, marker_thresholds, cellArrayP)
-    assert p[0, 1, 0] < .05
-    assert z[0, 1] > 0
+            distmatp, marker_thresholds, cellarrayp)
+    assert pp[0, 1, 0] < .05
+    assert pp[0, 1, 1] > .05
+    assert zp[0, 1] > 0
     # negative enrichment
-    cellArrayN = spatialanalysis_utils.make_expression_matrix("negative")
-    distMatN = spatialanalysis_utils.make_distance_matrix("negative")
-    closeNum, closeNumRand, z, muhat, sigmahat, p, h, adj_p, markerTitles = \
+    cellarrayn = make_expression_matrix("negative")
+    distmatn = make_distance_matrix("negative")
+    closenum, closenumrand, zn, muhatn, sigmahatn, pn, h, adj_p, markertitles = \
         spatialorgmarkerexpression_utils.spatial_analysis(
-            distMatN, marker_thresholds, cellArrayN)
-    assert p[0, 1, 1] < .05
-    assert z[0, 1] < 0
+            distmatn, marker_thresholds, cellarrayn)
+    assert pn[0, 1, 1] < .05
+    assert pn[0, 1, 0] > .05
+    assert zn[0, 1] < 0
     # no enrichment
-    cellArray = spatialanalysis_utils.make_expression_matrix("none")
-    distMat = spatialanalysis_utils.make_distance_matrix("none")
-    closeNum, closeNumRand, z, muhat, sigmahat, p, h, adj_p, markerTitles = \
+    cellarray = make_expression_matrix("none")
+    distmat = make_distance_matrix("none")
+    closenum, closenumrand, z, muhat, sigmahat, p, h, adj_p, markertitles = \
         spatialorgmarkerexpression_utils.spatial_analysis(
-            distMat, marker_thresholds, cellArray)
+            distmat, marker_thresholds, cellarray)
     assert p[0, 1, 0] > .05
+    assert p[0, 1, 1] < .05
     assert abs(z[0, 1]) < 1
--- segmentation/utils/spatialorgmarkerexpression_utils.py
+++ segmentation/utils/spatialorgmarkerexpression_utils.py
@@ -5,6 +5,7 @@ import statsmodels
 from statsmodels.stats.multitest import multipletests
 
 # Erin's Data Inputs
+
 cell_array = pd.read_csv("/Users/jaiveersingh/Downloads/SpatialEn"
                          "richment/granA_cellpheno_CS-asinh-norm_revised.csv")
 marker_thresholds = pd.read_csv("/Users/jaiveersingh/Downloads/Sp"
@@ -13,112 +14,151 @@ dist_matrix = np.asarray(pd.read_csv("/Users/jaiveersingh/Documen"
                                      "ts/MATLAB/distancesMat5.csv",
                                      header=None))
 
-# pv_cellarray = pd.read_csv("/Users/jaive
-# ersingh/Desktop/tests/pvcelllabel.csv")
 
-# pv_distmat = np.asarray(pd.read_csv("/Us
-# ers/jaiveersingh/Desktop/tests/pvdistmat.csv", header = None))
+# Test array inputs
+"""
+pv_cellarray = pd.read_csv("/Users/jaiveersingh/De
+sktop/tests/pvcelllabel.csv")
 
-# pv_cellarrayN = pd.read_csv("/Users/jaiv
-# eersingh/Desktop/tests/pvcellarrayN.csv")
+pv_distmat = np.asarray(pd.read_csv("/Users/jaiveersingh/
+Desktop/tests/pvdistmat.csv", header = None))
 
-# pv_distmatN = np.asarray(pd.read_csv("/Use
-# rs/jaiveersingh/Desktop/tests/pvdistmatN.csv", header = None))
+pv_cellarrayn = pd.read_csv("/Users/jaiv
+eersingh/Desktop/tests/pvcellarrayN.csv")
 
-# randMat = np.random.randint(0,200,size=(60,60))
-# np.fill_diagonal(randMat, 0)
+pv_distmatn = np.asarray(pd.read_csv("/Users/jaiveers
+ingh/Desktop/tests/pvdistmatN.csv", header = None))
 
-# pv_cellarrayR = pd.read_csv("/Users/jaiveers
-# ingh/Desktop/tests/pvcellarrayR.csv")
+randMat = np.random.randint(0,200,size=(60,60))
+np.fill_diagonal(randMat, 0)
+
+pv_cellarrayr = pd.read_csv("/Users/jaiveers"
+ingh/Desktop/tests/pvcellarrayR.csv")
+"""
 
 
 def spatial_analysis(dist_matrix, marker_thresholds, cell_array):
+    """Spatial enrichment analysis to find significant interactions between cells
+    expressing different markers.
+    Uses bootstrapping to permute cell labels randomly.
+            Args
+                dist_matrix: cells x cells matrix with the euclidian
+                    distance between centers of corresponding cells
+                marker_thresholds: threshold
+                    values for positive marker expression
+                cell_array: data including points, cell labels, and
+                    cell expression matrix for all markers
+            Returns
+                close_num: marker x marker matrix with counts for cells
+                    positive for corresponding markers
+                close_num_rand: random positive marker counts
+                    for every permutation in the bootstrap
+                z: z scores for corresponding markers
+                muhat: predicted mean values
+                    of close_num_rand random distribution
+                sigmahat: predicted standard deviation values of close_num_rand
+                    random distribution
+                p: p values for corresponding markers, for both positive
+                    and negative enrichment
+                h: matrix indicating whether
+                    corresponding marker interactions are significant
+                adj_p: fdh_br adjusted p values
+                marker_titles: list of markers"""
+
     # Setup input and parameters
     point = 6
-    dataAll = cell_array
-    markerInds = [7, 8] + list(range(10, 44))
-    dataMarkers = dataAll.loc[:, dataAll.columns[markerInds]]
-    markerTitles = dataAll.columns[markerInds]
-    markerNum = len(markerTitles)
+    data_all = cell_array
+    marker_inds = [7, 8] + list(range(10, 44))
+    data_markers = data_all.loc[:, data_all.columns[marker_inds]]
+    marker_titles = data_all.columns[marker_inds]
+    marker_num = len(marker_titles)
 
-    bootstrapNum = 100
-    distLim = 100
+    bootstrap_num = 100
+    dist_lim = 100
 
-    closeNum = np.zeros((markerNum, markerNum), dtype='int')
-    closeNumRand = np.zeros((markerNum, markerNum, bootstrapNum), dtype='int')
+    close_num = np.zeros((marker_num, marker_num), dtype='int')
+    close_num_rand = np.zeros((
+        marker_num, marker_num, bootstrap_num), dtype='int')
 
-    patientIdx = 0
-    cellLabelIdx = 1
+    patient_idx = 0
+    cell_label_idx = 1
 
-    markerThresh = marker_thresholds
-    thresh_vec = markerThresh.iloc[1:38, 1]
+    marker_thresh = marker_thresholds
+    thresh_vec = marker_thresh.iloc[1:38, 1]
 
     # Enrichment Analysis
 
     dist_mat = dist_matrix
-    patientIds = dataAll.iloc[:, patientIdx] == point
-    patientData = dataAll[patientIds]
-    patientDataMarkers = dataMarkers[patientIds]
+    patient_ids = data_all.iloc[:, patient_idx] == point
+    patient_data = data_all[patient_ids]
+    patient_data_markers = data_markers[patient_ids]
 
     # later implement outside for loop for dif points
-    for j in range(0, markerNum):
+    for j in range(0, marker_num):
         marker1_thresh = thresh_vec.iloc[j]
-        marker1PosInds = patientDataMarkers
-        [patientDataMarkers.columns[j]] > marker1_thresh
-        marker1PosLabels = patientData.loc[
-            marker1PosInds, patientData.columns[cellLabelIdx]]
-        marker1Num = len(marker1PosLabels)
-        for k in range(0, markerNum):
+        marker1posinds = patient_data_markers[patient_data_markers.columns[
+            j]] > marker1_thresh
+        marker1poslabels = patient_data.loc[
+            marker1posinds, patient_data.columns[cell_label_idx]]
+        marker1_num = len(marker1poslabels)
+        for k in range(0, marker_num):
             marker2_thresh = thresh_vec.iloc[k]
-            marker2PosInds = patientDataMarkers
-            [patientDataMarkers.columns[k]] > marker2_thresh
-            marker2PosLabels = patientData.loc[
-                marker2PosInds, patientData.columns[cellLabelIdx]]
-            marker2Num = len(marker2PosLabels)
-            truncDistMat = dist_mat[np.ix_(
-                np.asarray(marker1PosLabels-1), np.asarray(
-                    marker2PosLabels-1))]
+            marker2posinds = patient_data_markers[patient_data_markers.columns[
+                k]] > marker2_thresh
+            marker2poslabels = patient_data.loc[
+                marker2posinds, patient_data.columns[cell_label_idx]]
+            marker2_num = len(marker2poslabels)
+            trunc_dist_mat = dist_mat[np.ix_(
+                np.asarray(marker1poslabels-1), np.asarray(
+                    marker2poslabels-1))]
 
-            truncDistMatBin = np.zeros(truncDistMat.shape, dtype='int')
-            truncDistMatBin[truncDistMat < distLim] = 1
+            trunc_dist_mat_bin = np.zeros(trunc_dist_mat.shape, dtype='int')
+            trunc_dist_mat_bin[trunc_dist_mat < dist_lim] = 1
 
-            closeNum[j, k] = np.sum(np.sum(truncDistMatBin))
-            for r in range(0, bootstrapNum):
-                marker1LabelsRand = patientData[
-                    patientData.columns[cellLabelIdx]].sample(
-                    n=marker1Num, replace=True)
-                marker2LabelsRand = patientData[
-                    patientData.columns[cellLabelIdx]].sample(
-                    n=marker2Num, replace=True)
-                randTruncDistMat = dist_mat[np.ix_(
-                    np.asarray(marker1LabelsRand - 1), np.asarray(
-                        marker2LabelsRand - 1))]
-                randTruncDistMatBin = np.zeros(
-                    randTruncDistMat.shape, dtype='int')
-                randTruncDistMatBin[randTruncDistMat < distLim] = 1
+            close_num[j, k] = np.sum(np.sum(trunc_dist_mat_bin))
+            # print(close_num[j,k])
+            for r in range(0, bootstrap_num):
+                marker1labelsrand = patient_data[
+                    patient_data.columns[cell_label_idx]].sample(
+                    n=marker1_num, replace=True)
+                marker2labelsrand = patient_data[
+                    patient_data.columns[cell_label_idx]].sample(
+                    n=marker2_num, replace=True)
+                rand_trunc_dist_mat = dist_mat[np.ix_(
+                    np.asarray(marker1labelsrand - 1), np.asarray(
+                        marker2labelsrand - 1))]
+                rand_trunc_dist_mat_bin = np.zeros(
+                    rand_trunc_dist_mat.shape, dtype='int')
+                rand_trunc_dist_mat_bin[rand_trunc_dist_mat < dist_lim] = 1
 
-                closeNumRand[j, k, r] = np.sum(np.sum(randTruncDistMatBin))
-
+                close_num_rand[j, k, r] = \
+                    np.sum(np.sum(rand_trunc_dist_mat_bin))
     # z score, pval, and adj pval
-    z = np.zeros((markerNum, markerNum))
-    muhat = np.zeros((markerNum, markerNum))
-    sigmahat = np.zeros((markerNum, markerNum))
+    z = np.zeros((marker_num, marker_num))
+    muhat = np.zeros((marker_num, marker_num))
+    sigmahat = np.zeros((marker_num, marker_num))
 
-    p = np.zeros((markerNum, markerNum, 2))
+    p = np.zeros((marker_num, marker_num, 2))
 
-    for j in range(0, markerNum):
-        for k in range(0, markerNum):
-            tmp = np.reshape(closeNumRand[j, k, :], (bootstrapNum, 1))
+    for j in range(0, marker_num):
+        for k in range(0, marker_num):
+            tmp = np.reshape(close_num_rand[j, k, :], (bootstrap_num, 1))
+            # print(tmp)
             (muhat[j, k], sigmahat[j, k]) = scipy.stats.norm.fit(tmp)
-            z[j, k] = (closeNum[j, k] - muhat[j, k]) / sigmahat[j, k]
+            # print(muhat[j, k])
+            # print(sigmahat[j, k])
+            z[j, k] = (close_num[j, k] - muhat[j, k]) / sigmahat[j, k]
+            # print(z[j, k])
             p[j, k, 0] = (1 + (
-                np.sum(tmp >= closeNum[j, k]))) / (bootstrapNum + 1)
+                np.sum(tmp >= close_num[j, k]))) / (bootstrap_num + 1)
+            # print(p[j, k, 0])
             p[j, k, 1] = (1 + (
-                np.sum(tmp <= closeNum[j, k]))) / (bootstrapNum + 1)
+                np.sum(tmp <= close_num[j, k]))) / (bootstrap_num + 1)
+            # print(p[j, k, 1])
 
     p_summary = p[:, :, 0]
-    for j in range(0, markerNum):
-        for k in range(0, markerNum):
+    for j in range(0, marker_num):
+        for k in range(0, marker_num):
             if z[j, k] > 0:
                 p_summary[j, k] = p[j, k, 0]
             else:
@@ -126,6 +166,6 @@ def spatial_analysis(dist_matrix, marker_thresholds, cell_array):
     (h, adj_p, aS, aB) = statsmodels.stats.multitest.multipletests(
         p_summary, alpha=.05)
 
-    return \
-        closeNum, closeNumRand, z, muhat, sigmahat, p, h, adj_p, markerTitles
+    return close_num, close_num_rand, z, \
+        muhat, sigmahat, p, h, adj_p, marker_titles
     # end
