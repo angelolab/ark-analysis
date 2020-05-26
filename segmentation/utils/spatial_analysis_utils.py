462a0a2c6ce8d31d7296f195f792823b4ba9f9c7
Jaiveer Singh <jaiveers.24@gmail.com>
2020-05-26 16:42:15 -0700

    Code Cleanup with new Files

--- segmentation/utils/spatial_analysis_utils.py
+++ segmentation/utils/spatial_analysis_utils.py
@@ -2,6 +2,9 @@ import numpy as np
 import xarray as xr
 import pandas as pd
 import skimage.measure
+import scipy
+import statsmodels
+from statsmodels.stats.multitest import multipletests
 from scipy.spatial.distance import cdist
 
 
@@ -28,3 +31,156 @@ def calc_dist_matrix(label_map):
     dims = ["points", "rows", "cols"]
     dist_mats_xr = xr.DataArray(dist_mats, coords=coords, dims=dims)
     return dist_mats_xr
+
+
+def compute_close_cell_num(patient_data_markers, thresh_vec,
+                           dist_mat, marker_num, dist_lim):
+    """Finds positive cell labels and creates matrix with counts for cells positive for corresponding markers.
+
+    This function loops through all the included markers in the patient data and identifies cell labels positive for
+    corresponding markers. It then subsets the distance matrix to only include these positive cells and records
+    interactions based on whether cells are close to each other (within the dist_lim). It then stores the number of
+    interactions in the index of close_num corresponding to both markers (for instance markers 1 and 2 would be in
+    index [0, 1]).
+
+    Args:
+        patient_data_markers: cell expression data of markers
+            for the specific point
+        thresh_vec: matrix of thresholds column for markers
+        dist_mat: cells x cells matrix with the euclidian
+            distance between centers of corresponding cells
+        marker_num: number of markers in expresion data
+        dist_lim: threshold for spatial enrichment distance proximity
+
+    Returns:
+        close_num: marker x marker matrix with counts for cells
+            positive for corresponding markers
+        marker1_num: list of number of cell labels for marker 1
+        marker2_num: list of number of cell labels for marker 2"""
+
+    # Create close_num, marker1_num, and marker2_num
+    close_num = np.zeros((marker_num, marker_num), dtype='int')
+    marker1_num = []
+    marker2_num = []
+
+    for j in range(0, marker_num):
+        # Identify cell labels that are positive for respective markers
+        marker1_thresh = thresh_vec.iloc[j]
+        marker1posinds = patient_data_markers[patient_data_markers.columns[j]] > marker1_thresh
+        marker1_num.append(sum(marker1posinds))
+        for k in range(0, marker_num):
+            # Identify cell labels that are positive for the kth marker
+            marker2_thresh = thresh_vec.iloc[k]
+            marker2posinds = patient_data_markers[patient_data_markers.columns[k]] > marker2_thresh
+            marker2_num.append(sum(marker2posinds))
+            # Subset the distance matrix to only include cells positive for both markers j and k
+            trunc_dist_mat = dist_mat[np.ix_(np.asarray(marker1posinds), np.asarray(marker2posinds))]
+            # Binarize the truncated distance matrix to only include cells within distance limit
+            trunc_dist_mat_bin = np.zeros(trunc_dist_mat.shape, dtype='int')
+            trunc_dist_mat_bin[trunc_dist_mat < dist_lim] = 1
+            # Record the number of interactions and store in close_num in index corresponding to both markers
+            close_num[j, k] = np.sum(np.sum(trunc_dist_mat_bin))
+    return close_num, marker1_num, marker2_num
+
+
+def compute_close_cell_num_random(marker1_num, marker2_num,
+                                  dist_mat, marker_num, dist_lim, bootstrap_num):
+    """Uses bootstrapping to permute cell labels randomly and records the number of close cells (within the dit_lim)
+    in that random setup.
+
+    Args
+        marker1_num: list of number of cell labels for marker 1
+        marker2_num: list of number of cell labels for marker 2
+        dist_mat: cells x cells matrix with the euclidian
+            distance between centers of corresponding cells
+        marker_num: number of markers in expresion data
+        dist_lim: threshold for spatial enrichment distance proximity
+        bootstrap_num: number of permutations
+
+    Returns
+        close_num_rand: random positive marker counts
+            for every permutation in the bootstrap"""
+
+    # Create close_num_rand
+    close_num_rand = np.zeros((
+        marker_num, marker_num, bootstrap_num), dtype='int')
+
+    for j in range(0, marker_num):
+        for k in range(0, marker_num):
+            for r in range(0, bootstrap_num):
+                # Select same amount of random cell labels as positive ones in same marker in close_num
+                marker1_labels_rand = np.random.choice(a=range(dist_mat.shape[0]), size=marker1_num[j], replace=True)
+                marker2_labels_rand = np.random.choice(a=range(dist_mat.shape[0]), size=marker2_num[k], replace=True)
+                # Subset the distance matrix to only include positive randomly selected cell labels
+                rand_trunc_dist_mat = dist_mat[np.ix_(np.asarray(
+                    marker1_labels_rand), np.asarray(marker2_labels_rand))]
+                # Binarize the truncated distance matrix to only include cells within distance limit
+                rand_trunc_dist_mat_bin = np.zeros(rand_trunc_dist_mat.shape, dtype='int')
+                rand_trunc_dist_mat_bin[rand_trunc_dist_mat < dist_lim] = 1
+                # Record the number of interactions and store in close_num_rand in the index
+                # corresponding to both markers, for every permutation
+                close_num_rand[j, k, r] = np.sum(np.sum(rand_trunc_dist_mat_bin))
+    return close_num_rand
+
+
+def calculate_enrichment_stats(close_num, close_num_rand):
+    """Calculates z score and p values from spatial enrichment analysis.
+
+    Args:
+        close_num: marker x marker matrix with counts for cells
+            positive for corresponding markers
+        close_num_rand: random positive marker counts
+            for every permutation in the bootstrap
+
+    Returns:
+        z: z scores for corresponding markers
+        muhat: predicted mean values of close_num_rand random distribution
+        sigmahat: predicted standard deviation values of close_num_rand
+            random distribution
+        p: p values for corresponding markers, for both positive
+            and negative enrichment
+        h: matrix indicating whether
+            corresponding marker interactions are significant
+        adj_p: fdh_br adjusted p values"""
+    # Get the number of markers and number of permutations
+    marker_num = close_num.shape[0]
+    bootstrap_num = close_num_rand.shape[2]
+
+    # Create z, muhat, sigmahat, and p
+    z = np.zeros((marker_num, marker_num))
+    muhat = np.zeros((marker_num, marker_num))
+    sigmahat = np.zeros((marker_num, marker_num))
+    p_pos = np.zeros((marker_num, marker_num))
+    p_neg = np.zeros((marker_num, marker_num))
+
+    for j in range(0, marker_num):
+        for k in range(0, marker_num):
+            # Get close_num_rand value for every marker combination and reshape to use as input for norm fit
+            tmp = np.reshape(close_num_rand[j, k, :], (bootstrap_num, 1))
+            # Get muhat and sigmahat values for distribution from 100 permutations
+            (muhat[j, k], sigmahat[j, k]) = scipy.stats.norm.fit(tmp)
+            # Calculate z score based on distribution
+            z[j, k] = (close_num[j, k] - muhat[j, k]) / sigmahat[j, k]
+            # Calculate both positive and negative enrichment p values
+            p_pos[j, k] = (1 + (np.sum(tmp >= close_num[j, k]))) / (bootstrap_num + 1)
+            p_neg[j, k] = (1 + (np.sum(tmp <= close_num[j, k]))) / (bootstrap_num + 1)
+
+    # Get fdh_br adjusted p values
+    p_summary = np.zeros_like(p_pos[:, :])
+    for j in range(0, marker_num):
+        for k in range(0, marker_num):
+            # Use negative enrichment p values if the z score is negative, and vice versa
+            if z[j, k] > 0:
+                p_summary[j, k] = p_pos[j, k]
+            else:
+                p_summary[j, k] = p_neg[j, k]
+    (h, adj_p, aS, aB) = statsmodels.stats.multitest.multipletests(
+        p_summary, alpha=.05)
+
+    # Create an Xarray with the dimensions (stats variables, number of markers, number of markers)
+    stats_data = np.stack((z, muhat, sigmahat, p_pos, p_neg, h, adj_p), axis=0)
+    coords = [["z", "muhat", "sigmahat", "p_pos", "p_neg", "h", "p_adj"],
+              range(stats_data[0].data.shape[0]), range(stats_data[0].data.shape[1])]
+    dims = ["stats", "rows", "cols"]
+    stats_xr = xr.DataArray(stats_data, coords=coords, dims=dims)
+    return stats_xr
--- segmentation/utils/spatial_analysis_utils_test.py
+++ segmentation/utils/spatial_analysis_utils_test.py
@@ -28,92 +28,6 @@ def test_calc_dist_matrix():
 #     assert np.allclose(distmat, testmat)
 
 
-def make_distance_matrix(enrichment_type):
-    # Make a distance matrix for no enrichment, positive enrichment, and negative enrichment
-
-    if enrichment_type == "none":
-        # Create a 60 x 60 euclidian distance matrix of random values for no enrichment
-        rand_mat = np.random.randint(0, 200, size=(60, 60))
-        np.fill_diagonal(rand_mat, 0)
-        return rand_mat
-    elif enrichment_type == "positive":
-        # Create positive enrichment distance matrix where 10 cells mostly positive for marker 1
-        # are located close in proximity to 10 cells mostly positive for marker 2.
-        # Other included cells are not significantly positive for either marker and are located
-        # far from the two positive populations.
-        dist_mat_pos = np.zeros((80, 80))
-        dist_mat_pos[10:20, :10] = 50
-        dist_mat_pos[:10, 10:20] = 50
-        dist_mat_pos[20:40, :20] = 200
-        dist_mat_pos[:20, 20:40] = 200
-        dist_mat_pos[40:80, :40] = 300
-        dist_mat_pos[:40, 40:80] = 300
-        return dist_mat_pos
-    elif enrichment_type == "negative":
-        # This creates a distance matrix where there are two groups of cells significant for 2 different
-        # markers that are not located near each other (not within the dist_lim).
-        dist_mat_neg = np.zeros((60, 60))
-        dist_mat_neg[20:40, :20] = 300
-        dist_mat_neg[:20, 20:40] = 300
-        dist_mat_neg[40:50, :40] = 50
-        dist_mat_neg[:40, 40:50] = 50
-        dist_mat_neg[50:60, :50] = 200
-        dist_mat_neg[:50, 50:60] = 200
-        return dist_mat_neg
-
-
-def make_expression_matrix(enrichment_type):
-    # Create the expression matrix with cell labels and patient labels for no enrichment,
-    # positive enrichment, and negative enrichment.
-
-    # Column names for columns that are not markers (columns to be excluded)
-    excluded_colnames = {0: 'cell_size', 1: 'Background', 14: "HH3",
-                         23: "summed_channel", 24: "label", 25: "area", 26: "eccentricity",
-                         27: "major_axis_length", 28: "minor_axis_length", 29: "perimeter",
-                         30: "fov"}
-
-    if enrichment_type == "none":
-        all_patient_data = pd.DataFrame(np.zeros((60, 31)))
-        # Assigning values to the patient label and cell label columns
-        all_patient_data[30] = "Point8"
-        all_patient_data[24] = np.arange(len(all_patient_data[1])) + 1
-        # We create two populations of 20 cells each, each positive for a different marker (column index 2 and 3)
-        all_patient_data.iloc[0:20, 2] = 1
-        all_patient_data.iloc[20:40, 3] = 1
-        # Assign column names to columns not for markers (columns to be excluded)
-        all_patient_data = all_patient_data.rename(excluded_colnames, axis=1)
-        return all_patient_data
-    elif enrichment_type == "positive":
-        all_patient_data_pos = pd.DataFrame(np.zeros((80, 31)))
-        # Assigning values to the patient label and cell label columns
-        all_patient_data_pos[30] = "Point8"
-        all_patient_data_pos[24] = np.arange(len(all_patient_data_pos[1])) + 1
-        # We create 8 cells positive for column index 2, and 8 cells positive for column index 3.
-        # These are within the dist_lim in dist_mat_pos (positive enrichment distance matrix).
-        all_patient_data_pos.iloc[0:8, 2] = 1
-        all_patient_data_pos.iloc[10:18, 3] = 1
-        # We create 4 cells in column index 2 and column index 3 that are also positive
-        # for their respective markers.
-        all_patient_data_pos.iloc[28:32, 2] = 1
-        all_patient_data_pos.iloc[32:36, 3] = 1
-        # Assign column names to columns not for markers (columns to be excluded)
-        all_patient_data_pos = all_patient_data_pos.rename(excluded_colnames, axis=1)
-        return all_patient_data_pos
-    elif enrichment_type == "negative":
-        all_patient_data_neg = pd.DataFrame(np.zeros((60, 31)))
-        # Assigning values to the patient label and cell label columns
-        all_patient_data_neg[30] = "Point8"
-        all_patient_data_neg[24] = np.arange(len(all_patient_data_neg[1])) + 1
-        # We create two groups of 20 cells positive for marker 1 (in column index 2)
-        # and marker 2 (in column index 3) respectively.
-        # The two populations are not within the dist_lim in dist_mat_neg (negative enrichment distance matrix)
-        all_patient_data_neg.iloc[0:20, 2] = 1
-        all_patient_data_neg.iloc[20:40, 3] = 1
-        # Assign column names to columns not for markers (columns to be excluded)
-        all_patient_data_neg = all_patient_data_neg.rename(excluded_colnames, axis=1)
-        return all_patient_data_neg
-
-
 def make_threshold_mat():
     thresh = pd.DataFrame(np.zeros((20, 2)))
     thresh.iloc[:, 1] = .5
@@ -125,9 +39,7 @@ def make_example_data_closenum():
 
     # Create example all_patient_data cell expression matrix
     all_patient_data = pd.DataFrame(np.zeros((10, 31)))
-    # Assigning values to the patient label and cell label columns
-    all_patient_data[30] = "Point8"
-    all_patient_data[24] = np.arange(len(all_patient_data[1])) + 1
+
     # Create 4 cells positive for marker 1 and 2, 5 cells positive for markers 3 and 4,
     # and 1 cell positive for marker 5
     all_patient_data.iloc[0:4, 2] = 1
@@ -136,6 +48,10 @@ def make_example_data_closenum():
     all_patient_data.iloc[4:9, 6] = 1
     all_patient_data.iloc[9, 7] = 1
     all_patient_data.iloc[9, 8] = 1
+    
+    # Only include the columns of markers
+    data_markers = all_patient_data.drop(all_patient_data.columns[[
+        0, 1, 14, 23, 24, 25, 26, 27, 28, 29, 30]], axis=1)
 
     # Create the distance matrix to test the closenum function
     dist_mat = np.zeros((10, 10))
@@ -170,17 +86,14 @@ def make_example_data_closenum():
     dist_mat[8, 7] = 50
     dist_mat[7, 8] = 50
 
-    return all_patient_data, dist_mat
+    return data_markers, dist_mat
 
 
 def test_compute_close_cell_num():
     # Test the closenum function
-    example_all_patient_data, example_dist_mat = make_example_data_closenum()
+    data_markers, example_dist_mat = make_example_data_closenum()
     example_thresholds = make_threshold_mat()
 
-    # Subsets the expression matrix to only have marker columns
-    data_markers = example_all_patient_data.drop(example_all_patient_data.columns[[
-        0, 1, 14, 23, 24, 25, 26, 27, 28, 29, 30]], axis=1)
     # List of all markers
     marker_titles = data_markers.columns
     # Length of marker list
@@ -189,9 +102,8 @@ def test_compute_close_cell_num():
     # Subsetting threshold matrix to only include column with threshold values
     thresh_vec = example_thresholds.iloc[0:20, 1]
 
-    example_closenum, marker1_num, marker2_num = spatial_analysis.compute_close_cell_num(
-        data_markers, thresh_vec, example_dist_mat, marker_num,
-        dist_lim=100)
+    example_closenum, marker1_num, marker2_num = spatial_analysis_utils.compute_close_cell_num(
+        data_markers, thresh_vec, example_dist_mat, marker_num, dist_lim=100)
     assert (example_closenum[:2, :2] == 16).all()
     assert (example_closenum[3:5, 3:5] == 25).all()
     assert (example_closenum[5:7, 5:7] == 1).all()
@@ -213,14 +125,14 @@ def test_compute_close_cell_num():
 
 
 def test_compute_close_cell_num_random():
-    example_all_patient_data, example_distmat = make_example_data_closenum()
+    data_markers, example_distmat = make_example_data_closenum()
 
     # Generate random inputs to test shape
     marker1_num = [random.randrange(0, 10) for i in range(20)]
     marker2_num = [random.randrange(0, 5) for i in range(400)]
     marker_num = 20
 
-    example_closenumrand = spatial_analysis.compute_close_cell_num_random(
+    example_closenumrand = spatial_analysis_utils.compute_close_cell_num_random(
         marker1_num, marker2_num, example_distmat, marker_num, dist_lim=100,
         bootstrap_num=100)
 
@@ -237,7 +149,7 @@ def test_calculate_enrichment_stats():
     # Generate random closenumrand matrix, ensuring significant positive enrichment
     stats_cnrp = np.random.randint(1, 40, (20, 20, 100))
 
-    stats_xr_pos = spatial_analysis.calculate_enrichment_stats(stats_cnp, stats_cnrp)
+    stats_xr_pos = spatial_analysis_utils.calculate_enrichment_stats(stats_cnp, stats_cnrp)
 
     assert stats_xr_pos.loc["z", 0, 0] > 0
     assert stats_xr_pos.loc["p_pos", 0, 0] < .05
@@ -250,7 +162,7 @@ def test_calculate_enrichment_stats():
     # Generate random closenumrand matrix, ensuring significant negative enrichment
     stats_cnrn = np.random.randint(40, 80, (20, 20, 100))
 
-    stats_xr_neg = spatial_analysis.calculate_enrichment_stats(stats_cnn, stats_cnrn)
+    stats_xr_neg = spatial_analysis_utils.calculate_enrichment_stats(stats_cnn, stats_cnrn)
 
     assert stats_xr_neg.loc["z", 0, 0] < 0
     assert stats_xr_neg.loc["p_neg", 0, 0] < .05
@@ -264,45 +176,8 @@ def test_calculate_enrichment_stats():
     # Generate random closenumrand matrix, ensuring no enrichment
     stats_cnr = np.random.randint(78, 82, (20, 20, 100))
 
-    stats_xr = spatial_analysis.calculate_enrichment_stats(stats_cn, stats_cnr)
+    stats_xr = spatial_analysis_utils.calculate_enrichment_stats(stats_cn, stats_cnr)
 
     assert abs(stats_xr.loc["z", 0, 0]) < 1
     assert stats_xr.loc["p_neg", 0, 0] > .05
     assert stats_xr.loc["p_pos", 0, 0] > .05
-
-
-def test_calculate_channel_spatial_enrichment():
-    # Test z and p values
-
-    marker_thresholds = make_threshold_mat()
-
-    # Positive enrichment
-    all_patient_data_pos = make_expression_matrix("positive")
-    dist_mat_pos = make_distance_matrix("positive")
-    values, stats = \
-        spatial_analysis.calculate_channel_spatial_enrichment(
-            dist_mat_pos, marker_thresholds, all_patient_data_pos, bootstrap_num=100)
-    # z, muhat, sigmahat, p, h, adj_p, marker_titles = stats[0]
-    assert stats.loc["Point8", "p_pos", 2, 3] < .05
-    assert stats.loc["Point8", "p_neg", 2, 3] > .05
-    assert stats.loc["Point8", "z", 2, 3] > 0
-    # Negative enrichment
-    all_patient_data_neg = make_expression_matrix("negative")
-    dist_mat_neg = make_distance_matrix("negative")
-    values, stats = \
-        spatial_analysis.calculate_channel_spatial_enrichment(
-            dist_mat_neg, marker_thresholds, all_patient_data_neg, bootstrap_num=100)
-    # z, muhat, sigmahat, p, h, adj_p, marker_titles = stats[0]
-    assert stats.loc["Point8", "p_neg", 2, 3] < .05
-    assert stats.loc["Point8", "p_pos", 2, 3] > .05
-    assert stats.loc["Point8", "z", 2, 3] < 0
-    # No enrichment
-    all_patient_data = make_expression_matrix("none")
-    dist_mat = make_distance_matrix("none")
-    values, stats = \
-        spatial_analysis.calculate_channel_spatial_enrichment(
-            dist_mat, marker_thresholds, all_patient_data, bootstrap_num=100)
-    # z, muhat, sigmahat, p, h, adj_p, marker_titles = stats[0]
-    assert stats.loc["Point8", "p_pos", 2, 3] > .05
-    assert stats.loc["Point8", "p_pos", 2, 3] > .05
-    assert abs(stats.loc["Point8", "z", 2, 3]) < 2
