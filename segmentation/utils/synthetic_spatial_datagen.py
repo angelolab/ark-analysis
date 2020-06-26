8ab66ecd268a791f13473829854b0426a627967e
alex-l-kong <alkong@ucdavis.edu>
2020-06-26 14:15:41 -0700

    Add generate_labels func definition

--- segmentation/utils/synthetic_spatial_datagen.py
+++ segmentation/utils/synthetic_spatial_datagen.py
@@ -5,19 +5,37 @@ import skimage.measure
 import scipy
 import os
 
+
 def generate_labels(num_AB=1000, num_AC=1000):
 	"""
 	This function will generate a set of associated labels for each cell centroid.
 	A helper function to get_random_dist_matrix.
 	Return value will be a dictionary of mappings of the type cell_num: cell_label.
-	Cell label will be either 'A', 'B', or 'C'
+	Cell label will be either 'AB' or 'AC'
 
 	Args:
 		num_AB: the number of AB distances we wish to generate.
 		num_AC: the number of AC distances we wish to generate.
 	"""
 
-def get_random_dist_matrix(num_AB=1000, num_AC=1000, dist_AB=None, dist_AC=None):
+	# Generate a range of numbers the same length as the total of AB and AC labels desired
+	# And select a random set of indices to identify as AB connections
+	num_range = np.arange(num_AB + num_AC)
+	ab_indices = np.random.choice(num_range.size, num_AB, replace=False)
+
+	# Generate a 
+	ab_values = num_range[ab_indices]
+	ac_values = num_range[~ab_indices]
+
+	ab_dict = dict([(ab, 'AB') for ab in ab_values])
+	ac_dict = dict([(ac, 'AC') for ac in ac_values])
+
+	label_dict = {**ab_dict, **ac_dict}
+
+	return label_dict
+
+
+def get_random_dist_matrix(num_AB=1000, num_AC=1000, distr_AB=None, distr_AC=None):
 	"""
 	This function will return a random dist matrix such that the distance between cells
 	of types A and B are overall larger than the distance between cells of types A and C
@@ -33,7 +51,11 @@ def get_random_dist_matrix(num_AB=1000, num_AC=1000, dist_AB=None, dist_AC=None)
 	Args:
 		num_AB: the number of AB distances we wish to generate. Default 1000
 		num_AC: the number of AC distances we wish to generate. Default 1000
-		dist_AB: if specified, will be a tuple the mean and variance of the Gaussian distribution
+		distr_AB: if specified, will be a tuple the mean and variance of the Gaussian distribution
 			we wish to generate numbers from. If None, use the default values.
-		dist_AC: similar to dist_AB. Default will have a higher mean value.
+		distr_AC: similar to dist_AB. Default will have a higher mean value.
 	"""
+
+	labels = generate_labels(num_AB, num_AC)
+
+	return None
