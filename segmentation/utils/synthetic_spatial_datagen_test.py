bfce53e6e4abbe8e4c263a6db3827a91eadbfb50
alex-l-kong <alkong@ucdavis.edu>
2020-07-01 13:47:40 -0700

    Add function stubs for testing of synthetic data generation functions

---
+++ segmentation/utils/synthetic_spatial_datagen_test.py
@@ -0,0 +1,14 @@
+import numpy as np
+import xarray as xr
+import pandas as pd
+import skimage.measure
+import scipy
+import os
+from scipy.spatial.distance import cdist
+
+
+def test_direct_init_dist_matrix():
+    pass
+
+def test_point_init_dist_matrix():
+    pass
