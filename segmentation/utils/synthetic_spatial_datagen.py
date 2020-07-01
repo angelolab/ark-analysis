4199c8e7efd0693e4e1f94c653fc765b1a07f0bb
alex-l-kong <alkong@ucdavis.edu>
2020-07-01 12:58:36 -0700

    Finished debugging direct random matrix initialization; had to assure symmetry

--- segmentation/utils/synthetic_spatial_datagen.py
+++ segmentation/utils/synthetic_spatial_datagen.py
@@ -24,50 +24,51 @@ A_CENTROID_COV = [[1, 0], [0, 1]]
 B_CENTROID_COV = [[1, 0], [0, 1]]
 C_CENTROID_COV = [[1, 0], [0, 1]]
 
+### Leaving this function here in case we want random label generation for cells
 
-def generate_labels(num_A=100, num_B=100, num_C=100):
-    """
-    This function will generate a set of associated labels for each cell centroid.
+# def generate_labels(num_A=100, num_B=100, num_C=100):
+#     """
+#     This function will generate a set of associated labels for each cell centroid.
 
-    A helper function to get_random_dist_matrix.
+#     A helper function to get_random_dist_matrix.
 
-    Return value will be a list of values associated with AB and AC, as well as
-    a dictionary of mappings of the type cell_num: cell_label.
+#     Return value will be a list of values associated with AB and AC, as well as
+#     a dictionary of mappings of the type cell_num: cell_label.
 
-    Cell label will be either 'AB' or 'AC'
+#     Cell label will be either 'AB' or 'AC'
 
-    Args:
-        num_A: the number of A cells we wish to generate. Default 100.
-        num_B: the number of B cells we wish to generate. Default 100.
-        num_C: the number of C cells we wish to generate. Default 100.
-    """
+#     Args:
+#         num_A: the number of A cells we wish to generate. Default 100.
+#         num_B: the number of B cells we wish to generate. Default 100.
+#         num_C: the number of C cells we wish to generate. Default 100.
+#     """
 
-    # Generate a range of numbers the same length as the total of A, B, and C labels desired
-    # And select a random set of indices to identify as A cells
-    num_range = np.arange(num_A + num_B + num_C)
-    a_indices = np.random.choice(num_range.size, num_A, replace=False)
+#     # Generate a range of numbers the same length as the total of A, B, and C labels desired
+#     # And select a random set of indices to identify as A cells
+#     num_range = np.arange(num_A + num_B + num_C)
+#     a_indices = np.random.choice(num_range.size, num_A, replace=False)
 
-    # Get a indices
-    a_values = num_range[a_indices]
+#     # Get a indices
+#     a_values = num_range[a_indices]
 
-    # From remaining B and C cells, select a random set of indices for B cells
-    non_a_indices = num_range[~a_indices]
-    b_indices = np.random.choice(non_a_indices, num_B, replace=False)
+#     # From remaining B and C cells, select a random set of indices for B cells
+#     non_a_indices = num_range[~a_indices]
+#     b_indices = np.random.choice(non_a_indices, num_B, replace=False)
 
-    # Get b indices, set the remainder as c indices
-    b_indices = non_a_indices[b_indices]
-    c_indices = non_a_indices[c_indices]
+#     # Get b indices, set the remainder as c indices
+#     b_indices = non_a_indices[b_indices]
+#     c_indices = non_a_indices[c_indices]
 
-    a_dict = dict([(a, 'A') for a in a_values])
-    b_dict = dict([(b, 'B') for b in b_values])
-    c_dict = dict([(c, 'C') for c in c_values])
+#     a_dict = dict([(a, 'A') for a in a_values])
+#     b_dict = dict([(b, 'B') for b in b_values])
+#     c_dict = dict([(c, 'C') for c in c_values])
 
-    label_dict = {**a_dict, **b_dict, **c_dict}
+#     label_dict = {**a_dict, **b_dict, **c_dict}
 
-    return a_indices, b_indices, c_indices, label_dict
+#     return a_indices, b_indices, c_indices, label_dict
 
 
-def direct_init_dist_matrix(num_A=100, num_B=100, num_C=100, distr_AB=None, distr_AC=None):
+def direct_init_dist_matrix(num_A=100, num_B=100, num_C=100, distr_AB=None, distr_AC=None, seed=None):
     """
     This function will return a random dist matrix such that the distance between cells
     of types A and B are overall larger than the distance between cells of types A and C
@@ -87,72 +88,51 @@ def direct_init_dist_matrix(num_A=100, num_B=100, num_C=100, distr_AB=None, dist
         distr_AB: if specified, will be a dict listing the mean and variance of the Gaussian distribution
             we wish to generate numbers from. If None, use the default values.
         distr_AC: similar to dist_AB. Default will have a higher mean value.
+        seed: whether to fix the random seed or not. Useful for testing.
+            Should be a specified integer value. Default None.
     """
 
-    # generate a list of A, B, and C cells
-
-    # labels_a, labels_b, labels_c, dict_labels = generate_labels(num_A, num_B, num_C)
-
-    # initialize the distance matrix
-    # sample_dist_mat = np.zeros((num_A + num_B + num_C, num_A + num_B + num_C))
-
     # set the mean and variance of the Gaussian distributions of both AB and AC distances
-    if distr_AB = None:
+    if distr_AB == None:
         mean_ab = AB_DIST_MEAN
         var_ab = AB_DIST_VAR
     else:
         mean_ab = distr_AB['mean']
         var_ab = distr_AB['var']
 
-    if distr_AC = None:
+    if distr_AC == None:
         mean_ac = AC_DIST_MEAN
         var_ac = AC_DIST_VAR
     else:
         mean_ac = distr_AC['mean']
         var_ac = distr_AC['var']
 
-    # generate a random numpy matrix of Gaussian values from specified distribution
-    # and assign to corresponding labels_a, labels_b values
-    # random_ab = np.random.normal(mean_ab, var_ab, (num_A, num_B))
-    # random_ab = (random_ab + random_ab.T) / 2
-    # sample_dist_mat[labels_a, labels_b] = random_ab
-    # sample_dist_mat[labels_b, labels_a] = random_ab.T
+    # set random seed if set
+    if seed:
+        np.random.seed(seed)
 
-    # assert that the created submatrix is symmetric
-    # assert np.alclose(sample_dist_mat[labels_a, labels_b], sample_dist_mat[labels_a, labels_b].T, rtol=1e-05, atol=1e-08)
-
-    # follow the same steps for labels_a and labels_c
-    # random_ac = np.random.normal(mean_ac, var_ac, (num_A, num_C))
-    # random_ac = (random_ac + random_ac.T) / 2
-    # sample_dist_mat[labels_a, labels_c] = random_ac
-
-    # assert np.alclose(sample_dist_mat[labels_a, labels_c], sample_dist_mat[labels_a, labels_c].T, rtol=1e-05, atol=1e-08)
-
-    # generate a random matrix for each distance partition
-    # we only really care about ab and ac (and by extension, ba and ca)
-    # so we can just randomly generate the values in aa, bb, bc/cb, and cc
-    random_aa = np.random.normal(np.random.normal(0, 1, 1)[0], np.random.normal(0, 1, 1)[0], (num_A, num_A))
+    random_aa = np.random.normal(0, 1, (num_A, num_A))
     random_ab = np.random.normal(mean_ab, var_ab, (num_A, num_B))
     random_ac = np.random.normal(mean_ac, var_ac, (num_A, num_C))
-    random_bb = np.random.normal(np.random.normal(0, 1, 1)[0], np.random.normal(0, 1, 1)[0], (num_B, num_B))
-    random_bc = np.random.normal(np.random.normal(0, 1, 1)[0], np.random.normal(0, 1, 1)[0], (num_B, num_C))
-    random_cc = np.random.normal(np.random.normal(0, 1, 1)[0], np.random.normal(0, 1, 1)[0], (num_C, num_C))
+    random_bb = np.random.normal(0, 1, (num_B, num_B))
+    random_bc = np.random.normal(0, 1, (num_B, num_C))
+    random_cc = np.random.normal(0, 1, (num_C, num_C))
 
     # create each row one-by-one first
-    first_row = np.concatenate((random_aa, random_ab, random_ac), axis=1)
-    second_row = np.concatenate((random_ab.T, random_bb, random_bc), axis=1)
-    third_row = np.concatenate((random_ac.T, random_bc.T, random_cc), axis=1)
+    first_row = np.concatenate(((random_aa+random_aa.T)/2, random_ab, random_ac), axis=1)
+    second_row = np.concatenate((random_ab.T, (random_bb+random_bb.T)/2, random_bc), axis=1)
+    third_row = np.concatenate((random_ac.T, random_bc.T, (random_cc+random_cc.T)/2), axis=1)
 
     # then concatenate them together
     dist_mat = np.concatenate((first_row, second_row, third_row), axis=0)
 
     # assert that the created submatrix is symmetric
-    assert np.alclose(sample_dist_mat[labels_a, labels_b], sample_dist_mat[labels_a, labels_b].T, rtol=1e-05, atol=1e-08)
+    assert np.allclose(dist_mat, dist_mat.T, rtol=1e-05, atol=1e-08)
 
     return dist_mat# , dict_labels
 
 
-def get_random_centroid_centers(size_img=(1024, 1024), num_A=100, num_B=100, num_C=100, distr_A=None, distr_B=None, distr_C=None):
+def get_random_centroid_centers(size_img=(1024, 1024), num_A=100, num_B=100, num_C=100, distr_A=None, distr_B=None, distr_C=None, seed=None):
     """
     This function generates random centroid centers such that those of type A will have centers
     closer on average to those of type B than those of type C
@@ -178,26 +158,30 @@ def get_random_centroid_centers(size_img=(1024, 1024), num_A=100, num_B=100, num
     width = size_img[1]
 
     # set the distribution parameters, use the mean scaling factor and default covariance if not specified
-    if distr_A = None:
+    if distr_A == None:
         a_mean = (height * A_CENTROID_FACTOR, width * A_CENTROID_FACTOR)
         a_cov = A_CENTROID_COV
     else:
         mean_a = distr_A['mean']
         var_a = distr_A['cov']
 
-    if distr_B = None:
+    if distr_B == None:
         b_mean = (height * B_CENTROID_FACTOR, width * B_CENTROID_FACTOR)
         b_cov = B_CENTROID_COV
     else:
         mean_b = distr_B['mean']
         var_b = distr_B['cov']
 
-    if distr_C = None:
+    if distr_C == None:
         b_mean = (height * C_CENTROID_FACTOR, width * C_CENTROID_FACTOR)
         b_cov = C_CENTROID_COV
     else:
         mean_ac = distr_C['mean']
         var_ac = distr_C['var']
+
+    # if specified, set the random seed
+    if seed:
+        np.random.seed(seed)
 
     # use the multivariate_normal distribution to generate the points
     a_points = np.random.multivariate_normal(a_mean, a_cov, num_A)
@@ -228,6 +212,8 @@ def point_init_dist_matrix(size_img=(1024, 1024), num_A=100, num_B=100, num_C=10
             Used by get_random_centroid_centers. Default None, and will use predefined parameters.
         distr_B: dimilar to distr_A
         distr_C: similar to distr_C
+        seed: whether to fix the random seed or not. Useful for testing.
+            Should be a specified integer value. Default None.
     """
 
     # generate points for cell types A, B, and C
@@ -249,10 +235,9 @@ def point_init_dist_matrix(size_img=(1024, 1024), num_A=100, num_B=100, num_C=10
     third_row = np.concatenate((a_c_dist.T, b_c_dist.T, c_c_dist), axis=1)
 
     # and then the entire matrix
-    complete_mat = np.concatenate(first_row, second_row, third_row, axis=0)
+    dist_mat = np.concatenate(first_row, second_row, third_row, axis=0)
 
     # assert that the created submatrix is symmetric
-    assert np.alclose(sample_dist_mat[labels_a, labels_b], sample_dist_mat[labels_a, labels_b].T, rtol=1e-05, atol=1e-08)
-
+    assert np.allclose(dist_mat, dist_mat.T, rtol=1e-05, atol=1e-08)
 
     return complete_mat
