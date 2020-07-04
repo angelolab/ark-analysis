b6b7daf043261738e160b2bd64b430bae0196070
Jaiveer Singh <jaiveers.24@gmail.com>
2020-07-03 18:55:56 -0700

    fixed neighborhood matrix

--- segmentation/utils/spatial_analysis_test.py
+++ segmentation/utils/spatial_analysis_test.py
@@ -294,10 +294,10 @@ def test_create_neighborhood_matrix():
     dist_mat_pos = make_distance_matrix("positive")
 
     counts, freqs = spatial_analysis.create_neighborhood_matrix(all_data_pos, dist_mat_pos, distlim=50)
-    assert (counts.loc[:9, 2] == 8).all()
-    assert (counts.loc[10:19, 3] == 8).all()
+    assert (counts.loc[:9, 3] == 8).all()
+    assert (counts.loc[10:19, 2] == 8).all()
     assert ((counts.loc[20:39, 2:3] == 4).all()).all()
 
-    assert (counts.loc[80:89, 3] == 8).all()
-    assert (counts.loc[90:99, 2] == 8).all()
+    assert (counts.loc[80:89, 2] == 8).all()
+    assert (counts.loc[90:99, 3] == 8).all()
     assert ((counts.loc[100:119, 2:3] == 4).all()).all()
