f1f9deacb937aa80a41924b7565d17ac97cd65ba
alex-l-kong <alkong@ucdavis.edu>
2020-07-06 13:42:19 -0700

    Remove point init dist matrix test function since direct_init_dist_matrix will be enough to test the results for that

--- segmentation/utils/synthetic_spatial_datagen_test.py
+++ segmentation/utils/synthetic_spatial_datagen_test.py
@@ -35,26 +35,3 @@ def test_direct_init_dist_matrix():
     # this may not eliminate the possibility that the null is proved true
     # but it's definitely a great check that can ensure greater success
     assert sample_dist_mat[:100, 100:200].mean() > sample_dist_mat[:100, 200:].mean()
-
-
-def test_point_init_dist_matrix():
-    # this function tests the functionality of a random initialization of a
-    # distance matrix from a random selection of central points.
-    # the tests are fairly basic for now but will be expanded on.
-    # for now, we just want to ensure two things:
-        # matrix symmetry: a property of distance matrices
-        # mean AB > mean AC: the mean distance between cells of types A and B
-        # is greater than that of types A and C
-
-    # we'll be using the default parameters provided in the functions
-    # except for the random seed, which we specify
-    sample_dist_mat = synthetic_spatial_datagen.point_init_dist_matrix(seed=RSEED)
-
-    # assert matrix symmetry
-    assert np.allclose(sample_dist_mat, sample_dist_mat.T, rtol=1e-05, atol=1e-08)
-
-    # assert the average of the distance between A and B is greater
-    # than the average of the distance between A and C.
-    # this may not eliminate the possibility that the null is proved true
-    # but it's definitely a great check that can ensure greater success
-    assert sample_dist_mat[:100, 100:200].mean() > sample_dist_mat[:100, 200:].mean()
