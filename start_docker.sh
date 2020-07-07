003b81fcbedcc76a7ca755587bf2c28a60adc195
Noah Greenwald <noahfgreenwald@gmail.com>
2020-07-07 16:52:31 -0700

    Mount code to docker image (#116)
    * install segmentation within docker
    
    * add package to travis install
    
    * typo
    
    * specify correct folder name
    
    * explicit folder construction
    
    * global path
    
    * directory already exists
    
    * add back pip install
    
    * Remove USER flag from docker image

--- start_docker.sh
+++ start_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker run -it \
   -p 8888:8888 \
+  -v $PWD/segmentation:/usr/local/lib/python3.6/site-packages/segmentation \
   -v $PWD/scripts:/scripts \
   -v $PWD/data:/data \
-  $USER/segmentation:latest
+  segmentation:latest
