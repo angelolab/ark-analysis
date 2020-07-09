e78a8949b7e86ab5c68943bb3df991532caba688
Kevin <kevinwang0324@gmail.com>
2020-07-09 12:49:18 -0700

    added pandas

--- segmentation/utils/visualize_test.py
+++ segmentation/utils/visualize_test.py
@@ -2,6 +2,8 @@ import numpy as np
 import random
 from segmentation.utils import visualize
 import importlib
+import pandas as pd
+
 importlib.reload(visualize)
 
 
@@ -11,13 +13,14 @@ def test_visualize_z_scores():
     # Assign random phenotype titles
     pheno_titles = [chr(i) for i in range(ord('a'), ord('z') + 1)]
     plot = visualize.visualize_z_scores(z, pheno_titles)
-    
+
+
 def test_visualize_cells():
     rand_type = []
     ids = []
-    for x in range(0,1000):
-        rand_type.append(chr(random.randint(0,25) + 97))
-        ids.append(random.randint(1,5))
+    for x in range(0, 1000):
+        rand_type.append(chr(random.randint(0, 25) + 97))
+        ids.append(random.randint(1, 5))
     print(rand_type, ids)
     rand_dict = {"PatientID": ids, "cell_type": rand_type}
     df = pd.DataFrame.from_dict(rand_dict)
@@ -25,3 +28,5 @@ def test_visualize_cells():
     visualize.visualize_cell_distribution_in_all_patients(df, "cell_type")
     visualize.visualize_distribution_of_cell_count(df, "PatientID", "cell_type")
     visualize.visualize_proportion_of_cell_count(df, "PatientID", "cell_type")
+
+test_visualize_cells()
