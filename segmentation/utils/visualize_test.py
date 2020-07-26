1a7cd4b3c9f5271de4054f8d788524a2292055cd
Kevin <kevinwang0324@gmail.com>
2020-07-26 09:42:39 -0700

    added whitespace

--- segmentation/utils/visualize_test.py
+++ segmentation/utils/visualize_test.py
@@ -8,13 +8,18 @@ import string
 importlib.reload(visualize)
 
 def test_visualize_z_scores():
+
+
     # Create random Z score
     z = np.random.uniform(low=-5, high=5, size=(26, 26))
     # Assign random phenotype titles
     pheno_titles = [chr(i) for i in range(ord('a'), ord('z') + 1)]
     plot = visualize.visualize_z_scores(z, pheno_titles)
 
+
 def test_visualize_cells():
+
+    
     rand_type = random.choices(string.ascii_lowercase, k=100)
     ids = random.choices(range(1,3), k=100)
 
