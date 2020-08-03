cb9ef2300cd8efea0b05c22067b2c3e0bf4621de
ackagel <kagelosaurus@gmail.com>
2020-08-03 21:19:27 +0000

    demolishable scripts

--- start_docker.sh
+++ start_docker.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+if [ -d "$PWD/scripts" ]
+then
+  for f in "$PWD"/.templates/*.ipynb
+  do
+    name=$(basename "$f")
+    cp -- "$f" "$PWD/scripts/updated_$name"
+  done
+else
+  mkdir "$PWD/scripts"
+  cp "$PWD"/.templates/*.ipynb "$PWD/scripts/."
+fi
+
 docker run -it \
   -p 8888:8888 \
   -v "$PWD/segmentation:/usr/local/lib/python3.6/site-packages/segmentation" \
