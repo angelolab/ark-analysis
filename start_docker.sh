6d901468088b4ea624b1b99f1e253b83a9df24a8
ackagel <kagelosaurus@gmail.com>
2020-08-03 18:30:13 +0000

    fixed start_docker.sh folder-space issue

--- start_docker.sh
+++ start_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run -it \
   -p 8888:8888 \
-  -v $PWD/segmentation:/usr/local/lib/python3.6/site-packages/segmentation \
-  -v $PWD/scripts:/scripts \
-  -v $PWD/data:/data \
+  -v "$PWD/segmentation:/usr/local/lib/python3.6/site-packages/segmentation" \
+  -v "$PWD/scripts:/scripts" \
+  -v "$PWD/data:/data" \
   segmentation:latest
