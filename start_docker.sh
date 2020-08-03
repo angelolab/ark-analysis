5ca6d7fcc2bbda720cec5221a6cae43868a1f467
ackagel <kagelosaurus@gmail.com>
2020-08-03 23:50:07 +0000

    fleshed out cases in start_docker.sh

--- start_docker.sh
+++ start_docker.sh
@@ -4,10 +4,21 @@ then
   for f in "$PWD"/templates/*.ipynb
   do
     name=$(basename "$f")
-    DIFF=$(diff "$f" "$PWD/scripts/$name")
-    if [ "$DIFF" != "" ]
+    DIFF=$(diff "$f" "$PWD/scripts/$name" 2>/dev/null)
+    if [ $? -ne 0 ]
+    then
+      echo "$name was not found.  Creating new file $name in scripts"
+      cp -- "$f" "$PWD/scripts/$name"
+    elif [ "$DIFF" != "" ]
     then
       echo "Changes have been made to $name; adding updated version as updated_$name"
+      echo "Please add relevant changes from updated_$name into $name and remove updated_$name"
+      echo "updated_$name will be overwritten in future updates"
+      if [ -f "$PWD/scripts/updated_$name" ]
+      then
+        echo "WARNING: The file updated_$name is being overwritten..."
+        rm "$PWD/scripts/updated_$name"
+      fi
       cp -- "$f" "$PWD/scripts/updated_$name"
     fi
   done
