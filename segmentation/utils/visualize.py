937dd8234a8920bfe93021feac6a80978d928ae0
Kevin <kevinwang0324@gmail.com>
2020-08-04 01:02:10 -0700

    Added docs, made more concise, also optional save plot

--- segmentation/utils/visualize.py
+++ segmentation/utils/visualize.py
@@ -18,61 +18,73 @@ def visualize_z_scores(z, pheno_titles):
     sns.clustermap(zplot, figsize=(8, 8), cmap="vlag")
 
 
-def swap_columns(df, c1, c2):
-    df['temp'] = df[c1]
-    df[c1] = df[c2]
-    df[c2] = df['temp']
-    df.drop(columns=['temp'], inplace=True)
-    return df
+def getSortedData(cell_data, patient_col_name, population_col_name, is_normalized=False):
+    """Gets the cell data and generates a new Sorted DataFrame with each row representing a patient and column representing Population categories
+                  Args:
+                      cell_data: Pandas Dataframe containing columns with Patient ID and Cell Name
+                      patient_col_name: Name of column containing categorical Patient data
+                      population_col_name: Name of column in dataframe containing categorical Population data
+                      is_normalized: Boolean specifying whether to normalize cell counts or not, default is False
+                  Outputs:
+                      cell_data_stacked: DataFrame with rows and columns sorted by population"""
+    ids = cell_data[patient_col_name]
+    names = cell_data[population_col_name].unique().tolist()
+    unique_ids = cell_data[patient_col_name].unique()
 
-
-def getSortedDf(df, patient_col_name, population_col_name, isNormalized=False):
-    ids = df[patient_col_name]
-    names = df[population_col_name].value_counts().index.tolist()
-    unique_ids = df[patient_col_name].unique()
-
-    df_stacked = pd.DataFrame()
-    if isNormalized:
-        df_stacked = pd.crosstab(df[patient_col_name], df[population_col_name], normalize='index')
+    cell_data_stacked = pd.DataFrame()
+    if is_normalized:
+        cell_data_stacked = pd.crosstab(cell_data[patient_col_name], cell_data[population_col_name], normalize='index')
     else:
-        df_stacked = pd.crosstab(df[patient_col_name], df[population_col_name])
+        cell_data_stacked = pd.crosstab(cell_data[patient_col_name], cell_data[population_col_name])
 
     # Sorts by Kagel Method
-    id_order = df.groupby(patient_col_name).count().sort_values(by=population_col_name, ascending=False).index.values
-    pop_order = df.groupby(population_col_name).count().sort_values(by=patient_col_name, ascending=False).index.values
-    df_stacked = df_stacked.reindex(id_order, axis='index')
-    df_stacked = df_stacked.reindex(pop_order, axis='columns')
+    id_order = cell_data.groupby(patient_col_name).count().sort_values(by=population_col_name, ascending=False).index.values
+    pop_order = cell_data.groupby(population_col_name).count().sort_values(by=patient_col_name, ascending=False).index.values
+    cell_data_stacked = cell_data_stacked.reindex(id_order, axis='index')
+    cell_data_stacked = cell_data_stacked.reindex(pop_order, axis='columns')
 
-    return df_stacked
+    return cell_data_stacked
 
 
-def visualize_patient_population_distribution(df, patient_col_name, population_col_name, color_map="jet"):
+def visualize_patient_population_distribution(cell_data, patient_col_name, population_col_name, color_map="jet",
+                                              show_total_count=True, show_distribution=True, show_proportion=True, save_dir=""):
     """Plots the distribution of the population given by total count, direct count, and proportion
               Args:
-                  df: Pandas Dataframe containing columns with Patient ID and Cell Name
+                  cell_data: Pandas Dataframe containing columns with Patient ID and Cell Name
                   patient_col_name: Name of column containing categorical Patient data
                   population_col_name: Name of column in dataframe containing categorical Population data
-                  color_map: Name of MatPlotLib ColorMap used, default is jet"""
-    df = df.dropna()
+                  color_map: Name of MatPlotLib ColorMap used, default is jet
+                  show_total_count: Boolean specifying whether to show graph of total population count, default is true
+                  show_distribution: Boolean specifying whether to show graph of population distribution, default is true
+                  show_proportion: Boolean specifying whether to show graph of total count, default is true
+                  save_dir: Directory to save plots, default is blank"""
+    cell_data = cell_data.dropna()
 
     # Plot by total count
-    population_values = df[population_col_name].value_counts()
-    population_values.plot.bar(colormap=color_map)
-    plt.title("Distribution of Population in all patients")
-    plt.xlabel("Population Type")
-    plt.ylabel("Population Count")
+    if show_total_count:
+        population_values = cell_data[population_col_name].value_counts()
+        population_values.plot.bar(colormap=color_map)
+        plt.title("Distribution of Population in all patients")
+        plt.xlabel("Population Type")
+        plt.ylabel("Population Count")
+        plt.savefig(save_dir+"TotalPopulationDistribution.png")
 
     # Plot by count
-    getSortedDf(df, patient_col_name, population_col_name).plot.bar(stacked=True, colormap=color_map)
-    plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
-    plt.xlabel("Patient ID")
-    plt.ylabel("Cell Count")
-    plt.title("Distribution of Population Count in Patients")
+    if show_distribution:
+        getSortedData(cell_data, patient_col_name, population_col_name).plot.bar(stacked=True, colormap=color_map)
+        plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+        plt.xlabel("Patient ID")
+        plt.ylabel("Cell Count")
+        plt.title("Distribution of Population Count in Patients")
+        plt.savefig(save_dir+"PopulationDistribution.png")
 
     # Plot by Proportion
-    getSortedDf(df, patient_col_name, population_col_name, isNormalized=True).plot.bar(stacked=True, legend=False,
-                                                                                       colormap=color_map)
-    plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
-    plt.xlabel("Patient ID")
-    plt.ylabel("Population Proportion")
-    plt.title("Distribution of Population Count Proportion in Patients")
+    if show_proportion:
+        getSortedData(cell_data, patient_col_name, population_col_name, is_normalized=True).plot.bar(stacked=True,
+                                                                                                     legend=False,
+                                                                                                     colormap=color_map)
+        plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+        plt.xlabel("Patient ID")
+        plt.ylabel("Population Proportion")
+        plt.title("Distribution of Population Count Proportion in Patients")
+        plt.savefig(save_dir+"PopulationProportion.png")
