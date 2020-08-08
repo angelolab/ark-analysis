08d3f023e6b0016948ddf15d841583c2ba06dde0
ackagel <kagelosaurus@gmail.com>
2020-08-08 09:16:10 +0000

    added io_utils test function skeletons

---
+++ segmentation/utils/io_utils_test.py
@@ -0,0 +1,18 @@
+import tempfile
+
+from segmentation.utils import io_utils as iou
+
+
+def test_list_files():
+
+    return
+
+
+def test_extract_delimited_names():
+
+    return
+
+
+def test_list_folders():
+
+    return
