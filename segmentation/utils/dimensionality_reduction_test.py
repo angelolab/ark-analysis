efd7c939c169c19f906e82b503e96d9082fb37b5
Kevin <kevinwang0324@gmail.com>
2020-08-09 22:46:44 -0700

    testing all 3 algorithms now

--- segmentation/utils/dimensionality_reduction_test.py
+++ segmentation/utils/dimensionality_reduction_test.py
@@ -9,8 +9,12 @@ importlib.reload(dimensionality_reduction)
 
 
 def test_dimensionality_reduction():
-    random_cell_data = pd.DataFrame(np.random.random(size=(600, 8)), columns=list('ABCDEFGH'))
-    random_cell_data["cell_type"] = rand.choices(string.ascii_lowercase, k=600)
-    dimensionality_reduction.visualize_dimensionality_reduction(random_cell_data,
-                                                                ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], "cell_type",
-                                                                algorithm="PCA", save_dir="")
+    random_cell_data = pd.DataFrame(np.random.random(size=(300, 8)), columns=list('ABCDEFGH'))
+    random_cell_data["cell_type"] = rand.choices(string.ascii_lowercase, k=300)
+
+    test_algorithms = ['PCA', 'tSNE', 'UMAP']
+    for alg in test_algorithms:
+        dimensionality_reduction.visualize_dimensionality_reduction(random_cell_data,
+                                                                    ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
+                                                                    "cell_type",
+                                                                    algorithm=alg, save_dir="")
