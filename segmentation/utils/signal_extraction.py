b8bffe8a8a9c8685ede20cc9071081f95b49bf7b
alex-l-kong <alkong@ucdavis.edu>
2020-08-11 09:47:41 -0700

    Add function stubs for ones to implement

--- segmentation/utils/signal_extraction.py
+++ segmentation/utils/signal_extraction.py
@@ -1,16 +1,27 @@
 import numpy as np
 
+
 # TODO: work on weighted extraction and implement other discussed techniques of extraction
-def simple_weighted_extraction(cell_coords, image_data):
+def positive_pixels(cell_coords, image_data):
     """
-    Define a weighted extraction technique based on how close the coordinate is to the center
+    Extract channel counts by summing over the number of non-zero pixels in the cell,
+    improves on default_extraction by not distinguishing between pixel expression values
 
     Args:
-        cell_coords: tuples of values representing pixels within one cell
-        image_data: array containing channel counts
+        cell_coords (tuple): tuples of values representing pixels within one cell
+        image_data (numpy): array containing channel counts
 
     Returns:
-        np.array: sum of counts for each channel
+        channel_counts (numpy): sum of counts for each channel
+    """
+
+    pass
+
+
+def center_weighting(cell_coords, image_data):
+    """
+    Extract channel counts by summing over weighted expression values based on distance from center,
+    improves upon default extraction by including a level of certainty/uncertainty
     """
 
     pass
@@ -21,11 +32,11 @@ def default_extraction(cell_coords, image_data):
     Extract channel counts for an individual cell via basic summation for each channel
 
     Args:
-        cell_coords: tuples of values representing pixels within one cell
-        image_data: array containing channel counts
+        cell_coords (tuple): tuples of values representing pixels within one cell
+        image_data (numpy): array containing channel counts
 
     Returns:
-        np.array: sum of counts for each channel
+        channel_counts (numpy): sum of counts for each channel
     """
 
     # transpose coords so they can be used to index an array
