5ed7e6f0fb88b4cec9754ca3e72ca286546938cc
Kevin <kevinwang0324@gmail.com>
2020-08-10 18:54:53 -0700

    added tempfile and randomized saving

--- segmentation/utils/dimensionality_reduction_test.py
+++ segmentation/utils/dimensionality_reduction_test.py
@@ -4,6 +4,7 @@ import string
 import random as rand
 from segmentation.utils import dimensionality_reduction
 import importlib
+import tempfile
 
 importlib.reload(dimensionality_reduction)
 
@@ -13,8 +14,11 @@ def test_dimensionality_reduction():
     random_cell_data["cell_type"] = rand.choices(string.ascii_lowercase, k=300)
 
     test_algorithms = ['PCA', 'tSNE', 'UMAP']
-    for alg in test_algorithms:
-        dimensionality_reduction.visualize_dimensionality_reduction(random_cell_data,
-                                                                    ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
-                                                                    "cell_type",
-                                                                    algorithm=alg, save_dir="")
+    with tempfile.TemporaryDirectory() as temp_dir:
+        for alg in test_algorithms:
+            if rand.random() > 0.50:
+                dimensionality_reduction.visualize_dimensionality_reduction(random_cell_data, ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
+                                                   "cell_type", algorithm=alg, save_dir="")
+            else:
+                dimensionality_reduction.visualize_dimensionality_reduction(random_cell_data, ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
+                                                   "cell_type", algorithm=alg)
