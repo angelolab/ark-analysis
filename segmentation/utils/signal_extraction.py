655f0e4395d0e0cb4c1244d407a12a23449aa2be
alex-l-kong <alkong@ucdavis.edu>
2020-08-11 11:22:18 -0700

    Add signal extraction functions desired

--- segmentation/utils/signal_extraction.py
+++ segmentation/utils/signal_extraction.py
@@ -2,29 +2,82 @@ import numpy as np
 
 
 # TODO: work on weighted extraction and implement other discussed techniques of extraction
-def positive_pixels(cell_coords, image_data):
+def positive_pixels_extraction(cell_coords, image_data):
     """
     Extract channel counts by summing over the number of non-zero pixels in the cell,
     improves on default_extraction by not distinguishing between pixel expression values
 
     Args:
-        cell_coords (tuple): tuples of values representing pixels within one cell
-        image_data (numpy): array containing channel counts
+        cell_coords (numpy): values representing pixels within one cell
+        image_data (xarray): array containing channel counts
 
     Returns:
         channel_counts (numpy): sum of counts for each channel
     """
 
-    pass
+    # transpose coords so they can be used to index an array
+    cell_coords = cell_coords.T
+    channel_values = image_data.values[tuple(cell_coords)]
+
+    # sum up based on a binary mask that is 1 if the expression value > 0 else 0
+    channel_counts = np.sum(channel_values > 0, axis=0)
+
+    return channel_counts
 
 
-def center_weighting(cell_coords, image_data):
+def center_weighting_extraction(cell_coords, image_data):
     """
     Extract channel counts by summing over weighted expression values based on distance from center,
     improves upon default extraction by including a level of certainty/uncertainty
+
+    Args:
+        cell_coords (numpy): values representing pixels within one cell
+        image_data (xarray): array containing channel counts
+
+    Returns:
+        channel_counts (numpy): sum of counts for each channel
     """
 
-    pass
+    # create a weighting matrix, we will assign the highest value at the center
+    # and decrease by 1 each layer we move away from it
+    # I'm going to be assume we get square matrices in image_data
+    center = int(image_data.shape[0] / 2)
+
+    # this will ensure we never get zero or negative weighting values in our weight matrix
+    center_weight = center + 1
+
+    # now build the weight matrix using ogrid
+    image_row_index, image_col_index = np.ogrid[:image_data.shape[0], :image_data.shape[1]]
+    weight_matrix = center_weight - np.maximum(np.abs(image_row_index - center), np.abs(image_col_index - center))
+
+    # TODO: the center of even-length arrays is not a single pixel which currently causes
+    # minor signal irregularities at different layers
+    # ex. in a 4 x 4 matrix the center should be:
+    # 0 0 0 0
+    # 0 X X 0
+    # 0 X X 0
+    # 0 0 0 0
+
+    # and not:
+    # 0 0 0 0
+    # 0 0 0 0
+    # 0 0 X 0
+    # 0 0 0 0
+    # as would be defined without the correction by the regular signal weighting algorithm
+    # the easy way to fix this is to use skimage.draw.rectangle
+
+    # center the weight matrix by center_weight
+    weight_matrix = weight_matrix / center_weight
+
+    # transpose coords so they can be used to index an array
+    cell_coords = cell_coords.T
+    channel_values = image_data.values[tuple(cell_coords)]
+    weight_values = np.expand_dims(weight_matrix[tuple(cell_coords)], axis=0)
+
+    # multiply channel_values by weight_values and then sum across channels
+    channel_counts = np.sum(np.multiply(channel_values, weight_values.T), axis=0)
+
+    return channel_counts
 
 
 def default_extraction(cell_coords, image_data):
@@ -32,8 +85,8 @@ def default_extraction(cell_coords, image_data):
     Extract channel counts for an individual cell via basic summation for each channel
 
     Args:
-        cell_coords (tuple): tuples of values representing pixels within one cell
-        image_data (numpy): array containing channel counts
+        cell_coords (numpy): values representing pixels within one cell
+        image_data (xarray): array containing channel counts
 
     Returns:
         channel_counts (numpy): sum of counts for each channel
--- segmentation/utils/signal_extraction_test.py
+++ segmentation/utils/signal_extraction_test.py
@@ -4,17 +4,54 @@ import xarray as xr
 from segmentation.utils import signal_extraction
 
 
+def test_positive_pixels_extraction():
+    # this function tests the functionality of positive pixels extraction
+    # where we count the number of non-zero values for each channel
+
+    # create a series of constant arrays
+    base_channel = np.zeros((10, 10))
+
+    combined_channels = np.stack((base_channel, base_channel + 1, base_channel + 2), axis=-1)
+    combined_channels = xr.DataArray(combined_channels)
+
+    coords = np.stack((np.arange(4), np.arange(4)), axis=-1)
+
+    channel_counts = signal_extraction.positive_pixels_extraction(cell_coords=coords,
+                                                                  image_data=combined_channels)
+
+    assert np.all(channel_counts == [0, 4, 4])
+
+
+def test_center_weighting_extraction():
+    # this function tests the functionality of center weighting extraction
+    # where we add a weighting scheme with more confidence toward the center
+    # before summing across each channel
+
+    # create a series of constant arrays
+    base_channel = np.ones((10, 10))
+
+    combined_channels = np.stack((base_channel, base_channel * 2, base_channel * 4), axis=-1)
+    combined_channels = xr.DataArray(combined_channels)
+
+    coords = np.stack((np.arange(6), np.arange(6)), axis=-1)
+
+    channel_counts = signal_extraction.center_weighting_extraction(cell_coords=coords,
+                                                                   image_data=combined_channels)
+
+    assert np.all(channel_counts == [3.5, 7., 14.])
+
+
 def test_default_extraction():
+    # this function tests the functionality of default weighting extraction
+    # where we just sum across each channel
+
     # create a series of constant arrays
     base_channel = np.ones((10, 10))
 
     combined_channels = np.stack((base_channel, base_channel * 2, base_channel * 5), axis=-1)
     combined_channels = xr.DataArray(combined_channels)
 
-    coords = np.array([[0, 0],
-                       [1, 1],
-                       [2, 2],
-                       [3, 3]])
+    coords = np.stack((np.arange(4), np.arange(4)), axis=-1)
 
     channel_counts = signal_extraction.default_extraction(cell_coords=coords,
                                                           image_data=combined_channels)
