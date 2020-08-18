e81995f4ba4e27b452e415c2d85f8ee931319ff8
ackagel <kagelosaurus@gmail.com>
2020-08-18 00:38:46 +0000

    removed updated_ script naming system

--- start_docker.sh
+++ start_docker.sh
@@ -47,18 +47,11 @@ then
         echo "$name was not found.  Creating new file $name in scripts"
         cp -- "$f" "$PWD/scripts/$name"
 
-      # if no difference, add updated file
+      # if difference, add updated file
       elif [ "$DIFF" != "" ]
       then
-        echo "Changes have been made to $name; adding updated version as updated_$name"
-        echo "Please add relevant changes from updated_$name into $name and remove updated_$name"
-        echo "updated_$name will be overwritten in future updates"
-        if [ -f "$PWD/scripts/updated_$name" ]
-        then
-          echo "WARNING: The file updated_$name is being overwritten..."
-          rm "$PWD/scripts/updated_$name"
-        fi
-        cp -- "$f" "$PWD/scripts/updated_$name"
+        echo "WARNING: The file $name is being overwritten..."
+        cp -- "$f" "$PWD/scripts/$name"
       fi
     done
   else
