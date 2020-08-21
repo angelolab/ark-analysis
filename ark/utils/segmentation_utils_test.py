1f03fd628aa2041255e271cbcee87f75ed875565
William Xu <williamxu132@gmail.com>
2020-08-20 17:49:39 -0700

    updated segmentation_utils and segmentation_utils_test

--- ark/utils/segmentation_utils_test.py
+++ ark/utils/segmentation_utils_test.py
@@ -1,9 +1,35 @@
 import numpy as np
 import xarray as xr
-
+import os.path
+from os import path
 from skimage.measure import regionprops
+import tempfile
 
 from ark.utils import segmentation_utils
+
+
+def _generate_channel_xr(fov_num=2, chan_num=5):
+    fovs = ["fov" + str(i) for i in range(fov_num)]
+    channels = ["channel" + str(i) for i in range(chan_num)]
+    output = np.random.randint(0, 20, len(fovs) * 50 * 50 * len(channels))
+    output = output.reshape((len(fovs), 50, 50, len(channels)))
+
+    output_xr = xr.DataArray(output, coords=[fovs, range(50), range(50), channels],
+                             dims=["fovs", "rows", "cols", "channels"])
+
+    return output_xr
+
+
+def _generate_deepcell_ouput(fov_num=2):
+    fovs = ["fov" + str(i) for i in range(fov_num)]
+    models = ["pixelwise_interior", "watershed_inner", "watershed_outer",
+              "fgbg_foreground", "pixelwise_sum"]
+    output = np.random.rand(len(fovs) * 50 * 50 * len(models))
+    output = output.reshape((len(fovs), 50, 50, len(models)))
+
+    output_xr = xr.DataArray(output, coords=[fovs, range(50), range(50), models],
+                             dims=["fovs", "rows", "cols", "models"])
+    return output_xr
 
 
 def _create_test_extraction_data():
@@ -167,3 +193,14 @@ def test_transform_expression_matrix_multiple_compartments():
 
 
 def visualize_watershed_test():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        model_output = _generate_deepcell_ouput()
+        channel_xr = _generate_channel_xr()
+        cell_mask, channel_data = _create_test_extraction_data()
+        overlay_channels = [channel_data.channels.values[:2]],
+        saved_output = xr.load_dataarray(os.path.join(temp_dir, 'segmentation_labels.xr'))
+        for fov in range(saved_output.shape[0]):
+            segmentation_utils.visualize_watershed_transform(model_output=model_output, channel_xr=channel_xr,
+                                                                          overlay_channels=overlay_channels,
+                                                                          output_dir="", fov=fov)
+            assert path.exists("{}_segmentation_labels.tiff")
