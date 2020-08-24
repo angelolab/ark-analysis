5d142320117487278ccf4bb2fe6c5ac37482bc14
ackagel <kagelosaurus@gmail.com>
2020-08-24 18:14:16 +0000

    line length fixes in segmentation + some docstring revions

--- ark/segmentation/signal_extraction_test.py
+++ ark/segmentation/signal_extraction_test.py
@@ -23,41 +23,51 @@ def test_positive_pixels_extraction():
 
     # generate sample segmentation mask and channel data
     sample_segmentation_mask, sample_channel_data = \
-        synthetic_spatial_datagen.generate_two_cell_test_channel_synthetic_data(size_img=size_img,
-                                                                                cell_radius=cell_radius,
-                                                                                nuc_radius=nuc_radius,
-                                                                                memb_thickness=memb_thickness,
-                                                                                nuc_signal_strength=nuc_signal_strength,
-                                                                                memb_signal_strength=memb_signal_strength,
-                                                                                nuc_uncertainty_length=nuc_uncertainty_length,
-                                                                                memb_uncertainty_length=memb_uncertainty_length)
+        synthetic_spatial_datagen.generate_two_cell_test_channel_synthetic_data(
+            size_img=size_img,
+            cell_radius=cell_radius,
+            nuc_radius=nuc_radius,
+            memb_thickness=memb_thickness,
+            nuc_signal_strength=nuc_signal_strength,
+            memb_signal_strength=memb_signal_strength,
+            nuc_uncertainty_length=nuc_uncertainty_length,
+            memb_uncertainty_length=memb_uncertainty_length
+        )
 
     # extract the cell regions for cells 1 and 2
     coords_1 = np.argwhere(sample_segmentation_mask == 1)
     coords_2 = np.argwhere(sample_segmentation_mask == 2)
 
     # test default extraction (threshold == 0)
-    channel_counts_1 = signal_extraction.positive_pixels_extraction(cell_coords=coords_1,
-                                                                    image_data=xr.DataArray(sample_channel_data))
+    channel_counts_1 = signal_extraction.positive_pixels_extraction(
+        cell_coords=coords_1,
+        image_data=xr.DataArray(sample_channel_data)
+    )
 
-    channel_counts_2 = signal_extraction.positive_pixels_extraction(cell_coords=coords_2,
-                                                                    image_data=xr.DataArray(sample_channel_data))
+    channel_counts_2 = signal_extraction.positive_pixels_extraction(
+        cell_coords=coords_2,
+        image_data=xr.DataArray(sample_channel_data)
+    )
 
     # note that for cell 2 it's higher because of membrane-level expression
     assert np.all(channel_counts_1 == [25, 0])
     assert np.all(channel_counts_2 == [0, 236])
 
-    # test with new threshold == 10 (because that's what we label nuclear signal for the time being)
+    # test with new threshold == 10 (that's what we currently label nuclear signal)
     # nuclear signal extraction should now be 0
     test_threshold = 10
 
-    channel_counts_1 = signal_extraction.positive_pixels_extraction(cell_coords=coords_1,
-                                                                    image_data=xr.DataArray(sample_channel_data),
-                                                                    threshold=test_threshold)
+    channel_counts_1 = signal_extraction.positive_pixels_extraction(
+        cell_coords=coords_1,
+        image_data=xr.DataArray(sample_channel_data),
+        threshold=test_threshold
+    )
 
-    channel_counts_2 = signal_extraction.positive_pixels_extraction(cell_coords=coords_2,
-                                                                    image_data=xr.DataArray(sample_channel_data),
-                                                                    threshold=test_threshold)
+    channel_counts_2 = signal_extraction.positive_pixels_extraction(
+        cell_coords=coords_2,
+        image_data=xr.DataArray(sample_channel_data),
+        threshold=test_threshold
+    )
 
     assert np.all(channel_counts_1 == [0, 0])
     assert np.all(channel_counts_2 == [0, 236])
@@ -80,14 +90,16 @@ def test_center_weighting_extraction():
 
     # generate sample segmentation mask and channel data
     sample_segmentation_mask, sample_channel_data = \
-        synthetic_spatial_datagen.generate_two_cell_test_channel_synthetic_data(size_img=size_img,
-                                                                                cell_radius=cell_radius,
-                                                                                nuc_radius=nuc_radius,
-                                                                                memb_thickness=memb_thickness,
-                                                                                nuc_signal_strength=nuc_signal_strength,
-                                                                                memb_signal_strength=memb_signal_strength,
-                                                                                nuc_uncertainty_length=nuc_uncertainty_length,
-                                                                                memb_uncertainty_length=memb_uncertainty_length)
+        synthetic_spatial_datagen.generate_two_cell_test_channel_synthetic_data(
+            size_img=size_img,
+            cell_radius=cell_radius,
+            nuc_radius=nuc_radius,
+            memb_thickness=memb_thickness,
+            nuc_signal_strength=nuc_signal_strength,
+            memb_signal_strength=memb_signal_strength,
+            nuc_uncertainty_length=nuc_uncertainty_length,
+            memb_uncertainty_length=memb_uncertainty_length
+        )
 
     # extract the cell regions for cells 1 and 2
     coords_1 = np.argwhere(sample_segmentation_mask == 1)
@@ -102,19 +114,27 @@ def test_center_weighting_extraction():
     coords_1 = region_info[0].coords
     coords_2 = region_info[1].coords
 
-    channel_counts_1_center_weight = signal_extraction.center_weighting_extraction(cell_coords=coords_1,
-                                                                                   image_data=xr.DataArray(sample_channel_data),
-                                                                                   centroid=centroid_1)
+    channel_counts_1_center_weight = signal_extraction.center_weighting_extraction(
+        cell_coords=coords_1,
+        image_data=xr.DataArray(sample_channel_data),
+        centroid=centroid_1
+    )
 
-    channel_counts_2_center_weight = signal_extraction.center_weighting_extraction(cell_coords=coords_2,
-                                                                                   image_data=xr.DataArray(sample_channel_data),
-                                                                                   centroid=centroid_2)
+    channel_counts_2_center_weight = signal_extraction.center_weighting_extraction(
+        cell_coords=coords_2,
+        image_data=xr.DataArray(sample_channel_data),
+        centroid=centroid_2
+    )
 
-    channel_counts_1_base_weight = signal_extraction.default_extraction(cell_coords=coords_1,
-                                                                        image_data=xr.DataArray(sample_channel_data))
+    channel_counts_1_base_weight = signal_extraction.default_extraction(
+        cell_coords=coords_1,
+        image_data=xr.DataArray(sample_channel_data)
+    )
 
-    channel_counts_2_base_weight = signal_extraction.default_extraction(cell_coords=coords_2,
-                                                                        image_data=xr.DataArray(sample_channel_data))
+    channel_counts_2_base_weight = signal_extraction.default_extraction(
+        cell_coords=coords_2,
+        image_data=xr.DataArray(sample_channel_data)
+    )
 
     # assert that the nuclear signal for cell 1 is lower for weighted than for base
     # same for membrane signal for cell 2
@@ -144,24 +164,30 @@ def test_default_extraction():
 
     # generate sample segmentation mask and channel data
     sample_segmentation_mask, sample_channel_data = \
-        synthetic_spatial_datagen.generate_two_cell_test_channel_synthetic_data(size_img=size_img,
-                                                                                cell_radius=cell_radius,
-                                                                                nuc_radius=nuc_radius,
-                                                                                memb_thickness=memb_thickness,
-                                                                                nuc_signal_strength=nuc_signal_strength,
-                                                                                memb_signal_strength=memb_signal_strength,
-                                                                                nuc_uncertainty_length=nuc_uncertainty_length,
-                                                                                memb_uncertainty_length=memb_uncertainty_length)
+        synthetic_spatial_datagen.generate_two_cell_test_channel_synthetic_data(
+            size_img=size_img,
+            cell_radius=cell_radius,
+            nuc_radius=nuc_radius,
+            memb_thickness=memb_thickness,
+            nuc_signal_strength=nuc_signal_strength,
+            memb_signal_strength=memb_signal_strength,
+            nuc_uncertainty_length=nuc_uncertainty_length,
+            memb_uncertainty_length=memb_uncertainty_length
+        )
 
     # extract the cell regions for cells 1 and 2
     coords_1 = np.argwhere(sample_segmentation_mask == 1)
     coords_2 = np.argwhere(sample_segmentation_mask == 2)
 
-    channel_counts_1 = signal_extraction.default_extraction(cell_coords=coords_1,
-                                                            image_data=xr.DataArray(sample_channel_data))
+    channel_counts_1 = signal_extraction.default_extraction(
+        cell_coords=coords_1,
+        image_data=xr.DataArray(sample_channel_data)
+    )
 
-    channel_counts_2 = signal_extraction.default_extraction(cell_coords=coords_2,
-                                                            image_data=xr.DataArray(sample_channel_data))
+    channel_counts_2 = signal_extraction.default_extraction(
+        cell_coords=coords_2,
+        image_data=xr.DataArray(sample_channel_data)
+    )
 
     # note that for cell 2 it's higher because of membrane-level expression
     assert np.all(channel_counts_1 == [250, 0])
