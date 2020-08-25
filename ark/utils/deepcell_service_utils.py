b74ca5203891eb6ff8d2de05145d92e84395c93d
omerbartal <omer234@gmail.com>
2020-08-25 10:23:48 +0300

    Use kiosk-client modules for automated DeepCell tasks

--- ark/utils/deepcell_service_utils.py
+++ ark/utils/deepcell_service_utils.py
@@ -1,12 +1,11 @@
 from twisted.internet import reactor
 from kiosk_client import manager, utils
 import os
-import sys
 import glob
 import zipfile
 
 
-def run_deepcell_task(input_dir, output_dir, point, host='https://deepcell.org', job_type='multiplex'):
+def run_deepcell_task(input_filepath, output_dir, host='https://deepcell.org', job_type='multiplex'):
     """Uses kiosk-client to run DeepCell task and extracts zip to output folder.
         (https://github.com/vanvalenlab/kiosk-client)
 
@@ -27,14 +26,8 @@ def run_deepcell_task(input_dir, output_dir, point, host='https://deepcell.org',
         'download_results': True
     }
 
-    # will only work for the current project structure
-    input_path = input_dir[3:] if input_dir.startswith('../') else input_dir
-
-    # build absolute path
-    filename = os.path.join(os.path.dirname(__file__), os.path.join(sys.path[1], input_path) + point + '.tif.zip')
-
     mgr = manager.BatchProcessingJobManager(**mgr_kwargs)
-    mgr.run(filepath=filename)
+    mgr.run(filepath=input_filepath)
     reactor.run()
 
     # extract .zip file from /downloads to output_path
@@ -42,7 +35,7 @@ def run_deepcell_task(input_dir, output_dir, point, host='https://deepcell.org',
     dw_path = os.path.join(utils.get_download_path(), '*.zip')
     list_of_files = glob.glob(dw_path)
 
-    # TODO get the relevant filename instead of simply taking the last
+    # TODO get the relevant filename instead of simply taking the last [requires kiosk-client modification]
     last_modified_file = max(list_of_files, key=os.path.getctime)
 
     with zipfile.ZipFile(last_modified_file, "r") as z:
