239fda6da11d1c519a59254848cf4ded8592baa4
ackagel <kagelosaurus@gmail.com>
2020-08-27 21:24:35 +0000

    paired xr, tiff generation added to test_utils

--- ark/utils/test_utils.py
+++ ark/utils/test_utils.py
@@ -1,7 +1,9 @@
 import os
 import numpy as np
+import xarray as xr
 import skimage.io as io
 
+from mibidata import mibi_image as mi, tiff
 
 # required metadata for mibitiff writing (barf)
 METADATA = {
@@ -16,6 +18,109 @@ METADATA = {
     'filename': '20180703_1234_test', 'description': 'test image',
     'version': 'alpha',
 }
+
+
+def _gen_tif_data(fov_number, chan_number, img_shape, fills, dtype):
+    if fills is None:
+        tif_data = np.random.randint(0, 100,
+                                     fov_number * img_shape[0] * img_shape[1] * chan_number)
+        tif_data = tif_data.reshpae((fov_number, *img_shape, chan_number)).astype(dtype)
+    else:
+        tif_data = np.full((fov_number, *img_shape, chan_number), fills, dtype=dtype)
+
+    return tif_data
+
+
+def _create_tifs(base_dir, fov_names, img_names, shape, sub_dir, fills, dtype):
+    tif_data = _gen_tif_data(len(fov_names), len(img_names), shape, fills, dtype)
+
+    if sub_dir is None:
+        sub_dir = ""
+
+    filelocs = {}
+
+    for i, fov in enumerate(fov_names):
+        filelocs[fov] = []
+        fov_path = os.path.join(base_dir, fov, sub_dir)
+        os.makedirs(fov_path)
+        for j, name in enumerate(img_names):
+            io.imsave(os.path.join(base_dir, name), tif_data[i, :, :, j])
+            filelocs[fov].append(os.path.join(base_dir, fov, sub_dir, name))
+
+    return filelocs, tif_data
+
+
+def _create_multitiff(base_dir, fov_names, channel_names, shape, sub_dir, fills, dtype):
+    tif_data = _gen_tif_data(len(fov_names), len(channel_names), shape, fills, dtype)
+
+    filelocs = {}
+
+    for i, fov in enumerate(fov_names):
+        tiffpath = os.path.join(base_dir, f'{fov}.tiff')
+        io.imsave(tiffpath, tif_data[i, :, :, :], plugin='tifffile')
+        filelocs[fov] = tiffpath
+
+    return filelocs, tif_data
+
+
+def _create_mibitiff(base_dir, fov_names, channel_names, shape, sub_dir, fills, dtype):
+    tif_data = _gen_tif_data(len(fov_names), len(channel_names), shape, fills, dtype)
+
+    filelocs = {}
+
+    mass_map = tuple(enumerate(channel_names, 1))
+
+    for i, fov in enumerate(fov_names):
+        tif_obj = mi.MibiImage(tif_data[i, :, :, :],
+                               mass_map,
+                               **METADATA)
+
+        tiffpath = os.path.join(base_dir, f'{fov}.tiff')
+        tiff.write(tiffpath, tif_obj, dtype=dtype)
+        filelocs[fov] = tiffpath
+
+    return filelocs, tif_data
+
+
+TIFFMAKERS = {
+    'tiff': _create_tifs,
+    'multitiff': _create_multitiff,
+    'mibitiff': _create_mibitiff,
+}
+
+
+def create_paired_xarray_fovs(base_dir, fov_names, channel_names, img_shape=(1024, 1024),
+                              mode='tiff', sub_dir=None, fills=None, dtype="int8"):
+
+    if not os.path.isdir(base_dir):
+        raise FileNotFoundError(f'{base_dir} is not a directory')
+        return None
+
+    if channel_names is None or channel_names is []:
+        raise ValueError('No image names were given...')
+        return None
+
+    if not isinstance(channel_names, list):
+        channel_names = [channel_names]
+
+    if fills is not None and not isinstance(fills, list):
+        fills = [fills]
+
+    if fills is not None and (len(fills) != len(channel_names)):
+        raise ValueError('channel_names and fills must be the same length')
+        return None
+
+    filelocs, tif_data = TIFFMAKERS[mode](base_dir, fov_names, channel_names, img_shape, sub_dir,
+                                          fills, dtype)
+
+    data_xr = xr.DataArray(tif_data,
+                           coords=[fov_names,
+                                   range(img_shape[0]),
+                                   range(img_shape[1]),
+                                   channel_names],
+                           dims=["fovs", "rows", "cols", "channels"])
+
+    return filelocs, data_xr
 
 
 def _create_img_dir(temp_dir, fovs, imgs, img_sub_folder="TIFs", dtype="int8"):
