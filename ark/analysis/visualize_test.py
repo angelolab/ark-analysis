750665c78d7e15bd5b8b8df9aac960e37702904a
alex-l-kong <31424707+alex-l-kong@users.noreply.github.com>
2020-09-13 16:47:00 -0700

    Create boxplot function in visualize (#218)
    * Add boxplot viz function
    
    * Add test function for draw_boxplot and change save_dir functionality so it actually acts like a save_dir
    
    * Fix leftover PYCODESTYLE errors
    
    * Increase coverage with error tests, and fix split_vals check
    
    * PYCODESTYLE
    
    * Add new changes, including demo of draw_boxplot
    
    * Fix visualize cells test and add plot barchart tests
    
    * Finish up visualization cleanup, and adding creation of viz directory in notebook
    
    * Fix documentation for get_sorted_data
    
    * Change column to visualize boxplot for
    
    * Make get_sorted_data args and variable names more clear for documentation purposes

--- ark/analysis/visualize_test.py
+++ ark/analysis/visualize_test.py
@@ -1,8 +1,53 @@
+import os
 import numpy as np
-from os import path
+import pytest
+import tempfile
 
 from ark.analysis import visualize
 from ark.utils import test_utils
+
+
+def test_draw_boxplot():
+    # trim random data so we don't have to visualize as many facets
+    random_data = test_utils.make_segmented_csv(100)
+    random_data = random_data[random_data['PatientID'].isin(np.arange(1, 5))]
+
+    # basic error testing
+    with pytest.raises(ValueError):
+        # non-existant col_name
+        visualize.draw_boxplot(cell_data=random_data, col_name="AA")
+
+    with pytest.raises(ValueError):
+        # split_vals specified but not col_split
+        visualize.draw_boxplot(cell_data=random_data, col_name="A", split_vals=[])
+
+    with pytest.raises(ValueError):
+        # split_vals not found in col_split found
+        visualize.draw_boxplot(cell_data=random_data, col_name="A",
+                               col_split="PatientID", split_vals=[3, 4, 5, 6])
+
+    with pytest.raises(ValueError):
+        # trying to save to a non-existant directory
+        visualize.draw_boxplot(cell_data=random_data, col_name="A",
+                               save_dir="bad_dir")
+
+    # most basic visualization: just data and a column name
+    with tempfile.TemporaryDirectory() as temp_dir:
+        visualize.draw_boxplot(cell_data=random_data, col_name="A", save_dir=temp_dir)
+        assert os.path.exists(os.path.join(temp_dir, "boxplot_viz.png"))
+
+    # next level up: data, a column name, and a split column
+    with tempfile.TemporaryDirectory() as temp_dir:
+        visualize.draw_boxplot(cell_data=random_data, col_name="A",
+                               col_split="PatientID", save_dir=temp_dir)
+        assert os.path.exists(os.path.join(temp_dir, "boxplot_viz.png"))
+
+    # highest level: data, a column name, a split column, and split vals
+    with tempfile.TemporaryDirectory() as temp_dir:
+        visualize.draw_boxplot(cell_data=random_data, col_name="A",
+                               col_split="PatientID", split_vals=[1, 2],
+                               save_dir=temp_dir)
+        assert os.path.exists(os.path.join(temp_dir, "boxplot_viz.png"))
 
 
 def test_visualize_z_scores():
@@ -10,7 +55,16 @@ def test_visualize_z_scores():
     z = np.random.uniform(low=-5, high=5, size=(26, 26))
     # Assign random phenotype titles
     pheno_titles = [chr(i) for i in range(ord('a'), ord('z') + 1)]
-    visualize.visualize_z_scores(z, pheno_titles)
+
+    with pytest.raises(ValueError):
+        # trying to save on a non-existant directory
+        visualize.visualize_z_scores(z, pheno_titles, save_dir="bad_dir")
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        visualize.visualize_z_scores(z, pheno_titles, save_dir=temp_dir)
+
+        # check if correct plot is saved
+        assert os.path.exists(os.path.join(temp_dir, "z_score_viz.png"))
 
 
 def test_get_sort_data():
@@ -21,12 +75,34 @@ def test_get_sort_data():
     assert list(reversed(row_sums)) == sorted(row_sums)
 
 
+def test_plot_barchart():
+    # mostly error checking here, test_visualize_cells tests the meat of the functionality
+    random_data = test_utils.make_segmented_csv(100)
+
+    with pytest.raises(ValueError):
+        # trying to save to a non-existant directory
+        visualize.plot_barchart(random_data, "Random Title", "Random X Label",
+                                "Random Y Label", save_dir="bad_dir")
+
+    with pytest.raises(ValueError):
+        # setting save_dir but not setting save_file
+        visualize.plot_barchart(random_data, "Random Title", "Random X Label",
+                                "Random Y Label", save_dir=".")
+
+
 def test_visualize_cells():
     random_data = test_utils.make_segmented_csv(100)
-    visualize.visualize_patient_population_distribution(random_data, "PatientID", "cell_type",
-                                                        save_dir="")
 
-    # Check if correct plots are saved
-    assert path.exists("PopulationDistribution.png")
-    assert path.exists("TotalPopulationDistribution.png")
-    assert path.exists("PopulationProportion.png")
+    with pytest.raises(ValueError):
+        # trying to save to a non-existant directory
+        visualize.visualize_patient_population_distribution(random_data, "PatientID",
+                                                            "cell_type", save_dir="bad_dir")
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        visualize.visualize_patient_population_distribution(random_data, "PatientID",
+                                                            "cell_type", save_dir=temp_dir)
+
+        # Check if correct plots are saved
+        assert os.path.exists(os.path.join(temp_dir, "PopulationDistribution.png"))
+        assert os.path.exists(os.path.join(temp_dir, "TotalPopulationDistribution.png"))
+        assert os.path.exists(os.path.join(temp_dir, "PopulationProportion.png"))
