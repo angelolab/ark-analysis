7df4cfe9360daa52c56502ffb21502c9974332e0
alex-l-kong <31424707+alex-l-kong@users.noreply.github.com>
2020-09-17 13:18:43 -0700

    Bump version number to 0.2.4 (prepare initial deployment) (#227)
    * Bump version number to 1.0.0
    
    * Make version num a constant for easy updating, and add download_uri to setup call so that users can actually download the tools for our repo
    
    * Downgrade version number to 0.2.4, and add more classifier information

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from os import path
 from setuptools import setup, find_packages
 
+VERSION = '0.2.4'
+
 # set a long description which is basically the README
 with open(path.join(path.abspath(path.dirname(__file__)), 'README.md')) as f:
     long_description = f.read()
 
 setup(
     name='ark',
-    version='0.2.3',
+    version=VERSION,
     packages=find_packages(),
     license='Modified Apache License 2.0',
     description='Toolbox for analysis on segmented images from MIBI',
     author='Angelo Lab',
     url='https://github.com/angelolab/ark-analysis',
+    download_url='https://github.com/angelolab/ark-analysis/archive/{}.tar.gz'.format(VERSION),
     install_requires=['h5py',
                       'jupyter',
                       'jupyter_contrib_nbextensions',
@@ -36,5 +39,8 @@ setup(
     },
     long_description=long_description,
     long_description_content_type='text/markdown',
-    classifiers=['Programming Language :: Python :: 3.6']
+    classifiers=['License :: OSI Approved :: Apache Software License',
+                 'Development Status :: 4 - Beta',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.6']
 )
