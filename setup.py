7d0b258bc872b281b8505330041b9697a421db83
alex-l-kong <31424707+alex-l-kong@users.noreply.github.com>
2020-09-17 10:43:07 -0700

    Add PyPI deployment (#224)
    * Add PyPI deployment to travis.yml (and for those using PyCover and the coverage package, a line in .gitignore to prevent .coverage files from being added)
    
    * Add pip cache to .travis.yml
    
    * Try re-deploying with new encryption key
    
    * Remove tag requirement, and for the time being, release from pypi_setup branch
    
    * See if adding skip_cleanup option will fix
    
    * Remove cleanup (seems like this does nothing)
    
    * Try adding skip_cleanup back
    
    * Remove a comment from conf.py (mostly to see if Travis will finally deploy to PyPI)
    
    * Try with just the plain ol' token
    
    * Test deployment with a test API code hard-coded (WARNING: do NOT use with the real key)
    
    * Try adding a build distribution
    
    * Try quoting token to see if that makes a difference
    
    * Try unquoting __token__
    
    * Try setting config branch parameter to all
    
    * Try config parameters with username and password
    
    * Travis CI says: releases will never be triggered from a PR, reverting back to on: master...
    
    * Make sure tags must be present on PyPI build, still hanging...
    
    * Add travis_retry in attempt to fix deployment
    
    * Fix a damn tab error in the .travis.yml (if this is the error I've been trying to hunt down this whole time...)
    
    * NOW try to deploy from pypi_setup
    
    * Well, looks like we cannot deploy off of a PR lol
    
    * Forgot a damn colon
    
    * OK, see if removing if: tag IS present
    
    * OK, try adding if: tag is present again
    
    * OK, removing if tag IS true for good
    
    * Update setup.py for initial deployment params, and remove irrelevant ipykernel and duplicate pytest from requirements.txt
    
    * Change some config vars
    
    Co-authored-by: Noah Greenwald <noahfgreenwald@gmail.com>

--- setup.py
+++ setup.py
@@ -1,5 +1,40 @@
+from os import path
 from setuptools import setup, find_packages
-setup(name='segmentation-mibi',
-      version='0.2.3',
-      packages=find_packages(),
-      )
+
+# set a long description which is basically the README
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='ark',
+    version='0.2.3',
+    packages=find_packages(),
+    license='Modified Apache License 2.0',
+    description='Toolbox for analysis on segmented images from MIBI',
+    author='Angelo Lab',
+    url='https://github.com/angelolab/ark-analysis',
+    install_requires=['h5py',
+                      'jupyter',
+                      'jupyter_contrib_nbextensions',
+                      'matplotlib',
+                      'git+git://github.com/ionpath/mibilib@v1.2.6',
+                      'netCDF4',
+                      'numpy',
+                      'pandas',
+                      'scikit-image',
+                      'sciit-learn',
+                      'scipy',
+                      'seaborn',
+                      'statsmodels',
+                      'tables',
+                      'umap-learn',
+                      'xarray'],
+    extras_require={
+        'tests': ['pytest',
+                  'pytest-cov',
+                  'pytest-pycodestyle']
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=['Programming Language :: Python :: 3.6']
+)
