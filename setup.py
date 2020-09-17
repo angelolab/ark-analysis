797bb9cfde9a30e3e208f14855ca6ddc2a809223
alex-l-kong <alkong@ucdavis.edu>
2020-09-17 11:25:48 -0700

    Make version num a constant for easy updating, and add download_uri to setup call so that users can actually download the tools for our repo

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from os import path
 from setuptools import setup, find_packages
 
+VERSION = '1.0.0'
+
 # set a long description which is basically the README
 with open(path.join(path.abspath(path.dirname(__file__)), 'README.md')) as f:
     long_description = f.read()
 
 setup(
     name='ark',
-    version='1.0.0',
+    version=VERSION,
     packages=find_packages(),
     license='Modified Apache License 2.0',
     description='Toolbox for analysis on segmented images from MIBI',
     author='Angelo Lab',
     url='https://github.com/angelolab/ark-analysis',
+    download_url='https://github.com/angelolab/ark-analysis/archive/{}.tar.gz'.format(VERSION),
     install_requires=['h5py',
                       'jupyter',
                       'jupyter_contrib_nbextensions',
