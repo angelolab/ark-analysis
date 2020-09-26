8859a731eec0ad18d974e2553bb1f70b03bb9afe
omerbartal <omer234@gmail.com>
2020-09-26 14:03:59 +0300

    test multiple .tif

--- ark/utils/deepcell_service_utils_test.py
+++ ark/utils/deepcell_service_utils_test.py
@@ -8,12 +8,15 @@ from ark.utils.deepcell_service_utils import create_deepcell_output
 
 
 def mocked_run_deepcell(input_dir, output_dir, host, job_type):
-    pathlib.Path(os.path.join(output_dir, 'example_point_feature_0.tif')).touch()
+    pathlib.Path(os.path.join(output_dir, 'example_point1_feature_0.tif')).touch()
+    pathlib.Path(os.path.join(output_dir, 'example_point2_feature_0.tif')).touch()
 
     zip_path = os.path.join(output_dir, 'example_output.zip')
     with ZipFile(zip_path, 'w') as zipObj:
-        filename = os.path.join(output_dir, 'example_point_feature_0.tif')
-        zipObj.write(filename, os.path.basename(filename))
+        for i in [1, 2]:
+            filename = os.path.join(output_dir, f'example_point{i}_feature_0.tif')
+            zipObj.write(filename, os.path.basename(filename))
+            os.remove(filename)
 
 
 def test_create_deepcell_output(mocker):
@@ -25,16 +28,19 @@ def test_create_deepcell_output(mocker):
 
         output_dir = os.path.join(temp_dir, 'output_dir')
         os.makedirs(output_dir)
-        pathlib.Path(os.path.join(input_dir, 'example_point.tif')).touch()
+        pathlib.Path(os.path.join(input_dir, 'example_point1.tif')).touch()
+        pathlib.Path(os.path.join(input_dir, 'example_point2.tif')).touch()
+        pathlib.Path(os.path.join(input_dir, 'example_point3.tif')).touch()
 
         create_deepcell_output(deepcell_input_dir=input_dir, deepcell_output_dir=output_dir,
-                               points=['example_point'])
+                               points=['example_point1', 'example_point2'])
 
         # make sure DeepCell (.zip) output exists
         assert os.path.exists(os.path.join(output_dir, 'example_output.zip'))
 
         # DeepCell output .zip file should be extracted
-        assert os.path.exists(os.path.join(output_dir, 'example_point_feature_0.tif'))
+        assert os.path.exists(os.path.join(output_dir, 'example_point1_feature_0.tif'))
+        assert os.path.exists(os.path.join(output_dir, 'example_point2_feature_0.tif'))
 
         # /points.zip file should be removed from input folder after completion
         assert not os.path.isfile(os.path.join(input_dir, 'points.zip'))
@@ -43,14 +49,17 @@ def test_create_deepcell_output(mocker):
         # Warning should be displayed if /points.zip file exists (will be overwritten)
         with pytest.warns(UserWarning):
             create_deepcell_output(deepcell_input_dir=input_dir, deepcell_output_dir=output_dir,
-                                   points=['example_point'])
+                                   points=['example_point1'])
 
         # DeepCell output .tif file does not exist for some point
         with pytest.warns(UserWarning):
             create_deepcell_output(deepcell_input_dir=input_dir, deepcell_output_dir=output_dir,
-                                   suffix='_other_suffix', points=['example_point'])
+                                   suffix='_other_suffix', points=['example_point1'])
+        with pytest.warns(UserWarning):
+            create_deepcell_output(deepcell_input_dir=input_dir, deepcell_output_dir=output_dir,
+                                   points=['example_point1', 'example_point2', 'example_point3'])
 
         # ValueError should be raised if .tif file does not exists for some point in points
         with pytest.raises(ValueError):
             create_deepcell_output(deepcell_input_dir=input_dir, deepcell_output_dir=output_dir,
-                                   points=['example_point', 'other_point'])
+                                   points=['example_point1', 'other_point'])
