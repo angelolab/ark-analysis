72c6f92dabd2753af897674759667d865c6216c4
alex-l-kong <alkong@ucdavis.edu>
2020-10-14 14:27:17 -0700

    Add initial pass of new config parameters to install requirements as well as ark-analysis on a pip install

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from os import path
+from distutils.command.build_ext import build_ext as DistUtilsBuildExt
 from setuptools import setup, find_packages
 
-VERSION = '0.2.6'
+VERSION = '0.3.6'
 
 
 # define a parsing function for requirements.txt
@@ -22,6 +23,39 @@ try:
 except Exception:
     install_reqs = []
 
+
+# this mess is needed for numpy to build properly and to install the requirements
+class BuildExtension(setuptools.Command):
+    description = DistUtilsBuildExt.description
+    user_options = DistUtilsBuildExt.user_options
+    boolean_options = DistUtilsBuildExt.boolean_options
+    help_options = DistUtilsBuildExt.help_options
+
+    def __init__(self, *args, **kwargs):
+        from setuptools.command.build_ext import build_ext as SetupToolsBuildExt
+
+        # Bypass __setatrr__ to avoid infinite recursion.
+        self.__dict__['_command'] = SetupToolsBuildExt(*args, **kwargs)
+
+    def __getattr__(self, name):
+        return getattr(self._command, name)
+
+    def __setattr__(self, name, value):
+        setattr(self._command, name, value)
+
+    def initialize_options(self, *args, **kwargs):
+        return self._command.initialize_options(*args, **kwargs)
+
+    def finalize_options(self, *args, **kwargs):
+        ret = self._command.finalize_options(*args, **kwargs)
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+        return ret
+
+    def run(self, *args, **kwargs):
+        return self._command.run(*args, **kwargs)
+
+
 setup(
     name='ark-analysis',
     version=VERSION,
@@ -31,6 +65,7 @@ setup(
     author='Angelo Lab',
     url='https://github.com/angelolab/ark-analysis',
     download_url='https://github.com/angelolab/ark-analysis/archive/v{}.tar.gz'.format(VERSION),
+    cmdclass={'build_ext': BuildExtension},
     install_requires=install_reqs,
     extras_require={
         'tests': ['pytest',
