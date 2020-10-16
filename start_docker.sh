5742d2623cf9a7024672975bc57dd21fd5cd257a
alex-l-kong <alkong@ucdavis.edu>
2020-10-16 11:38:18 -0700

    Initial try for Docker (not working...)

--- start_docker.sh
+++ start_docker.sh
@@ -3,9 +3,20 @@
 # catch and pass update flag to notebook update routine
 bash update_notebooks.sh "$@"
 
+# find lowest open port available
+PORT=8888
+
+until [[ $(lsof -i -P -n | grep 127.0.0.1:$PORT | wc -l) -eq 0 ]]
+  do
+    ((PORT=PORT+1))
+done
+
+echo $PORT
+
+
 # run docker and start notebook server
 docker run -it \
-  -p 8888:8888 \
+  -p $PORT:8888 \
   -v "$PWD/ark:/usr/local/lib/python3.6/site-packages/ark" \
   -v "$PWD/scripts:/scripts" \
   -v "$PWD/data:/data" \
