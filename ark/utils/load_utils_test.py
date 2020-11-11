85a7484db4c9417ceedce4b508b0be9bbc107604
alex-l-kong <alkong@ucdavis.edu>
2020-11-11 11:55:35 -0800

    Fix tests for empty channels lists (take 2)

--- ark/utils/load_utils_test.py
+++ ark/utils/load_utils_test.py
@@ -18,12 +18,6 @@ def test_load_imgs_from_mibitiff():
                                                            channels=None,
                                                            delimiter='_')
 
-        with pytest.raises(ValueError):
-            # attempt to pass an empty channels list
-            loaded_xr = load_utils.load_imgs_from_mibitiff(temp_dir,
-                                                           channels=[],
-                                                           delimiter='_')
-
         # config test environment
         fovs, channels = test_utils.gen_fov_chan_names(num_fovs=2, num_chans=3, use_delimiter=True)
 
@@ -31,6 +25,12 @@ def test_load_imgs_from_mibitiff():
             temp_dir, fovs, channels, img_shape=(10, 10), mode='mibitiff', delimiter='_',
             fills=True, dtype=np.float32
         )
+
+        with pytest.raises(ValueError):
+            # attempt to pass an empty channels list
+            loaded_xr = load_utils.load_imgs_from_mibitiff(temp_dir,
+                                                           channels=[],
+                                                           delimiter='_')
 
         # check unspecified fov loading
         loaded_xr = load_utils.load_imgs_from_mibitiff(temp_dir,
@@ -91,12 +91,6 @@ def test_load_imgs_from_tree():
             loaded_xr = \
                 load_utils.load_imgs_from_tree(temp_dir, img_sub_folder="TIFs", dtype="int16")
 
-        with pytest.raises(ValueError):
-            # attempt to pass an empty channels list
-            loaded_xr = \
-                load_utils.load_imgs_from_tree(temp_dir, img_sub_folder="TIFs",
-                                               dtype="int16", channels=[])
-
         fovs, chans, imgs = test_utils.gen_fov_chan_names(num_fovs=3, num_chans=3,
                                                           return_imgs=True)
 
@@ -104,6 +98,12 @@ def test_load_imgs_from_tree():
             temp_dir, fovs, chans, img_shape=(10, 10), delimiter='_', fills=True, sub_dir="TIFs",
             dtype="int16"
         )
+
+        with pytest.raises(ValueError):
+            # attempt to pass an empty channels list
+            loaded_xr = \
+                load_utils.load_imgs_from_tree(temp_dir, img_sub_folder="TIFs",
+                                               dtype="int16", channels=[])
 
         # check default loading of all files
         loaded_xr = \
