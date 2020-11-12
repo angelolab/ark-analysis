3b86720e14a6e8bd43f81172a4e4e0e21de94476
Austin Edwards <edwardsa@janelia.hhmi.org>
2020-11-11 16:35:42 -0800

    added a check to see that scale arg can be converted to float in deepcell_service_utils, added a corresponding test in deepcell_service_utils_test

--- ark/utils/deepcell_service_utils_test.py
+++ ark/utils/deepcell_service_utils_test.py
@@ -42,6 +42,12 @@ def test_create_deepcell_output(mocker):
         create_deepcell_output(deepcell_input_dir=input_dir, deepcell_output_dir=output_dir,
                                fovs=['fov1', 'fov2'])
 
+        with pytest.raises(ValueError):
+            # fail if scale argument can not be converted to float
+            create_deepcell_output(deepcell_input_dir=input_dir, deepcell_output_dir=output_dir,
+                                   fovs=['fov1','fov2'], scale='test')
+
+
         # make sure DeepCell (.zip) output exists
         assert os.path.exists(os.path.join(output_dir, 'example_output.zip'))
 
