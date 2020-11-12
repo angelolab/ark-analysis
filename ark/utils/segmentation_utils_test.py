6a9eb0652fd846016db8816eb839b801883af91f
William Xu <williamxu132@gmail.com>
2020-11-11 20:09:48 -0800

    changed second loop to only test one fov

--- ark/utils/segmentation_utils_test.py
+++ ark/utils/segmentation_utils_test.py
@@ -239,35 +239,35 @@ def test_visualize_segmentation():
                                                    f'{mod_output_fov.values}'
                                                    f'_segmentation_labels.tiff'))
 
+    # test only one fov
     with tempfile.TemporaryDirectory() as temp_dir:
-
         segmentation_utils.visualize_segmentation(
             segmentation_labels_xr=segmentation_labels_xr,
-            fovs=segmentation_labels_xr.fovs.values, channel_data_xr=channel_xr,
+            fovs=segmentation_labels_xr.fovs.values[:1], channel_data_xr=channel_xr,
             output_dir=temp_dir)
-        for mod_output_fov in segmentation_labels_xr.fovs:
-            assert os.path.exists(os.path.join(temp_dir,
-                                               f'{mod_output_fov.values}'
-                                               f'_segmentation_borders.tiff'))
-            assert os.path.exists(os.path.join(temp_dir,
-                                               f'{mod_output_fov.values}'
-                                               f'_segmentation_labels.tiff'))
+
+        assert os.path.exists(os.path.join(temp_dir,
+                                           f'{segmentation_labels_xr.fovs[0].values}'
+                                           f'_segmentation_borders.tiff'))
+        assert os.path.exists(os.path.join(temp_dir,
+                                           f'{segmentation_labels_xr.fovs[0].values}'
+                                           f'_segmentation_labels.tiff'))
         for chan_list in overlay_channels:
             segmentation_utils.visualize_segmentation(
                 segmentation_labels_xr=segmentation_labels_xr,
-                fovs=segmentation_labels_xr.fovs.values, channel_data_xr=channel_xr,
+                fovs=segmentation_labels_xr.fovs.values[:1], channel_data_xr=channel_xr,
                 chan_list=chan_list,
                 output_dir=temp_dir)
-            for mod_output_fov in segmentation_labels_xr.fovs:
-                assert os.path.exists(
-                    os.path.join(temp_dir, '_'.join(
-                        [f'{mod_output_fov.values}', *chan_list, 'overlay.tiff'])))
-                assert os.path.exists(os.path.join(temp_dir,
-                                                   f'{mod_output_fov.values}'
-                                                   f'_segmentation_borders.tiff'))
-                assert os.path.exists(os.path.join(temp_dir,
-                                                   f'{mod_output_fov.values}'
-                                                   f'_segmentation_labels.tiff'))
+
+            assert os.path.exists(
+                os.path.join(temp_dir, '_'.join(
+                    [f'{segmentation_labels_xr.fovs[0].values}', *chan_list, 'overlay.tiff'])))
+            assert os.path.exists(os.path.join(temp_dir,
+                                               f'{segmentation_labels_xr.fovs[0].values}'
+                                               f'_segmentation_borders.tiff'))
+            assert os.path.exists(os.path.join(temp_dir,
+                                               f'{segmentation_labels_xr.fovs[0].values}'
+                                               f'_segmentation_labels.tiff'))
 
 
 def test_concatenate_csv():
