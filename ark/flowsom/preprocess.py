99953de16bdb4cb0c4046f237ce6604e02b20b1a
alex-l-kong <alkong@ucdavis.edu>
2020-11-12 16:07:47 -0800

    Next pass (still failing)

--- ark/flowsom/preprocess.py
+++ ark/flowsom/preprocess.py
@@ -2,6 +2,7 @@ import os
 
 import numpy as np
 import pandas as pd
+import xarray as xr
 import scipy.ndimage as ndimage
 from skimage.io import imread
 
@@ -10,12 +11,17 @@ from ark.utils import load_utils
 from ark.utils import misc_utils
 
 
-def preprocess_flowsom(img_xr, fovs=None, channels=None, blur_factor=2):
+def preprocess_flowsom(img_xr, seg_labels, save_path,
+                       fovs=None, channels=None, blur_factor=2):
     """Preprocess the images for FlowSOM clustering
 
     Args:
         img_data (xarray.DataArray):
             Array representing image data for each fov
+        seg_labels (xarray.DataArray):
+            Array representing segmentation labels for each image
+        save_dir (str):
+            The directory to write the pixel cluster CSVs to
         fovs (list):
             List of fovs to subset over, if None selects all
         channels (list):
@@ -37,30 +43,57 @@ def preprocess_flowsom(img_xr, fovs=None, channels=None, blur_factor=2):
         channels = img_xr.coords['channels'].values
 
     # verify that the fovs and channels provided are valid
-    misc_utils.verify_in_list(fovs, img_xr.coords['fovs'].values)
-    misc_utils.verify_in_list(channels, img_xr.coords['fovs'].values)
+    misc_utils.verify_in_list(fovs=fovs, image_fovs=img_xr.coords['fovs'].values)
+    misc_utils.verify_in_list(channels=channels, image_channels=img_xr.coords['channels'].values)
+
+    # verify the save path exists
+    if not os.path.exists(save_path):
+        raise ValueError("save_path provided does not exist")
 
     img_xr_subset = img_xr.loc[fovs, :, :, channels]
 
     # create an xarray to store the pre-processed data, assuming square images
-    flat_size = img_xr.shape[1]**2
-    img_xr_proc = xr.DataArray(np.zeros(len(fovs), flat_size, len(channels)),
-                               coords=[fovs, np.arange(flat_size), channels],
-                               dims=["fovs", "pixel_num", "channels"])
+    # flat_size = img_xr.shape[1]**2
+    # img_xr_proc = xr.DataArray(np.zeros((len(fovs), flat_size, len(channels))),
+    #                            coords=[fovs, np.arange(flat_size), channels],
+    #                            dims=["fovs", "pixel_num", "channels"])
 
     # iterate over fovs
     for fov in fovs:
+        print(fov)
         # subset img_xr with only the fov we're looking for
-        img_xr_sub = img_xr.loc[fov, ...].values
+        img_xr_sub = img_xr_subset.loc[fov, ...].values
 
         # apply a Gaussian blur for each marker
         img_data_blur = np.apply_along_axis(ndimage.gaussian_filter, axis=2,
                                             arr=img_xr_sub, sigma=blur_factor)
 
         # flatten each image, assuming each image is square for now
-        img_data_flat = np.reshape(img_data_blur.shape[0]**2, len(channels))
+        pixel_mat = np.reshape(img_data_blur, (img_data_blur.shape[0]**2, len(channels)))
 
-        # assign to respective fov in img_xr_proc
-        img_xr_proc.loc[fov, ...] = img_data_flat
+        # NOTE: the following lines may be unnecessary, this is assuming that
+        # people who use FlowSOM expect pixel cluster matrices to be referenced
+
+        # assign the fov to each cell
+        pixel_mat[:, len(channels)] = np.repeat(fov, img_data_blur.shape[0]**2)
+
+        # assign x and y coords
+        pixel_mat[:, len(channels) + 1] = np.repeat(
+            range(img_data_blur.shape[0]), img_data_blur.shape[0])
+        pixel_mat[:, len(channels) + 2] = np.tile(
+            range(img_data_blur.shape[0]), img_data_blur.shape[0])
+
+        # assign the corresponding segmentation labels
+        pixel_mat[:, len(channels) + 3] = seg_labels.loc[fov, ...].values.flatten()
+
+        # remove zero pixels
+        pixel_mat_nonzero = pixel_mat[np.sum(pixel_mat[:, num_markers], axis=1) != 0, :]
+
+        header = ','.join(channels) + ',sample,x,y,label'
+        np.savetxt(os.path.join(save_path, fov + '_pixel_info.csv'), pixel_mat_nonzero,
+                   delimiter=',', header=header)
+
+        # # assign to respective fov in img_xr_proc
+        # img_xr_proc.loc[fov, ...] = img_data_flat
 
     return img_xr_proc
