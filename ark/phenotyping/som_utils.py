78e9ca29f02974471b9797062d14277382e203ba
alex-l-kong <alkong@ucdavis.edu>
2021-01-25 16:18:11 -0800

    Remove comments

--- ark/phenotyping/som_utils.py
+++ ark/phenotyping/som_utils.py
@@ -106,9 +106,6 @@ def cluster_pixels(base_dir, chan_list):
             The list of markers to subset on
     """
 
-    # get the path to som_runner.R os.path.join(som_runner_path, 'som_runner.R')
-    # som_runner_path = os.path.dirname(os.path.realpath(__file__))
-
     # use Rscript to run som_runner.R with the correct command line args
     subprocess.call(['Rscript', 'som_runner.R',
                      os.path.join(base_dir, 'pixel_mat_preprocessed.csv'),
