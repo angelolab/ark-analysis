51d7f8a0c78de000fb6cef359619f8fe22b4ce4f
alex-l-kong <31424707+alex-l-kong@users.noreply.github.com>
2021-07-27 12:57:45 -0700

    Automatically rebuild Docker if requirements.txt modified (#435)
    * Automatically build Docker if requirements.txt modified in the last day
    
    * Tell user that Docker is rebuilding if requirements.txt is changed
    
    * Update README with new Docker info

--- start_docker.sh
+++ start_docker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# if requirements.txt has been changed in the last day, automatically rebuild Docker first
+if [[ $(find . -mmin -1440 -type f -print | grep requirements.txt | wc -l) -eq 1 ]]
+  then
+    echo "New requirements.txt file detected, rebuilding Docker"
+    docker build -t ark-analysis .
+fi
+
 # catch and pass update flag to notebook update routine
 bash update_notebooks.sh "$@"
 
@@ -12,10 +19,10 @@ until [[ $(lsof -i -P -n | grep 127.0.0.1:$PORT | wc -l) -eq 0 ]]
 done
 
 if [ $PORT -ne 8888 ]
-then
-  echo "WARNING: another Jupyter server on port 8888 running"
-  echo "Enter this URL instead to access the notebooks: http://127.0.0.1:$PORT/"
-  echo "In the URLs below, copy the token after \"token=\", paste that into the password prompt, and log in"
+  then
+    echo "WARNING: another Jupyter server on port 8888 running"
+    echo "Enter this URL instead to access the notebooks: http://127.0.0.1:$PORT/"
+    echo "In the URLs below, copy the token after \"token=\", paste that into the password prompt, and log in"
 fi
 
 docker run -it \
