b47cd47bedd19b35491f98af3cd0a51d11f5756d
y2k <git@y2kbugger.com>
2021-07-31 18:56:21 -0400

    Add unit tests for metacluster remapping

---
+++ ark/utils/metacluster_remap_gui/test_metaclusterdata.py
@@ -0,0 +1,80 @@
+from pathlib import Path
+from tempfile import tempdir
+
+import pandas as pd
+import numpy as np
+import pytest
+
+from .metaclusterdata import MetaClusterData, metaclusterdata_from_files
+
+THIS_DIR = Path(__file__).parent
+DATA_DIR = THIS_DIR.parent.parent.parent / 'data'
+MC_DATA_DIR = DATA_DIR / "example_dataset" / "metaclustering"
+
+
+def test_can_read_example_input_1():
+    metaclusterdata_from_files(
+        MC_DATA_DIR / "ex1_clusters_nozscore.csv",
+        MC_DATA_DIR / "ex1_clusters_pixelcount.csv")
+
+
+def test_can_read_example_input_2():
+    metaclusterdata_from_files(
+        MC_DATA_DIR / "ex2_clusters_nozscore.csv",
+        MC_DATA_DIR / "ex2_clusters_pixelcount.csv")
+
+
+@pytest.fixture
+def simple_metaclusterdata():
+    clusters_headers = ['CD163', 'CD206', 'CD31', 'cluster', 'hCluster_cap']
+    clusters_data = [
+        (0.1, 0.2, 0.1, 1, 1),
+        (0.1, 0.1, 0.3, 2, 2),
+        (0.5, 0.1, 0.1, 3, 3),
+        (0.7, 0.2, 0.1, 4, 3),
+        ]
+    clusters_raw_df = pd.DataFrame(data=clusters_data, columns=clusters_headers)
+
+    pixelcount_headers = ['cluster', 'count']
+    pixelcount_data = [
+        (1, 25),
+        (2, 10),
+        (3, 50),
+        (4, 77),
+        ]
+    pixelcount_df = pd.DataFrame(data=pixelcount_data, columns=pixelcount_headers)
+
+    return MetaClusterData(clusters_raw_df, pixelcount_df)
+
+
+def test_can_get_mapping(simple_metaclusterdata: MetaClusterData):
+    np.testing.assert_array_equal(
+        simple_metaclusterdata.mapping['hCluster_cap'].values,
+        np.array((1, 2, 3, 3)))
+
+
+def test_can_remap(simple_metaclusterdata: MetaClusterData):
+    simple_metaclusterdata.remap(4, 1)
+    assert simple_metaclusterdata.mapping.loc[4, 'hCluster_cap'] == 1
+
+
+def test_can_create_new_metacluster(simple_metaclusterdata: MetaClusterData):
+    new_mc = simple_metaclusterdata.new_metacluster()
+    simple_metaclusterdata.remap(4, new_mc)
+    assert simple_metaclusterdata.mapping.loc[4, 'hCluster_cap'] == 4
+
+
+def test_can_save_mapping(simple_metaclusterdata: MetaClusterData, tmp_path):
+    simple_metaclusterdata.output_mapping_filename = tmp_path / 'output_mapping.csv'
+    simple_metaclusterdata.save_output_mapping()
+    with open(tmp_path / 'output_mapping.csv', 'r') as f:
+        output = [ll.strip() for ll in f.readlines()]
+    assert output == [
+        "cluster,hCluster_cap",
+        "1,1",
+        "2,2",
+        "3,3",
+        "4,3",
+        ]
+
+# ptest_can_provide_alternate_name_for_hcluster_cap
---
+++ ark/utils/metacluster_remap_gui/test_metaclustergui.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+from .metaclusterdata import MetaClusterData
+from .metaclustergui import MetaClusterGui
+from .test_metaclusterdata import simple_metaclusterdata
+
+THIS_DIR = Path(__file__).parent
+
+
+def test_can_create_metaclustergui(simple_metaclusterdata: MetaClusterData):
+    MetaClusterGui(simple_metaclusterdata)
+
+
+def test_can_select_cluster(simple_metaclusterdata: MetaClusterData):
+    mcg = MetaClusterGui(simple_metaclusterdata)
+    mcg.selected_clusters.add(2)
+    assert mcg.selected_clusters == set([2])
+
+
+def test_can_select_all_clusters_in_metacluster(simple_metaclusterdata: MetaClusterData):
+    mcg = MetaClusterGui(simple_metaclusterdata)
+    mcg.select_metacluster(3)
+    assert len(mcg.selected_clusters) == 2
+
+
+def test_can_clear_selection(simple_metaclusterdata: MetaClusterData):
+    mcg = MetaClusterGui(simple_metaclusterdata)
+    mcg.selected_clusters.add(2)
+    mcg.clear_selection(None)
+    assert len(mcg.selected_clusters) == 0
+
+
+def test_can_remap_all_selected(simple_metaclusterdata: MetaClusterData):
+    mcg = MetaClusterGui(simple_metaclusterdata)
+    mcg.selected_clusters.add(1)
+    mcg.selected_clusters.add(2)
+    mcg.remap_current_selection(3)
+    assert simple_metaclusterdata.mapping.loc[1, 'hCluster_cap'] == 3
+    assert simple_metaclusterdata.mapping.loc[2, 'hCluster_cap'] == 3
+
+
+def test_can_pick_cell_in_heatmap(simple_metaclusterdata: MetaClusterData):
+    mcg = MetaClusterGui(simple_metaclusterdata)
+    mcg.ax_c.pick
