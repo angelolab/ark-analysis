738dac03f06c64b639960cb3cb4a4cfaad0384fb
ackagel <ackagel@ucdavis.edu>
2021-10-19 00:12:19 +0000

    tweak start docker script

--- start_docker.sh
+++ start_docker.sh
@@ -2,6 +2,8 @@
 
 # check for template developer flag
 JUPYTER_DIR='scripts'
+update=0
+external=''
 while test $# -gt 0
 do
   case "$1" in
@@ -9,9 +11,19 @@ do
       JUPYTER_DIR='templates'
       shift
       ;;
+    -u|--update)
+      update=1
+      shift
+      ;;
+    -e|--external)
+      external="$2"
+      shift
+      ;;
     *)
       echo "$1 is not an accepted option..."
-      echo "-d, --develop-notebook-templates : Mount templates for direct editing."
+      echo "-d, --develop-notebook-templates  : Mount templates for direct editing."
+      echo "-u, --update                      : Update default scripts"
+      echo "-e, --external                    : Mount external drives to /data/external"
       exit
       ;;
   esac
@@ -24,8 +36,18 @@ if [[ $(find . -mmin -1440 -type f -print | grep requirements.txt | wc -l) -eq 1
     docker build -t ark-analysis .
 fi
 
-# catch and pass update flag to notebook update routine
-bash update_notebooks.sh "$@"
+# perform update if requested
+if [ -d "$PWD/scripts" ]
+  then
+    if [ $update -ne 0 ]
+      then
+        bash update_notebooks.sh
+      else
+        cp -n "$PWD"/templates/*.ipynb "$PWD/scripts/."
+    fi
+  else
+    mkdir "$PWD/scripts"
+    cp "$PWD"/templates/*.ipynb "$PWD/scripts/."
 
 # find lowest open port available
 PORT=8888
@@ -35,12 +57,26 @@ until [[ $(docker container ls | grep 0.0.0.0:$PORT | wc -l) -eq 0 ]]
     ((PORT=$PORT+1))
 done
 
-docker run -it \
-  -p $PORT:$PORT \
-  -e JUPYTER_PORT=$PORT\
-  -e JUPYTER_DIR=$JUPYTER_DIR\
-  -v "$PWD/ark:/usr/local/lib/python3.6/site-packages/ark" \
-  -v "$PWD/$JUPYTER_DIR:/$JUPYTER_DIR" \
-  -v "$PWD/data:/data" \
-  -v "$PWD/.toks:/home/.toks" \
-  ark-analysis:latest
+if [ $external -ne '' ]
+  then
+    docker run -it \
+      -p $PORT:$PORT \
+      -e JUPYTER_PORT=$PORT\
+      -e JUPYTER_DIR=$JUPYTER_DIR\
+      -v "$PWD/ark:/usr/local/lib/python3.6/site-packages/ark" \
+      -v "$PWD/$JUPYTER_DIR:/$JUPYTER_DIR" \
+      -v "$PWD/data:/data" \
+      -v "$external:/data/external" \
+      -v "$PWD/.toks:/home/.toks" \
+      ark-analysis:latest
+  else
+    docker run -it \
+      -p $PORT:$PORT \
+      -e JUPYTER_PORT=$PORT\
+      -e JUPYTER_DIR=$JUPYTER_DIR\
+      -v "$PWD/ark:/usr/local/lib/python3.6/site-packages/ark" \
+      -v "$PWD/$JUPYTER_DIR:/$JUPYTER_DIR" \
+      -v "$PWD/data:/data" \
+      -v "$PWD/.toks:/home/.toks" \
+      ark-analysis:latest
+fi
