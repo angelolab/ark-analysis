1b5ebf26012b2e8563c4df33c0cbb508fa4064dc
alex-l-kong <alkong@ucdavis.edu>
2021-10-21 13:57:02 -0700

    Speed up the QC computation pipeline a ton

--- ark/mibi/qc_comp.py
+++ ark/mibi/qc_comp.py
@@ -1,3 +1,5 @@
+import cv2
+from numba import njit
 import numpy as np
 import pandas as pd
 import scipy.ndimage as ndimage
@@ -6,6 +8,8 @@ import xarray as xr
 import ark.utils.io_utils as io_utils
 import ark.utils.load_utils as load_utils
 import ark.utils.misc_utils as misc_utils
+
+import timeit
 
 
 def compute_nonzero_mean_intensity(image_data):
@@ -20,15 +24,24 @@ def compute_nonzero_mean_intensity(image_data):
             Matrix indicating the nonzero mean intensity of each channel per fov
     """
 
-    # mask the data to remove 0 values
-    masked_image_data = np.ma.masked_equal(image_data.values, 0)
+    # define the nonzero_mean_intensity array
+    nonzero_mean_intensity = np.zeros((image_data.shape[0], image_data.shape[3]))
 
-    # compute the nonzero mean across each fov and channel, convert back to numpy array
-    nonzero_mean_intensity = masked_image_data.mean(axis=(1, 2)).filled()
+    # iterate over each fov and channel
+    for i in np.arange(image_data.shape[0]):
+        for j in np.arange(image_data.shape[3]):
+            # extract the data for the fov and channel
+            image_data_np = image_data[i, :, :, j].values
 
-    # clip very large values (this applies when a division by 0 occurred
-    # because the channel values were all 0 for a specific fov)
-    nonzero_mean_intensity[nonzero_mean_intensity > 10 ** 10] = np.nan
+            # take just the non-zero pixels
+            image_data_nonzero = image_data_np[image_data_np != 0]
+
+            # take the mean of the non-zero pixels and assign to (fov, channel) in array
+            # unless there are no non-zero pixels, in which case default to np.nan
+            if len(image_data_nonzero) > 0:
+                nonzero_mean_intensity[i, j] = image_data_nonzero.mean()
+            else:
+                nonzero_mean_intensity[i, j] = np.nan
 
     return nonzero_mean_intensity
 
@@ -45,6 +58,7 @@ def compute_total_intensity(image_data):
             Matrix indicating the nonzero mean intensity of each channel per fov
     """
 
+    # a broadcasted approach is faster for sum
     return image_data.values.sum(axis=(1, 2))
 
 
@@ -60,13 +74,24 @@ def compute_99_9_intensity(image_data):
             Matrix indicating the 99.9% intensity of each channel per fov
     """
 
-    # compute the 99.9% intensity value across each fov and channel
-    return np.percentile(image_data.values, q=99.9, axis=(1, 2))
+    # define the 99.9% intensity array
+    intensity_99_9 = np.zeros((image_data.shape[0], image_data.shape[3]))
+
+    # iterate over each fov and channel
+    for i in np.arange(image_data.shape[0]):
+        for j in np.arange(image_data.shape[3]):
+            # extract the data for the fov and channel
+            image_data_np = image_data[i, :, :, j].values
+
+            # take the 99.9% value of the data and assign
+            intensity_99_9[i, j] = np.percentile(image_data_np, q=99.9)
+
+    return intensity_99_9
 
 
 def compute_qc_metrics(tiff_dir, img_sub_folder="TIFs", is_mibitiff=False,
                        fovs=None, chans=None, batch_size=5, gaussian_blur=False,
-                       blur_factor=1, dtype="int16"):
+                       blur_factor=1, dtype=np.float32):
     """Compute the QC metric matrices
 
     Args:
@@ -132,6 +157,7 @@ def compute_qc_metrics(tiff_dir, img_sub_folder="TIFs", is_mibitiff=False,
         [filenames[i:i + batch_size] for i in range(0, cohort_len, batch_size)]
     ):
         # extract the image data for each batch
+        start_time = timeit.default_timer()
         if is_mibitiff:
             image_data = load_utils.load_imgs_from_mibitiff(data_dir=tiff_dir,
                                                             mibitiff_files=batch_files,
@@ -141,6 +167,9 @@ def compute_qc_metrics(tiff_dir, img_sub_folder="TIFs", is_mibitiff=False,
                                                         img_sub_folder=img_sub_folder,
                                                         fovs=batch_names,
                                                         dtype=dtype)
+
+        end_time = timeit.default_timer()
+        print("Time to load image data: %.5f" % (end_time - start_time))
 
         # get the channel names directly from image_data if not specified
         if chans is None:
@@ -156,24 +185,48 @@ def compute_qc_metrics(tiff_dir, img_sub_folder="TIFs", is_mibitiff=False,
         image_data = image_data.loc[..., chans]
 
         # run Gaussian blurring per channel
+        start_time = timeit.default_timer()
         if gaussian_blur:
             for fov in batch_names:
-                image_data.loc[fov, ...].values = ndimage.gaussian_filter(
-                    image_data.loc[fov, ...], sigma=blur_factor
-                )
+                for chan in chans:
+                    # NOTE: running opencv GaussianBlur with sigmaX=1, borderType=BORDER_REPLICATE,
+                    # and ksize=(5, 5) might work even better and may be faster
+
+                    # mode 'nearest' extends input by replicating last pixel
+                    # same method that MATLAB imgaussfilter uses
+                    # truncate the filter at 2 standard deviations like MATLAB
+                    # image_data.loc[fov, :, :, chan] = ndimage.gaussian_filter(
+                    #     image_data.loc[fov, :, :, chan], sigma=blur_factor,
+                    #     mode='nearest', truncate=2.0
+                    # )
+                    image_data.loc[fov, :, :, chan] = cv2.GaussianBlur(
+                        image_data.loc[fov, :, :, chan].values, sigmaX=1,
+                        borderType=cv2.BORDER_REPLICATE, ksize=(5, 5)
+                    )
+        end_time = timeit.default_timer()
+        print("Time to add Gaussian blur: %.5f" % (end_time - start_time))
 
         # compute the QC metrics for the batch
+        start_time = timeit.default_timer()
         df_nonzero_mean_batch = pd.DataFrame(
             compute_nonzero_mean_intensity(image_data), columns=chans
         )
+        end_time = timeit.default_timer()
+        print("Time to compute non-zero mean intensity: %.5f" % (end_time - start_time))
 
+        start_time = timeit.default_timer()
         df_total_intensity_batch = pd.DataFrame(
             compute_total_intensity(image_data), columns=chans
         )
+        end_time = timeit.default_timer()
+        print("Time to compute total intensity: %.5f" % (end_time - start_time))
 
+        start_time = timeit.default_timer()
         df_99_9_intensity_batch = pd.DataFrame(
             compute_99_9_intensity(image_data), columns=chans
         )
+        end_time = timeit.default_timer()
+        print("Time to compute 99.9%% intensity value: %.5f" % (end_time - start_time))
 
         # append the batch_names as fovs to each DataFrame
         df_nonzero_mean_batch['fov'] = batch_names
@@ -181,9 +234,12 @@ def compute_qc_metrics(tiff_dir, img_sub_folder="TIFs", is_mibitiff=False,
         df_99_9_intensity_batch['fov'] = batch_names
 
         # append the batch QC metric data to the full processed data
+        start_time = timeit.default_timer()
         df_nonzero_mean = pd.concat([df_nonzero_mean, df_nonzero_mean_batch])
         df_total_intensity = pd.concat([df_total_intensity, df_total_intensity_batch])
         df_99_9_intensity = pd.concat([df_99_9_intensity, df_99_9_intensity_batch])
+        end_time = timeit.default_timer()
+        print("Time to concatenate DataFrame: %.5f" % (end_time - start_time))
 
         # update number of fovs processed
         fovs_processed += batch_size
