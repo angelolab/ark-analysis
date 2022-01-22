1ef21a863b6066a94ed636ed32c0d4b4c2e35e45
alex-l-kong <alkong@ucdavis.edu>
2022-01-21 19:28:18 -0800

    Add a test for the pixel and cell cluster overlay

--- ark/utils/plot_utils_test.py
+++ ark/utils/plot_utils_test.py
@@ -3,6 +3,7 @@ import pytest
 import tempfile
 
 import numpy as np
+import pandas as pd
 import skimage.io as io
 import xarray as xr
 import pytest
@@ -32,6 +33,80 @@ def _generate_image_data(img_dims):
         raise ValueError("must be image data of [rows, cols, channels]")
 
     return np.random.randint(low=0, high=100, size=img_dims)
+
+
+def test_plot_pixel_cell_cluster_overlay():
+    sample_img_data = np.random.randint(0, 20, size=(3, 1024, 1024))
+    sample_img_xr = xr.DataArray(
+        sample_img_data,
+        coords=[['fov1', 'fov2', 'fov3'], np.arange(1024), np.arange(1024)],
+        dims=['fovs', 'x', 'y']
+    )
+
+    # invalid FOVs provided
+    with pytest.raises(ValueError):
+        plot_utils.plot_pixel_cell_cluster_overlay(
+            sample_img_xr, ['fov3', 'fov4'], 'sample_mapping_path.csv', {}
+        )
+
+    # invalid mapping path provided
+    with pytest.raises(FileNotFoundError):
+        plot_utils.plot_pixel_cell_cluster_overlay(
+            sample_img_xr, ['fov1', 'fov2'], 'bad_sample_mapping_path.csv', {}
+        )
+
+    with tempfile.TemporaryDirectory() as td:
+        mapping_path = os.path.join(td, 'sample_mapping_path.csv')
+
+        # invalid columns provided in mapping
+        df = pd.DataFrame.from_dict({
+            'bad_cluster_col': np.arange(20),
+            'metacluster': np.repeat(np.arange(5), 4),
+            'mc_name': ['meta' + str(i) for i in np.repeat(np.arange(5), 4)]
+        })
+        df.to_csv(mapping_path, index=False)
+
+        with pytest.raises(ValueError):
+            plot_utils.plot_pixel_cell_cluster_overlay(
+                sample_img_xr, ['fov1', 'fov2'], mapping_path, {}
+            )
+
+        # rename bad_cluster_col to cluster so it passes that test
+        df = df.rename({'bad_cluster_col': 'cluster'}, axis=1)
+        df.to_csv(mapping_path, index=False)
+
+        # invalid mapping provided
+        bad_sample_mapping = {i + 2: (0.0, 0.0, 0.0) for i in np.arange(5)}
+
+        with pytest.raises(ValueError):
+            plot_utils.plot_pixel_cell_cluster_overlay(
+                sample_img_xr, ['fov1', 'fov2'], mapping_path, bad_sample_mapping
+            )
+
+        # define a valid mapping
+        sample_mapping = {
+            i: tuple(np.random.rand(3)) for i in np.arange(5)
+        }
+
+        # test 1: save_dir not specified
+        plot_utils.plot_pixel_cell_cluster_overlay(
+            sample_img_xr, ['fov1', 'fov2'], mapping_path, sample_mapping
+        )
+
+        # assert no files created in temp_dir
+        for fov in sample_img_xr.fovs.values:
+            assert not os.path.exists(os.path.join(td, fov + '.png'))
+
+        # test 2: save_dir specified
+        plot_utils.plot_pixel_cell_cluster_overlay(
+            sample_img_xr, ['fov1', 'fov2'], mapping_path, sample_mapping,
+            save_dir=td
+        )
+
+        # assert files only created for fov1`and fov2, not fov3
+        assert os.path.exists(os.path.join(td, 'fov1.png'))
+        assert os.path.exists(os.path.join(td, 'fov2.png'))
+        assert not os.path.exists(os.path.join(td, 'fov3.png'))
 
 
 def test_tif_overlay_preprocess():
