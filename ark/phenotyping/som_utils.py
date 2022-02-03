Relabel barchart, fix off-by-one meta cluster colormap error, ensure heatmap meta cluster colors are grouped together

ark/phenotyping/som_utils.py
@@ -1814,8 +1814,10 @@ def generate_meta_cluster_colormap_dict(meta_cluster_remap_path, cmap):
     )
 
     # define the raw meta cluster colormap
+    # NOTE: colormaps returned by interactive reclustering are zero-indexed
+    # need to subtarct 1 to account for that
     raw_colormap = {
-        i: cmap(i) for i in np.unique(remapping['metacluster'])
+        i: cmap(i - 1) for i in np.unique(remapping['metacluster'])
     }
 
     # define the renamed meta cluster colormap
@@ -1875,6 +1877,9 @@ def generate_weighted_channel_avg_heatmap(cell_cluster_channel_avg_path, cell_cl
         channel_avg_cols=cell_cluster_channel_avgs.columns.values
     )
 
+    # sort the data by the meta cluster value
+    cell_cluster_channel_avgs = cell_cluster_channel_avgs.sort_values(by='cell_meta_cluster')
+
     # map raw_cmap onto cell_cluster_channel_avgs for the heatmap to display the side color bar
     meta_cluster_index = cell_cluster_channel_avgs[cell_cluster_col].values
     meta_cluster_mapping = pd.Series(cell_cluster_channel_avgs['cell_meta_cluster']).map(raw_cmap)
@@ -1890,6 +1895,7 @@ def generate_weighted_channel_avg_heatmap(cell_cluster_channel_avg_path, cell_cl
         max_val=max_val,
         cbar_ticks=np.arange(-3, 4),
         row_colors=meta_cluster_mapping,
+        row_cluster=False,
         colormap='vlag'
     )
 

ark/phenotyping/som_utils_test.py
@@ -2260,8 +2260,8 @@ def test_generate_meta_cluster_colormap_dict():
         # define a dummy remapping
         sample_remapping = {
             'cluster': [i for i in np.arange(100)],
-            'metacluster': [int(i / 50) for i in np.arange(100)],
-            'mc_name': ['meta' + str(int(i / 50)) for i in np.arange(100)]
+            'metacluster': [int(i / 50) + 1 for i in np.arange(100)],
+            'mc_name': ['meta' + str(int(i / 50) + 1) for i in np.arange(100)]
         }
 
         sample_remapping = pd.DataFrame.from_dict(sample_remapping)
@@ -2271,7 +2271,7 @@ def test_generate_meta_cluster_colormap_dict():
         )
 
         # define a sample ListedColormap
-        cmap = ListedColormap(['red', 'blue'])
+        cmap = ListedColormap(['red', 'blue', 'green'])
 
         raw_cmap, renamed_cmap = som_utils.generate_meta_cluster_colormap_dict(
             os.path.join(temp_dir, 'sample_remapping.csv'), cmap
@@ -2288,8 +2288,8 @@ def test_generate_meta_cluster_colormap_dict():
         )
 
         # assert the colors match up
-        assert raw_cmap[0] == renamed_cmap['meta0'] == (1.0, 0.0, 0.0, 1.0)
-        assert raw_cmap[1] == renamed_cmap['meta1'] == (0.0, 0.0, 1.0, 1.0)
+        assert raw_cmap[1] == renamed_cmap['meta1'] == (1.0, 0.0, 0.0, 1.0)
+        assert raw_cmap[2] == renamed_cmap['meta2'] == (0.0, 0.0, 1.0, 1.0)
 
 
 def test_generate_weighted_channel_avg_heatmap():

ark/utils/metacluster_remap_gui/metaclustergui.py
@@ -179,7 +179,7 @@ class MetaClusterGui():
         # xaxis pixelcount graphs
         self.ax_cp.xaxis.set_tick_params(which='both', bottom=False, labelbottom=False)
         self.ax_cp.yaxis.set_tick_params(which='both', left=False, labelleft=False)
-        self.ax_cp.set_ylabel("Pixels (k)", rotation=90)
+        self.ax_cp.set_ylabel("Count (k)", rotation=90)
         self.ax_cp.set_xlim(0, self.mcd.cluster_count)
         self.rects_cp = self.ax_cp.bar(
             np.arange(self.mcd.cluster_count) + 0.5,