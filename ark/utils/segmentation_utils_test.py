a2af20a2b0b05df5476a5fae72b0eacde9973174
Sricharan Reddy Varra <sricharanvarra@gmail.com>
2022-03-11 15:43:51 -0800

    missed one

--- ark/utils/segmentation_utils_test.py
+++ ark/utils/segmentation_utils_test.py
@@ -263,8 +263,10 @@ def test_save_segmentation_labels():
         for fov in segmentation_labels_xr.fovs.values:
             fov_whole_cell = segmentation_labels_xr.loc[fov, :, :, 'whole_cell'].values
             fov_nuclear = segmentation_labels_xr.loc[fov, :, :, 'nuclear'].values
-            io.imsave(os.path.join(temp_dir, '%s_feature_0.tif' % fov), fov_whole_cell)
-            io.imsave(os.path.join(temp_dir, '%s_feature_1.tif' % fov), fov_nuclear)
+            io.imsave(os.path.join(temp_dir, '%s_feature_0.tif' % fov), fov_whole_cell,
+                      check_contrast=False)
+            io.imsave(os.path.join(temp_dir, '%s_feature_1.tif' % fov), fov_nuclear,
+                      check_contrast=False)
 
         # invalid channel values passed
         with pytest.raises(ValueError):
