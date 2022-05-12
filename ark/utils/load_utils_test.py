4c32fa74267abec628edb67488680fb787b7a2f7
Sricharan Reddy Varra <sricharanvarra@gmail.com>
2022-05-11 18:21:57 -0700

    test fix

--- ark/utils/load_utils_test.py
+++ ark/utils/load_utils_test.py
@@ -142,7 +142,7 @@ def test_load_imgs_from_tree():
             load_utils.load_imgs_from_tree(temp_dir, img_sub_folder="TIFS", dtype="int16",
                                            fovs='fov0', channels=some_chans)
 
-        assert loaded_xr.equals(data_xr[:1, :, :, :1])
+        assert loaded_xr.equals(data_xr[:, :, :, :1])
 
         # check that an error raises when a channel provided does not exist
         with pytest.raises(ValueError):
