dca579e538d6126c3f1ba3decc83c730a41af7ac
Sricharan Reddy Varra <sricharanvarra@gmail.com>
2022-05-11 17:54:52 -0700

    verify_same_elements adjustment

--- ark/utils/load_utils.py
+++ ark/utils/load_utils.py
@@ -154,7 +154,7 @@ def load_imgs_from_tree(data_dir, img_sub_folder=None, fovs=None, channels=None,
         channels_indices = [channels_no_delim.index(chan.split('.')[0]) for chan in all_channels]
 
         # verify if channels from user input are present in `all_channels`
-        misc_utils.verify_same_elements(channels_no_delim=channels_no_delim, all_channels=all_channels)
+        misc_utils.verify_same_elements(channels=channels, channels_no_delim=channels_no_delim)
 
         # reorder back to original
         channels = [chan for _, chan in sorted(zip(channels_indices, all_channels))]
