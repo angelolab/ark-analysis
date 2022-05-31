6a2bd5650cb8ee9f095fa86c180e886b055504e5
ackagel <ackagel@ucdavis.edu>
2022-05-31 23:47:27 +0000

    adds batching to fib seg

--- ark/segmentation/fiber_segmentation.py
+++ ark/segmentation/fiber_segmentation.py
@@ -13,23 +13,26 @@ from skimage.morphology import remove_small_objects
 from skimage.measure import regionprops_table
 from skimage.exposure import equalize_adapthist
 
+from ark.utils import io_utils, load_utils
 from ark.utils.plot_utils import set_minimum_color_for_colormap
 from ark import settings
 
 
-def plot_fiber_segmentation_steps(data_xr, fov_name, fiber_channel, blur=2,
+def plot_fiber_segmentation_steps(data_dir, fov_name, fiber_channel, img_sub_folder=None, blur=2,
                                   contrast_scaling_divisor=128, fiber_widths=(2, 4),
                                   ridge_cutoff=0.1, sobel_blur=1, min_fiber_size=15,
                                   img_cmap=plt.cm.bone, labels_cmap=plt.cm.cool):
     """Plots output from each fiber segmentation step for single FoV
 
     Args:
-        data_xr (xr.DataArray):
-            Multiplexed image data in (fov, x, y, channel) format
+        data_dir (str | PathLike):
+            Folder containing dataset
         fov_name (str):
             Name of test FoV
         fiber_channel (str):
             Channel for fiber segmentation, e.g collagen
+        img_sub_folder (str | NoneType):
+            Whether to expect image subfolder in `data_dir`.  If no subfolder, set to None.
         blur (float):
             Preprocessing gaussian blur radius
         contrast_scaling_divisor (int):
@@ -45,7 +48,16 @@ def plot_fiber_segmentation_steps(data_xr, fov_name, fiber_channel, blur=2,
             Gaussian blur radius for sobel driven elevation map creation
         min_fiber_size (int):
             Minimum area of fiber object
+        img_cmap (matplotlib.cm.Colormap):
+            Matplotlib colormap to use for (non-labeled) images
+        labels_cmap (matplotlib.cm.Colormap):
+            Base matplotlib colormap to use for labeled images.  This will only be applied to the
+            non-zero labels, with the zero-region being colored black.
     """
+
+    data_xr = load_utils.load_imgs_from_tree(
+        data_dir, img_sub_folder, fovs=[fov_name], channels=[fiber_channel]
+    )
 
     channel_data = data_xr.loc[fov_name, :, :, fiber_channel].values
 
@@ -106,6 +118,45 @@ def plot_fiber_segmentation_steps(data_xr, fov_name, fiber_channel, blur=2,
 
     for ax in axes.reshape(-1):
         ax.axis('off')
+
+
+def batch_segment_fibers(data_dir, fiber_channel, out_dir, img_sub_folder=None, batch_size=5,
+                         **kwargs):
+    """Segments fiber objects in batches
+
+    Args:
+        data_dir (str | PathLike):
+            Folder containing dataset
+        fiber_channel (str):
+            Channel for fiber segmentation, e.g collagen.
+        out_dir (str | PathLike):
+            Directory to save fiber object labels and table.
+        img_sub_folder (str | NoneType):
+            Image subfolder name in `data_dir`. If there is not subfolder, set this to None.
+        batch_size (int):
+            Number of FoVs to load/process at a time
+        **kwargs:
+            Keyword arguments for `segment_fibers`
+    """
+
+    fovs = io_utils.list_folders(data_dir)
+    batching_strategry = \
+        [fovs[i:i + batch_size] for i in range(0, len(fovs), batch_size)]
+
+    fiber_label_images = {}
+    fiber_object_table = []
+
+    for batch in batching_strategry:
+        subset_xr = load_utils.load_imgs_from_tree(
+            data_dir, img_sub_folder, fovs=batch, channels=[fiber_channel]
+        )
+        subtable, sublabel = segment_fibers(subset_xr, fiber_channel, out_dir, **kwargs)
+        fiber_label_images.update(sublabel)
+        fiber_object_table.append(subtable)
+
+    fiber_object_table = pd.concat(fiber_object_table)
+
+    return fiber_object_table, fiber_label_images
 
 
 def segment_fibers(data_xr, fiber_channel, out_dir, blur=2, contrast_scaling_divisor=128,
