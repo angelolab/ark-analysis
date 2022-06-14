4c07392c8b5342f11365ee3e174db83d8ae0789f
Sricharan Reddy Varra <srivarra@stanford.edu>
2022-06-14 14:52:08 -0700

    added toffy stile dependency retrieval

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from os import path
+from os import path, pardir
 from setuptools import setup, find_packages
 
 VERSION = '0.2.10'
 
+PKG_FOLDER = path.abspath(path.join(__file__, pardir))
+
 # set a long description which is basically the README
 with open(path.join(path.abspath(path.dirname(__file__)), 'README.md')) as f:
     long_description = f.read()
+
+with open(path.join(PKG_FOLDER, 'requirements.txt')) as req_file:
+    requirements = req_file.read().splitlines()
 
 setup(
     name='ark-analysis',
@@ -16,26 +21,7 @@ setup(
     author='Angelo Lab',
     url='https://github.com/angelolab/ark-analysis',
     download_url='https://github.com/angelolab/ark-analysis/archive/v{}.tar.gz'.format(VERSION),
-    install_requires=['cryptography>=3.4.8,<4',
-                      'feather_format>=0.4.1,<1',
-                      'ipympl>=0.7.0,<0.8.0',
-                      'jupyter>=1.0.0,<2',
-                      'jupyter_contrib_nbextensions>=0.5.1,<1',
-                      'jupyterlab>=3.4.3,<4',
-                      'matplotlib>=2.2.2,<3',
-                      'numpy>=1.21.6,<2',
-                      'palettable>=3.3.0,<4',
-                      'pandas>=1.3.5,<2',
-                      'requests>=2.27.1,<3',
-                      'scikit-image>=0.14.3,<=0.16.2',
-                      'scikit-learn>=0.24.2,<1',
-                      'scipy>=1.7.3,<2',
-                      'seaborn>=0.11.2,<1',
-                      'spatial-lda>=0.1.3,<1',
-                      'statsmodels>=0.13.2,<1',
-                      'umap-learn>=0.5.3,<1',
-                      'xarray>=0.17.0,<1',
-                      'tqdm>=4.64.0,<5'],
+    install_requires=requirements,
     extras_require={
         'tests': ['pytest',
                   'pytest-cov',
