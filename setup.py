f0df1b49d1c96991acf944ee55abd0b3e85b1ff2
Sricharan Reddy Varra <srivarra@stanford.edu>
2022-06-27 15:24:21 -0700

    changed PKG_FOLDER (is this the fix?)

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ VERSION = '0.3.1'
 
 PKG_FOLDER = path.abspath(path.join(__file__, pardir))
 
-# set a long description which is basically the README
-with open(path.join(path.abspath(path.dirname(__file__)), 'README.md')) as f:
-    long_description = f.read()
-
 with open(path.join(PKG_FOLDER, 'requirements.txt')) as req_file:
     requirements = req_file.read().splitlines()
+    
+# set a long description which is basically the README
+with open(path.join(PKG_FOLDER, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='ark-analysis',
