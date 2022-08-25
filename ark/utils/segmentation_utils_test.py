23cbf9d7b738ab6ff4fab63fa0d1d98db083e050
alex-l-kong <31424707+alex-l-kong@users.noreply.github.com>
2022-08-25 11:52:28 -0700

    Simplify `save_segmentation_labels` to remove label saving and default to channel overlay saving (#661)
    * Remove redundant segmentation label saving and default channel overlay to ['nuclear_channel', 'membrane_channel']
    
    * Save contour_mask, change default channels to None, explicitly pass in channel names in notebook
    
    * Force mistune library to 0.8.4 to prevent it from conflicting with m2r2
    
    * Mock natsort in conf.py

--- ark/utils/segmentation_utils_test.py
+++ ark/utils/segmentation_utils_test.py
@@ -210,9 +210,9 @@ def test_save_segmentation_labels():
     channel_xr = test_utils.make_images_xarray(
         np.zeros((2, 50, 50, 2)), channel_names=['nuclear_channel', 'membrane_channel']
     )
-    chan_sub = channel_xr.channels.values[:2]
     segmentation_labels_xr = test_utils.make_labels_xarray(np.zeros((2, 50, 50, 2)))
     fov_sub = segmentation_labels_xr.fovs.values[:1]
+    chan_sub = channel_xr.channels.values[:2]
 
     # test saving without channel overlay
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -236,13 +236,11 @@ def test_save_segmentation_labels():
 
         # test segmentation without channel overlay
         segmentation_utils.save_segmentation_labels(
-            segmentation_dir=temp_dir, data_dir=img_dir, output_dir=temp_dir, fovs=fov_sub
+            segmentation_dir=temp_dir, data_dir=img_dir, output_dir=temp_dir,
+            fovs=fov_sub
         )
 
         # make sure all files except overlay.tiff exists
-        assert os.path.exists(os.path.join(temp_dir,
-                                           f'{fov_sub[0]}'
-                                           f'_segmentation_labels.tiff'))
         assert os.path.exists(os.path.join(temp_dir,
                                            f'{fov_sub[0]}'
                                            f'_segmentation_borders.tiff'))
@@ -284,13 +282,10 @@ def test_save_segmentation_labels():
         # make sure all files and overlay.tiff exists
         assert os.path.exists(os.path.join(temp_dir,
                                            f'{fov_sub[0]}'
-                                           f'_segmentation_labels.tiff'))
-        assert os.path.exists(os.path.join(temp_dir,
-                                           f'{fov_sub[0]}'
                                            f'_segmentation_borders.tiff'))
         assert os.path.exists(os.path.join(temp_dir,
                                            '_'.join([f'{fov_sub[0]}',
-                                                     *chan_sub,
+                                                     *channel_xr.channels.values[:2],
                                                      'overlay.tiff'])))
 
 
