65a58ec9be13d2d1d5552f58fb8b044fadce839b
Candace Liu <candace.liu94@gmail.com>
2022-09-21 20:46:51 +0000

    Change silhouette function code

--- ark/utils/spatial_analysis_utils.py
+++ ark/utils/spatial_analysis_utils.py
@@ -529,19 +529,18 @@ def compute_kmeans_silhouette(neighbor_mat_data, min_k=2, max_k=10, subsample=No
     for n in range(min_k, max_k + 1):
         cluster_fit = KMeans(n_clusters=n).fit(neighbor_mat_data)
         cluster_labels = cluster_fit.labels_
-        neighbor_mat_data["cluster"] = cluster_labels
 
-        if subsample is None:
-            cluster_score = sklearn.metrics.silhouette_score(neighbor_mat_data, cluster_labels,
-                                                             metric="euclidean")
-        else:
+        sub_dat = neighbor_mat_data.copy()
+        sub_dat["cluster"] = cluster_labels
+
+        if subsample is not None:
             # Subsample each cluster
-            sub_dat = neighbor_mat_data.groupby("cluster").apply(
+            sub_dat = sub_dat.groupby("cluster").apply(
                 lambda x: x.sample(subsample, replace=len(x) < subsample)).reset_index(drop=True)
-            cluster_score = sklearn.metrics.silhouette_score(sub_dat.drop("cluster", axis=1),
-                                                             sub_dat["cluster"],
-                                                             metric="euclidean")
 
+        cluster_score = sklearn.metrics.silhouette_score(sub_dat.drop("cluster", axis=1),
+                                                         sub_dat["cluster"],
+                                                         metric="euclidean")
         cluster_stats.loc[n] = cluster_score
 
     return cluster_stats
