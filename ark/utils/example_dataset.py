84fc6c6fdf5b26a617339a7606280a7515c7e878
Sricharan Reddy Varra <srivarra@stanford.edu>
2022-09-29 15:57:17 -0600

    moved example dataset into it's own respective file and test file

---
+++ ark/utils/example_dataset.py
@@ -0,0 +1,130 @@
+import datasets
+import pathlib
+import shutil
+from typing import Union
+
+
+class ExampleDataset():
+    def __init__(self, dataset: str, overwrite_existing: bool = True, cache_dir: str = None, revision: str = None) -> None:
+        """
+        Constructs a utility class for downloading and moving the dataset with respect to it's
+        various partitions on Hugging Face: https://huggingface.co/datasets/angelolab/ark_example.
+
+        Args:
+            dataset (str): The name of the dataset to download. Can be one of `nb1`, `nb2`,
+                `nb3`, `nb4`.
+            overwrite_existing (bool): 
+            cache_dir (str, optional): The directory to save the cache dir. Defaults to `None`,
+                which internally in Hugging Face defaults to `~/.cache/huggingface/datasets`.
+            revision (str, optional): The commit ID from Hugging Face for the dataset. Used for
+                internal development only. Allows the user to fetch a commit from a particular
+                `revision` (Hugging Face's terminology for branch). Defaults to `None`. This
+                defaults to the latest version in the `main` branch.
+                (https://huggingface.co/datasets/angelolab/ark_example/tree/main).
+        """
+        self.dataset = dataset
+        self.overwrite_existing = overwrite_existing
+        self.cache_dir = cache_dir
+        self.revision = revision
+
+        self.path_suffixes = {
+            "image_data": "image_data",
+            "cell_table": "segmentation/cell_table",
+            "deepcell_output": "segmentation/deepcell_output"
+        }
+        """
+        Path suffixes for mapping each downloaded dataset partition to it's appropriate
+        relative save directory.
+        """
+
+    def download_example_dataset(self):
+        """Downloads the example dataset from Hugging Face Hub.
+        The following is a link to the dataset used:
+        https://huggingface.co/datasets/angelolab/ark_example
+
+        The dataset will be downloaded to the Hugging Face default cache
+        `~/.cache/huggingface/datasets`.
+        """
+        self.dataset_paths = datasets.load_dataset(path="angelolab/ark_example",
+                                                   revision=self.revision,
+                                                   name=self.dataset,
+                                                   cache_dir=self.cache_dir,
+                                                   use_auth_token=True)
+
+    def check_downloaded(self, dst_path: pathlib.Path) -> bool:
+        """
+        Checks to see if the folder for a dataset config already exists in the `save_dir`
+        (i.e. `dst_path` is the specific folder for the config.). If the folder exists, and it
+        there are no contents, then it'll return True, False otherwise.
+
+        Args:
+            dst_path (pathlib.Path): _description_
+
+        Returns:
+            bool: _description_
+        """
+        dst_files = list(dst_path.rglob("*"))
+
+        if len(dst_files) == 0:
+            return False
+        else:
+            return True
+
+    def move_example_dataset(self, save_dir: Union[str, pathlib.Path]):
+        """
+        Moves the downloaded example data from the cache_dir
+
+        Args:
+            save_dir (Union[str, pathlib.Path]): The path to save the dataset files in.
+        """
+        if type(save_dir) is not pathlib.Path:
+            save_dir = pathlib.Path(save_dir)
+
+        dataset_names = list(self.dataset_paths[self.dataset].features.keys())
+
+        for ds_n in dataset_names:
+            ds_n_suffix = self.path_suffixes[ds_n]
+
+            # The path where the dataset is saved in the Hugging Face Cache post-download,
+            # Necessary to copy + move the data from the cache to the user specified `save_dir`.
+            dataset_cache_path = pathlib.Path(self.dataset_paths[self.dataset][ds_n][0])
+            src_path = dataset_cache_path / ds_n
+            dst_path = save_dir / ds_n_suffix
+
+            # Overwrite the existing dataset if specified, or if there is no dataset.
+            if self.overwrite_existing or self.check_downloaded(dst_path=dst_path):
+
+                shutil.copytree(src_path, dst_path, dirs_exist_ok=True,
+                                ignore=shutil.ignore_patterns("._*"))
+
+
+def get_example_dataset(dataset: str, save_dir: Union[str, pathlib.Path],
+                        overwrite_existing: bool = True):
+    """
+    A user facing wrapper function which downloads a specified dataset from Hugging Face,
+    and moves it to the specified save directory `save_dir`.
+    The dataset may be found here: https://huggingface.co/datasets/angelolab/ark_example
+
+
+    Args:
+        dataset (str): The dataset to download for a particular notebook.
+        save_dir (Union[str, pathlib.Path]): The path to save the dataset files in.
+        overwrite_existing (bool): The option to overwrite existing configs of the `dataset` 
+            downloaded. Defaults to True.
+    """
+
+    valid_datasets = ["nb1", "nb2", "nb3", "nb4"]
+
+    # Check the appropriate dataset name
+    if dataset not in valid_datasets:
+        ValueError(f"The dataset <{dataset}> is not one of the valid datasets available.")
+
+    example_dataset = ExampleDataset(dataset=dataset, overwrite_existing=overwrite_existing,
+                                     cache_dir=None,
+                                     revision="9fecc0ccbb8f2cf1b33172b827f51dfdcf11c149")
+
+    # Download the dataset
+    example_dataset.download_example_dataset()
+
+    # Move the dataset over to the save_dir from the user.
+    example_dataset.move_example_dataset(save_dir=save_dir)
---
+++ ark/utils/test_example_dataset.py
@@ -0,0 +1,166 @@
+import pytest
+import pathlib
+from example_dataset import ExampleDataset, get_example_dataset
+from typing import Callable, Iterator
+
+
+# Sets the example dataset path once: Will not cause duplicate downloads
+@pytest.fixture(scope="session")
+def setup_temp_path_factory(tmp_path_factory) -> Iterator[pathlib.Path]:
+    """
+    A Fixture which creates the directory where the dataset is saved.
+    Downloads the dataset once per session, instead of once per notebook.
+
+    Args:
+        tmp_path_factory (pytest.TempPathFactory): Factory for temporary directories under the
+            common base temp directory.
+
+    Yields:
+        Iterator[pathlib.Path]: The iterable path containing the location of the dataset.
+    """
+    cache_dir = tmp_path_factory.mktemp("example_dataset")
+    yield cache_dir
+
+
+# Only download the dataset configs required per tests w.r.t the notebooks.
+# Will not download reused dataset configs.
+@pytest.fixture(scope="session", params=["nb1", "nb2"])
+def dataset_download(setup_temp_path_factory, request) -> Iterator[ExampleDataset]:
+    """
+    A Fixture which instantiates and downloads the dataset with respect to each
+    notebook.
+
+    Args:
+        setup_temp_path_factory (pytest.Fixture): Factory for temporary directories under the
+            common base temp directory.
+        request (pytest.FixtureRequest): The parameter, in this case it is the notebook to
+            download the dataset for.
+
+    Yields:
+        Iterator[ExampleDataset]: The iterable Example Dataset.
+    """
+    # Set up ExampleDataset class
+    example_dataset: ExampleDataset = ExampleDataset(
+        dataset=request.param,
+        cache_dir=setup_temp_path_factory,
+        revision="9fecc0ccbb8f2cf1b33172b827f51dfdcf11c149"
+    )
+    # Download example data for a particular notebook
+    example_dataset.download_example_dataset()
+    yield example_dataset
+
+
+class TestExampleDataset:
+    @pytest.fixture(autouse=True)
+    def _setup(self):
+        """
+        Sets up necessary information needed for assert statements.
+        Sets up dictionary to call the functions which check each dataset that is downloaded.
+        """
+        self.fov_names = [f"fov{i}" for i in range(11)]
+        self.channel_names = ["CD3", "CD4", "CD8", "CD14", "CD20", "CD31", "CD45", "CD68",
+                              "CD163", "CK17", "Collagen1", "ECAD", "Fibronectin", "GLUT1",
+                              "H3K9ac", "H3K27me3", "HLADR", "IDO", "Ki67", "PD1", "SMA", "Vim"]
+        self.cell_table_names = ["cell_table_arcsinh_transformed", "cell_table_size_normalized"]
+        self.deepcell_output_names = [f"fov{i}_feature_{j}" for i in range(11) for j in range(2)]
+        self.dataset_test_fns: dict[str, Callable] = {
+            "image_data": self._image_data_check,
+            "cell_table": self._cell_table_check,
+            "deepcell_output": self._deepcell_output_check
+        }
+
+        # Mapping the datasets to their respective test functions.
+        self.move_path_suffixes = {
+            "image_data": "image_data",
+            "cell_table": "segmentation/cell_table",
+            "deepcell_output": "segmentation/deepcell_output"
+        }
+
+    def test_download_example_dataset(self, dataset_download: ExampleDataset):
+        """
+        Tests to make sure the proper files are downloaded from Hugging Face.
+
+        Args:
+            dataset_download (ExampleDataset): Fixture for the dataset, respective to each
+            partition (`nb1`, `nb2`, `nb3`, `nb4`).
+        """
+        dataset_names = list(
+            dataset_download.dataset_paths[dataset_download.dataset].features.keys())
+
+        for ds_n in dataset_names:
+            dataset_cache_path = pathlib.Path(
+                dataset_download.dataset_paths[dataset_download.dataset][ds_n][0])
+            self.dataset_test_fns[ds_n](dir_p=dataset_cache_path / ds_n)
+
+    def test_move_example_dataset(self, tmp_path_factory, dataset_download: ExampleDataset):
+        """
+        Tests to make sure the proper files are moved to the correct directories.
+
+        Args:
+            dataset_download (ExampleDataset): Fixture for the dataset, respective to each
+            partition (`nb1`, `nb2`, `nb3`, `nb4`).
+        """
+        tmp_dir = tmp_path_factory.mktemp("move_example_data")
+        move_dir = tmp_dir / "example_dataset"
+        dataset_download.move_example_dataset(save_dir=move_dir)
+
+        dataset_names = list(
+            dataset_download.dataset_paths[dataset_download.dataset].features.keys())
+
+        for ds_n in dataset_names:
+            ds_n_suffix = self.move_path_suffixes[ds_n]
+
+            dir_p = move_dir / ds_n_suffix
+            self.dataset_test_fns[ds_n](dir_p)
+
+    def test_get_example_dataset(self):
+        """
+        Tests to make sure that the `data_utils.py::get_example_dataset` works.
+        #! Only tests for incorrect `dataset` value.
+        """
+
+        with pytest.raises(ValueError):
+            get_example_dataset("incorrect_dataset", save_dir=None)
+
+    def _image_data_check(self, dir_p: pathlib.Path):
+        """
+        Checks to make sure that all the FOVs exist.
+
+        Args:
+            dir (pathlib.Path): The directory to check.
+        """
+        # Check to make sure all the FOVs exist
+        downloaded_fovs = list(dir_p.glob("*"))
+        downloaded_fov_names = [f.stem for f in downloaded_fovs]
+        assert set(self.fov_names) == set(downloaded_fov_names)
+
+        # Check to make sure all 22 channels exist
+        for fov in downloaded_fovs:
+            c_names = [c.stem for c in fov.rglob("*")]
+            assert set(self.channel_names) == set(c_names)
+
+    def _cell_table_check(self, dir_p: pathlib.Path):
+        """
+        Checks to make sure that the following cell tables exist:
+            * `cell_table_arcsinh_transformed.csv`
+            * `cell_table_size_normalized.csv`
+
+        Args:
+            dir_p (pathlib.Path): The directory to check.
+        """
+
+        downloaded_cell_tables = list(dir_p.glob("*.csv"))
+        downloaded_cell_table_names = [f.stem for f in downloaded_cell_tables]
+        assert set(self.cell_table_names) == set(downloaded_cell_table_names)
+
+    def _deepcell_output_check(self, dir_p: pathlib.Path):
+        """
+        Checks to make sure that all cell nucleus (feature 0) and cell membrane masks (feature 1)
+        exist from deepcell output.
+
+        Args:
+            dir_p (pathlib.Path): The directory to check.
+        """
+        downloaded_deepcell_output = list(dir_p.glob("*.tif"))
+        downloaded_deepcell_output_names = [f.stem for f in downloaded_deepcell_output]
+        assert set(self.deepcell_output_names) == set(downloaded_deepcell_output_names)
