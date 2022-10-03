e362e528d70b80ccb0259f4ed54c3e776247818b
Sricharan Reddy Varra <srivarra@stanford.edu>
2022-10-03 14:57:16 -0600

    Next Release v0.4.2 (#721)
    * small dataset loading fix
    
    * bumped version from 0.4.1 -> 0.4.2
    
    * test fix
    
    * remove unnecessary print
    
    * notebook path fix
    
    * updated start_docker version
    
    * input_data -> image_data
    
    * several links and badges point to master instead of main. Added docs on how to update ark-analysis within the docker image.
    
    * some sphinx whitespace fix
    
    * force readthedocs to build again
    
    * dockerfile change

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if CYTHON_DEBUG:
 
 CYTHON_MACROS = [('CYTHON_TRACE', '1')] if CYTHON_DEBUG else None
 
-VERSION = '0.4.1'
+VERSION = '0.4.2'
 
 PKG_FOLDER = path.abspath(path.join(__file__, pardir))
 
