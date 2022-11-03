bb45d15cdcdb446a4a6d454f32eeb636ad7bfd29
Sricharan Reddy Varra <srivarra@stanford.edu>
2022-11-02 17:32:27 -0700

    bumped ark version: 0.4.3 -> 0.4.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if CYTHON_DEBUG:
 
 CYTHON_MACROS = [('CYTHON_TRACE', '1')] if CYTHON_DEBUG else None
 
-VERSION = '0.4.3'
+VERSION = '0.4.4'
 
 PKG_FOLDER = path.abspath(path.join(__file__, pardir))
 
