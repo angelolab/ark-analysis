b6954ee9b3d82b95f39636412b9b651c6a4e3253
Sricharan Reddy Varra <srivarra@stanford.edu>
2022-11-21 12:37:26 -0700

    removed duplicate computation

--- ark/phenotyping/post_cluster_utils.py
+++ ark/phenotyping/post_cluster_utils.py
@@ -76,19 +76,19 @@ def create_mantis_project(cell_table, fovs, seg_dir, pop_col,
     # generate unique numeric value for each population
     small_table['pop_vals'] = pd.factorize(small_table[pop_col].tolist())[0] + 1
 
+    whole_cell_files = [fov + seg_suffix_name + ".tiff" for fov in fovs]
+    # load the segmentation labels in for the FOVs
+    label_map = load_utils.load_imgs_from_dir(
+        data_dir=seg_dir, files=whole_cell_files, xr_dim_name='compartments',
+        xr_channel_names=[seg_suffix_name], trim_suffix=seg_suffix_name
+    )
+
     # label and save the cell mask for each FOV
     for fov in fovs:
-        whole_cell_file = [fov + seg_suffix_name + '.tiff' for fov in fovs]
-
-        # load the segmentation labels in for the FOV
-        label_map = load_utils.load_imgs_from_dir(
-            data_dir=seg_dir, files=whole_cell_file, xr_dim_name='compartments',
-            xr_channel_names=[seg_suffix_name], trim_suffix=seg_suffix_name
-        ).loc[fov, ...]
 
         # use label_cells_by_cluster to create cell masks
         mask_data = data_utils.label_cells_by_cluster(
-            fov, small_table, label_map, fov_col='fov',
+            fov, small_table, label_map.loc[fov, ...], fov_col='fov',
             cell_label_column='label', cluster_column='pop_vals'
         )
 
