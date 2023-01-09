eb8787a751fbffb8f0270d683af0179828610f97
Sricharan Reddy Varra <srivarra@stanford.edu>
2023-01-09 12:42:52 -0800

    reset example_dataset_revision to main

--- ark/settings.py
+++ ark/settings.py
@@ -67,4 +67,4 @@ EXTENSION_TYPES: Dict[str, List[str]] = {
 
 # Switch it from `main` to the commit ID on HuggingFace to test a
 # specific version of the Example Dataset
-EXAMPLE_DATASET_REVISION: str = "04aa699241b75fb04a0cabeb0e3944659349499c"
+EXAMPLE_DATASET_REVISION: str = "main"
