1bbc5c2eac868fa41f13e64e2f0f0d165282f0b0
alex-l-kong <alkong@ucdavis.edu>
2023-01-17 16:40:09 -0800

    Filter out NA columns prior to running cell clustering

--- ark/phenotyping/cell_cluster_utils.py
+++ ark/phenotyping/cell_cluster_utils.py
@@ -385,6 +385,16 @@ def create_c2pc_data(fovs, pixel_data_path,
     cell_table = cell_table.reset_index(drop=True)
     cell_table_norm = cell_table_norm.reset_index(drop=True)
 
+    # filter out all NA columns
+    cell_table_norm = cell_table_norm[cell_table_norm.isnull().sum() == 0]
+    cell_table = cell_table[cell_table_norm.isnull().sum() == 0]
+
+    # inform the user of the columns dropped, if applicable
+    if cell_table_norm.shape < len(count_cols):
+        nan_pixel_cols = set(count_cols).difference(list(cell_table_norm.columns.values))
+        warnings.warn('Pixel clusters %s do not appear in any cells, removed from analysis' %
+                      ','.join(nan_pixel_cols))
+
     return cell_table, cell_table_norm
 
 
