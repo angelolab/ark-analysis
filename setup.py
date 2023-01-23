e4f892ab4251fcd29e3ebd8a304dab5abcad5871
Sricharan Reddy Varra <srivarra@stanford.edu>
2023-01-23 14:05:41 -0800

    updates for next release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if CYTHON_DEBUG:
 
 CYTHON_MACROS = [('CYTHON_TRACE', '1')] if CYTHON_DEBUG else None
 
-VERSION = '0.5.0'
+VERSION = '0.5.1'
 
 PKG_FOLDER = path.abspath(path.join(__file__, pardir))
 
