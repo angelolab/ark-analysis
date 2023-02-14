6bdac58ff72083504e0d280d36ea3699d55ae245
alex-l-kong <alkong@ucdavis.edu>
2023-02-14 14:26:52 -0800

    Remove deterministic flag for testing

--- ark/phenotyping/cluster_helpers.py
+++ ark/phenotyping/cluster_helpers.py
@@ -68,7 +68,7 @@ class PixieSOMCluster(ABC):
         # make sure to run a deterministic SOM for reproducibility purposes
         som_weights = som(
             data=data.values, xdim=self.xdim, ydim=self.ydim, rlen=self.num_passes,
-            alpha_range=(self.lr_start, self.lr_end), deterministic=True
+            alpha_range=(self.lr_start, self.lr_end)
         )
 
         # ensure dimensions of weights are flattened
