b1ff2822e2f1b53823ea33f638a7845d55f7a2fb
Sricharan Reddy Varra <srivarra@stanford.edu>
2023-05-16 12:09:44 -0700

    no posix() path

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ import os
 def dataset_cache_dir() -> Iterator[Union[str,None]]:
     # Change cache directory if running on CI
     if os.environ.get("CI", None):
-        cache_dir = pathlib.Path("./data/cache/").as_posix()
+        cache_dir = pathlib.Path("./data/cache/")
     else:
         cache_dir = None
     yield cache_dir
