359f466996ad6f9d470ec63647ecc6f4988cd726
alex-l-kong <alkong@ucdavis.edu>
2023-05-25 15:59:28 -0700

    Ensure pixel_meta_cluster gets re-merged in meta clustering on overwrite

--- src/ark/phenotyping/cell_meta_clustering.py
+++ src/ark/phenotyping/cell_meta_clustering.py
@@ -158,6 +158,10 @@ def generate_meta_avg_files(base_dir, cell_cc, cell_som_cluster_cols,
     # read in the average number of pixel/SOM clusters across all cell SOM clusters
     cell_som_cluster_avgs = pd.read_csv(som_expr_col_avg_path)
 
+    # this happens if the overwrite flag is set with previously generated data, need to overwrite
+    if 'cell_meta_cluster' in cell_som_cluster_avgs.columns.values:
+        cell_som_cluster_avgs = cell_som_cluster_avgs.drop(columns='cell_meta_cluster')
+
     # merge metacluster assignments in
     cell_som_cluster_avgs = pd.merge_asof(
         cell_som_cluster_avgs, cell_cc.mapping, on='cell_som_cluster'
