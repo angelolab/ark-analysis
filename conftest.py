c0c31edac976dbb6ecda5d101de1cbd3a880cc78
Sricharan Reddy Varra <srivarra@stanford.edu>
2023-06-05 21:59:37 -0700

    Save Colored Pixie Masks (#1007)
    * added save_pixie_masks
    
    * docstring fix
    
    * moved `saved_colored_masks` to be before `plot_pixel_cell_cluster_overlay`

--- conftest.py
+++ conftest.py
@@ -1,1 +1,17 @@
-# empty file so that pyetst adds top-level directory
+from typing import Generator
+
+import numpy as np
+import pytest
+
+
+@pytest.fixture(scope="module")
+def rng() -> Generator[np.random.Generator, None, None]:
+    """
+    Create a new Random Number Generator for tests which require randomized data.
+
+    Yields:
+        Generator[np.random.Generator, None, None]: The generator used for creating randomized
+        numbers.
+    """
+    rng: np.random.Generator = np.random.default_rng(12345)
+    yield rng
