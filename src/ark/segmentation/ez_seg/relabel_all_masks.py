7a0c75e7d83aad1dfcd2c13ae0ae82abcddc5078
bryjc <bryjcannon@gmail.com>
2023-10-12 13:25:59 -0700

    Fixed relabeline of all masks in merged mask dir.

--- src/ark/segmentation/ez_seg/relabel_all_masks.py
+++ src/ark/segmentation/ez_seg/relabel_all_masks.py
@@ -11,8 +11,20 @@ def renumber_masks(mask_dir):
     io_utils.validate_paths(mask_dir)
     
     all_images: Generator[pathlib.Path, None, None] = mask_dir.rglob("*.tiff")
-    
+
+    global_unique_labels = 1
+
     for image in all_images:
+        img: np.ndarray = imread(image)
+        unique_labels: np.ndarray = np.unique(img)
+        for label in unique_labels:
+            if label != 0:
+                global_unique_labels += 1
+
+    all_images: Generator[pathlib.Path, None, None] = mask_dir.rglob("*.tiff")
+
+    for image in all_images:
+        print("Relabeling: " + image.stem + image.suffix)
         img: np.ndarray = imread(image)
         unique_labels: np.ndarray = np.unique(img)
         for label in unique_labels:
@@ -20,6 +32,7 @@ def renumber_masks(mask_dir):
                 img[img == label] = global_unique_labels
                 global_unique_labels += 1
         save_image(fname=image, data=img)
+    print("Complete.")
     
     
     
