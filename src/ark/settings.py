90e18aa04b9f9f9fd04370ac6ee91599fffd3a8e
alex-l-kong <alkong@ucdavis.edu>
2023-10-27 14:46:39 -0700

    Initial commit of ezSegmenter example dataset integration

--- src/ark/settings.py
+++ src/ark/settings.py
@@ -60,4 +60,4 @@ MIBITRACKER_BACKEND = 'https://backend-dot-mibitracker-angelolab.appspot.com'
 
 # Switch it from `main` to the commit ID on HuggingFace to test a
 # specific version of the Example Dataset
-EXAMPLE_DATASET_REVISION: str = "main"
+EXAMPLE_DATASET_REVISION: str = "1359d2bd16d134f4b995595cf2918cc1f063bee4"
