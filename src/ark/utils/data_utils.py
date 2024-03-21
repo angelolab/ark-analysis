Fix color masking in Pixie (#1127)
* adjust color masking

* update pixel clustering mantis mapping file

* comments

* add test just to make super sure

* pycodestyle

* simplify _metacluster_cmap_generator

* fix pixel cluster mask generation

* avoid verify_same_elements error when cluster_id is in mapping file

src/ark/utils/data_utils.py
@@ -131,9 +131,11 @@ class ClusterMaskData:
         ].copy()
 
         # Add a cluster_id_column to the column in case the cluster_column is
-        # non-numeric (i.e. string)
+        # non-numeric (i.e. string), index in ascending order of cell_meta_cluster
         cluster_name_id = pd.DataFrame(
             {self.cluster_column: mapping_data[self.cluster_column].unique()})
+        cluster_name_id.sort_values(by=f'{self.cluster_column}', inplace=True)
+        cluster_name_id.reset_index(drop=True, inplace=True)
 
         cluster_name_id[self.cluster_id_column] = (cluster_name_id.index + 1).astype(np.int32)
 
@@ -475,7 +477,8 @@ def generate_and_save_cell_cluster_masks(
 
 
 def generate_pixel_cluster_mask(fov, base_dir, tiff_dir, chan_file_path,
-                                pixel_data_dir, pixel_cluster_col='pixel_meta_cluster'):
+                                pixel_data_dir, cluster_mapping,
+                                pixel_cluster_col='pixel_meta_cluster'):
     """For a fov, create a mask labeling each pixel with their SOM or meta cluster label
 
     Args:
@@ -491,6 +494,8 @@ def generate_pixel_cluster_mask(fov, base_dir, tiff_dir, chan_file_path,
         pixel_data_dir (str):
             The path to the data with full pixel data.
             This data should also have the SOM and meta cluster labels appended.
+        cluster_mapping (pd.DataFrame)
+            Dataframe detailing which meta_cluster IDs map to which cluster_id
         pixel_cluster_col (str):
             Whether to assign SOM or meta clusters
             needs to be `'pixel_som_cluster'` or `'pixel_meta_cluster'`
@@ -537,8 +542,13 @@ def generate_pixel_cluster_mask(fov, base_dir, tiff_dir, chan_file_path,
     # convert to 1D indexing
     coordinates = x_coords * img_data.shape[1] + y_coords
 
-    # get the cooresponding cluster labels for each pixel
+    # get the corresponding cluster labels for each pixel
     cluster_labels = list(fov_data[pixel_cluster_col])
+
+    # relabel meta_cluster numbers with cluster_id
+    cluster_mapping = cluster_mapping.drop_duplicates()[[pixel_cluster_col, 'cluster_id']]
+    id_mapping = dict(zip(cluster_mapping[pixel_cluster_col], cluster_mapping['cluster_id']))
+    cluster_labels = [id_mapping[label] for label in cluster_labels]
 
     # assign each coordinate in pixel_cluster_mask to its respective cluster label
     img_subset = img_data.ravel()
@@ -554,6 +564,7 @@ def generate_and_save_pixel_cluster_masks(fovs: List[str],
                                           tiff_dir: Union[pathlib.Path, str],
                                           chan_file: Union[pathlib.Path, str],
                                           pixel_data_dir: Union[pathlib.Path, str],
+                                          cluster_id_to_name_path: Union[pathlib.Path, str],
                                           pixel_cluster_col: str = 'pixel_meta_cluster',
                                           sub_dir: str = None,
                                           name_suffix: str = ''):
@@ -574,6 +585,9 @@ def generate_and_save_pixel_cluster_masks(fovs: List[str],
         pixel_data_dir (Union[pathlib.Path, str]):
             The path to the data with full pixel data.
             This data should also have the SOM and meta cluster labels appended.
+        cluster_id_to_name_path (Union[str, pathlib.Path]): A path to a CSV identifying the
+            pixel cluster to manually-defined name mapping this is output by the remapping
+            visualization found in `metacluster_remap_gui`.
         pixel_cluster_col (str, optional):
             The path to the data with full pixel data.
             This data should also have the SOM and meta cluster labels appended.
@@ -585,6 +599,19 @@ def generate_and_save_pixel_cluster_masks(fovs: List[str],
         name_suffix (str, optional):
             Specify what to append at the end of every pixel mask. Defaults to `''`.
     """
+    # read in gui cluster mapping file and save cluster_id created in generate_pixel_cluster_mask
+    gui_map = pd.read_csv(cluster_id_to_name_path)
+    cluster_map = gui_map.copy()[[pixel_cluster_col]]
+
+    cluster_map = cluster_map.drop_duplicates().sort_values(by=[pixel_cluster_col])
+    cluster_map["cluster_id"] = list(range(1, len(cluster_map) + 1))
+
+    # drop the cluster_id column from gui_map if it already exists, otherwise do nothing
+    gui_map = gui_map.drop(columns="cluster_id", errors="ignore")
+
+    # add a cluster_id column corresponding to the new mask integers
+    updated_cluster_map = gui_map.merge(cluster_map, on=[pixel_cluster_col], how="left")
+    updated_cluster_map.to_csv(cluster_id_to_name_path, index=False)
 
     # create the pixel cluster masks across each fov
     with tqdm(total=len(fovs), desc="Pixel Cluster Mask Generation", unit="FOVs") \
@@ -600,7 +627,8 @@ def generate_and_save_pixel_cluster_masks(fovs: List[str],
                 generate_pixel_cluster_mask(fov=fov, base_dir=base_dir, tiff_dir=tiff_dir,
                                             chan_file_path=chan_file_path,
                                             pixel_data_dir=pixel_data_dir,
-                                            pixel_cluster_col=pixel_cluster_col)
+                                            pixel_cluster_col=pixel_cluster_col,
+                                            cluster_mapping=updated_cluster_map)
 
             # save the pixel mask generated
             save_fov_mask(fov, data_dir=save_dir, mask_data=pixel_mask, sub_dir=sub_dir,

tests/phenotyping/cell_meta_clustering_test.py
@@ -257,7 +257,8 @@ def test_apply_cell_meta_cluster_remapping(weighted_cell_channel_exists):
         sample_cell_remapping = {
             'cell_som_cluster': [i for i in np.arange(100)],
             'cell_meta_cluster': [int(i / 5) for i in np.arange(100)],
-            'cell_meta_cluster_rename': ['meta' + str(int(i / 5)) for i in np.arange(100)]
+            'cell_meta_cluster_rename': ['meta' + str(int(i / 5)) for i in np.arange(100)],
+            'cluster_id': [int(i / 5) for i in np.arange(100)]
         }
         sample_cell_remapping = pd.DataFrame.from_dict(sample_cell_remapping)
         sample_cell_remapping.to_csv(
@@ -411,7 +412,8 @@ def test_generate_remap_avg_count_files():
         sample_cell_remapping = {
             'cell_som_cluster': [i for i in np.arange(100)],
             'cell_meta_cluster': [int(i / 5) for i in np.arange(100)],
-            'cell_meta_cluster_rename': ['meta' + str(int(i / 5)) for i in np.arange(100)]
+            'cell_meta_cluster_rename': ['meta' + str(int(i / 5)) for i in np.arange(100)],
+            'cluster_id': [int(i / 5) for i in np.arange(100)]
         }
         sample_cell_remapping = pd.DataFrame.from_dict(sample_cell_remapping)
         sample_cell_remapping.to_csv(

tests/utils/plot_utils_test.py
@@ -55,7 +55,7 @@ def metacluster_colors(n_metaclusters: int) -> Generator[Dict, None, None]:
     Yields:
         Generator[dict, None, None]: Yields the dictionary of metacluster colors.
     """
-    sample_mapping = {i: tuple(np.random.rand(4)) for i in np.arange(n_metaclusters)}
+    sample_mapping = {i: tuple(np.random.rand(4)) for i in np.arange(n_metaclusters) + 1}
 
     yield sample_mapping
 
@@ -158,11 +158,12 @@ def test_plot_pixel_cell_cluster(metacluster_colors):
         # invalid columns provided in mapping
         df = pd.DataFrame.from_dict(
             {
-                "bad_cluster_col": np.arange(20),
-                "pixel_meta_cluster": np.repeat(np.arange(5), 4),
+                "bad_cluster_col": np.arange(20) + 1,
+                "pixel_meta_cluster": np.repeat(np.arange(5) + 1, 4),
                 "pixel_meta_cluster_rename": [
-                    "meta" + str(i) for i in np.repeat(np.arange(5), 4)
+                    "meta" + str(i) for i in np.repeat(np.arange(5) + 1, 4)
                 ],
+                "cluster_id": np.repeat(np.arange(5) + 1, 4),
             }
         )
         df.to_csv(cluster_id_to_name_path, index=False)
@@ -174,6 +175,7 @@ def test_plot_pixel_cell_cluster(metacluster_colors):
 
         # rename bad_cluster_col to cluster so it passes that test
         df = df.rename({"bad_cluster_col": "pixel_som_cluster"}, axis=1)
+
         df.to_csv(cluster_id_to_name_path, index=False)
 
         # invalid sample_mapping dict provided, metaclusters do not match
@@ -592,8 +594,7 @@ def test_create_mantis_dir(
                 os.path.join(output_path, f"population{mask_suff}.csv"))
 
             # 3.a. Assert that appropratiate cluster col equals the region_id col
-            cluster_col = "cluster_id" if cluster_type == "cell"\
-                else f"{cluster_type}_meta_cluster"
+            cluster_col = "cluster_id"
             metacluster_col = original_mapping_df[[cluster_col]].drop_duplicates()
             region_id_col = new_mapping_df[["region_id"]]
             assert np.equal(metacluster_col.values, region_id_col.values).all()
@@ -659,16 +660,13 @@ def cluster_id_to_name_mapping(
 
     df = pd.DataFrame.from_dict(
         {
-            f"{cluster_type}_som_cluster": np.arange(20),
-            f"{cluster_type}_meta_cluster": np.repeat(np.arange(n_metaclusters), 4),
+            f"{cluster_type}_som_cluster": np.arange(20) + 1,
+            f"{cluster_type}_meta_cluster": np.repeat(np.arange(n_metaclusters) + 1, 4),
             f"{cluster_type}_meta_cluster_rename": [
-                "meta" + str(i) for i in np.repeat(np.arange(n_metaclusters), 4)],
+                "meta" + str(i) for i in np.repeat(np.arange(n_metaclusters) + 1, 4)],
+            "cluster_id": np.repeat(np.arange(n_metaclusters) + 1, 4)
         }
     )
-
-    # add cluster_id for cell file
-    if cluster_type == "cell":
-        df["cluster_id"] = np.repeat(np.arange(n_metaclusters)+1, 4)
 
     df.to_csv(mapping_path, index=False)
 
@@ -690,30 +688,23 @@ class TestMetaclusterColormap:
         self.n_metaclusters = n_metaclusters
 
     def test_metacluster_cmap_generator(self):
-        # Test by initilizing the MetaclusterColormap class, then __post_init__ will
+        # Test by initializing the MetaclusterColormap class, then __post_init__ will
         # run `_metacluster_cmp_generator`
         mcc = plot_utils.MetaclusterColormap(
             cluster_type=self.cluster_type,
             cluster_id_to_name_path=self.cluster_id_to_name_mapping,
             metacluster_colors=self.metacluster_colors,
         )
-
         # Assert that the fields after __post_init__ are correct
-
         # Assert metacluster_to_id_name is correct, contains "`Unassigned`" and `"No Cluster`
         assert set(
             mcc.metacluster_id_to_name[f"{self.cluster_type}_meta_cluster_rename"]
-        ) == set([f"meta{i}" for i in range(5)] + ["Unassigned", "Empty"])
+        ) == set([f"meta{i}" for i in range(1, 6)] + ["Unassigned", "Empty"])
 
         # Assert mc_colors has the correct shape
         # Add 2 colors to account for an element with no associated cluster, and an element with an
         # unassigned cluster
         assert mcc.mc_colors.shape == (self.n_metaclusters + 2, 4)
-
-        # Assert metacluster_to_index is correct
-        assert mcc.metacluster_to_index == {
-            i: i + 1 for i in range(self.n_metaclusters + 1)
-        }
 
         # Assert the cmap has the correct number of colors
         assert mcc.cmap.N == self.n_metaclusters + 2
@@ -722,29 +713,6 @@ class TestMetaclusterColormap:
         # This would be for `plot_neighborhood_cluster_result`, currently not used by
         # `plot_pixel_cell_cluster`
         assert mcc.norm.N == self.n_metaclusters + 3
-
-    def test_assign_metacluster_cmap(self):
-        mcc = plot_utils.MetaclusterColormap(
-            cluster_type=self.cluster_type,
-            cluster_id_to_name_path=self.cluster_id_to_name_mapping,
-            metacluster_colors=self.metacluster_colors,
-        )
-        fov_img: np.ndarray = _generate_segmentation_labels(
-            (1024, 1024), num_cells=self.n_metaclusters
-        )
-        mc_cmap = mcc.assign_metacluster_cmap(fov_img=fov_img)
-
-        # Assert colored mask shape
-        assert mc_cmap.shape == (1024, 1024)
-
-        # Assert colored mask dtype
-        assert mc_cmap.dtype == np.uint8
-
-        # Assert the number of unique metacluster_cmap values in the colored mask are correct
-        assert np.unique(mc_cmap).shape == (self.n_metaclusters,)
-
-        # Assert the unique metacluster_cmap values are correct
-        assert np.all(np.unique(mc_cmap) == np.arange(self.n_metaclusters) + 1)
 
 
 @pytest.fixture(scope="function")
@@ -828,6 +796,17 @@ def test_save_colored_masks(
 
         # Get the unique colors in the colored mask
         assert np.max(np.unique(colored_mask, axis=None)) <= 255
+
+        # check that colored mask is mapped correctly
+        cluster_mask = io.imread(os.path.join(create_masks, fov + f'_{cluster_type}_mask.tiff'))
+        rgb_mask = (colored_mask[:, :, 0]/255).round(1)
+
+        for id_num in metacluster_colors.keys():
+            if id_num != 6:
+                cluster_idx = np.where(cluster_mask == id_num)
+                colored_idx = np.where(rgb_mask == np.round(metacluster_colors[id_num][0], 1))
+                assert np.all(cluster_idx[0] == colored_idx[0])
+                assert np.all(cluster_idx[1] == colored_idx[1])
 
 
 @dataclass